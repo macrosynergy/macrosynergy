4155e3da40d639981ce4c277c7b0aa5ca2cf25a1
Daniel Szeredi <daniel@szeredi.hu>
2021-06-01 23:17:45 +0100

    needed changes for pypi release

---
+++ macrosynergy/version.py
@@ -0,0 +1,8 @@
+# THIS FILE IS GENERATED FROM macrosynergy SETUP.PY
+short_version: str = '0.0.1'
+version: str = '0.0.1'
+full_version: str = '0.0.1'
+git_revision: str = '56b1bc144f144c7eaba544d1f1c2d2fbfcb26f6c'
+release: bool = True
+if not release:
+    version = full_version
