d436deb2408ed484e66229eec537301915b9602d
rsueppel <rsueppel@mail.com>
2021-07-02 15:59:53 +0100

    first version of signal_return

--- macrosynergy/signal/signal_return.py
+++ macrosynergy/signal/signal_return.py
@@ -42,9 +42,12 @@ class SignalReturnRelations:
                                 lag=1, fwin=fwin, xcat_aggs=['mean', 'last'])
         self.ret = ret
         self.sig = sig
+        self.freq = freq
         self.cids = list(np.sort(self.df.index.get_level_values(0).unique()))
         self.df_cs = self.panel_relations(cs_type='cids')
         self.df_ys = self.panel_relations(cs_type='years')
+        self.dic_freq = {'D': 'daily', 'W': 'weekly', 'M': 'monthly', 'Q': 'quarterly', 'A': 'annual'}
+
         """Creates a dataframe of return and signal in the appropriate form for subsequent analysis."""
 
     def panel_relations(self, cs_type: str = 'cids'):
@@ -59,8 +62,8 @@ class SignalReturnRelations:
             df['year'] = np.array(df.reset_index(level=1)['real_date'].dt.year)
             css = [str(i) for i in df['year'].unique()]
 
-        statms = ['accuracy', 'bal_accuracy', 'f1_score', 'pearson', 'pearson_prob', 'kendall', 'kendall_prob']
-        df_out = pd.DataFrame(index=['Panel', 'Average', 'PosRatio'] + css, columns=statms)
+        statms = ['accuracy', 'bal_accuracy', 'f1_score', 'pearson', 'pearson_pval', 'kendall', 'kendall_pval']
+        df_out = pd.DataFrame(index=['Panel', 'Mean', 'PosRatio'] + css, columns=statms)
 
         for cs in (css + ['Panel']):
             if cs in css:
@@ -77,18 +80,18 @@ class SignalReturnRelations:
             df_out.loc[cs, 'f1_score'] = skm.f1_score(ret_signs, sig_signs, average='weighted')
 
             ret_vals, sig_vals = df_cs[self.ret], df_cs[self.sig]
-            df_out.loc[cs, ['kendall', 'kendall_prob']] = stats.kendalltau(ret_vals, sig_vals)
-            df_out.loc[cs, ['pearson', 'pearson_prob']] = stats.pearsonr(ret_vals, sig_vals)
+            df_out.loc[cs, ['kendall', 'kendall_pval']] = stats.kendalltau(ret_vals, sig_vals)
+            df_out.loc[cs, ['pearson', 'pearson_pval']] = stats.pearsonr(ret_vals, sig_vals)
 
-        df_out.loc['Average', :] = df_out.loc[css, :].mean()
+        df_out.loc['Mean', :] = df_out.loc[css, :].mean()
 
         above50s = statms[0:3]
         df_out.loc['PosRatio', above50s] = (df_out.loc[css, above50s] > 0.5).mean()
         above0s = [statms[i] for i in [3, 5]]
         df_out.loc['PosRatio', above0s] = (df_out.loc[css, above0s] > 0).mean()
         below50s = [statms[i] for i in [4, 6]]
-        pos_probs = np.mean(np.array(df_out.loc[css, below50s] < 0.5) * np.array(df_out.loc[css, above0s] > 0), axis=0)
-        df_out.loc['PosRatio', below50s] = pos_probs  # positive correlations with error probabilities < 50%
+        pos_pvals = np.mean(np.array(df_out.loc[css, below50s] < 0.5) * np.array(df_out.loc[css, above0s] > 0), axis=0)
+        df_out.loc['PosRatio', below50s] = pos_pvals  # positive correlations with error probabilities < 50%
         return df_out
 
     def cross_section_table(self):
@@ -99,11 +102,68 @@ class SignalReturnRelations:
         """Returns dataframe with information on the signal-return relation across years and the panel."""
         return self.df_ys.round(decimals=3)
 
-    def accuracy_bars(self):
-        pass
+    def accuracy_bars(self, type: str = 'cross_section', title: str = None, size: Tuple[float] = None):
+        """Bars of overall and balanced accuracy
 
-    def correlation_bars(self):
-        pass
+        :param <str> type: type of segment over which bars are drawn. Must be 'cross_section' (default) or 'years'
+        :param <str> title: chart header. Default will be applied if none is chosen,
+        :param <Tuple[float]> size: 2-tuple of width and height of plot.  Default will be applied if none is chosen,
+
+        """
+
+        assert type in ['cross_section', 'years']
+
+        df_xs = self.df_cs if type == 'cross_section' else self.df_ys
+        dfx = df_xs[~df_xs.index.isin(['PosRatio'])]
+
+        if title is None:
+            title = f'Accuracy for sign prediction of {self.ret} based on {self.sig} ' \
+                    f'at {self.dic_freq[self.freq]} frequency'
+        if size is None:
+            size = (np.max([dfx.shape[0]/2, 8]), 6)
+
+        plt.style.use('seaborn')
+        plt.figure(figsize=size)
+        x_indexes = np.arange(len(dfx.index))  # generic x index
+        w = 0.4  # offset parameter, related to width of bar
+        plt.bar(x_indexes - w / 2, dfx['accuracy'], label='Accuracy', width=w, color='lightblue')
+        plt.bar(x_indexes + w / 2, dfx['bal_accuracy'], label='Balanced Accuracy', width=w, color='steelblue')
+        plt.xticks(ticks=x_indexes, labels=dfx.index, rotation=0)  # customize x ticks/labels
+        plt.axhline(y=0.5, color='black', linestyle='-', linewidth=0.5)
+        plt.ylim(np.round(np.max(dfx.loc[:, ['accuracy', 'bal_accuracy']].min().min()-0.03, 0), 2))
+        plt.title(title)
+        plt.legend()
+        plt.show()
+
+    def correlation_bars(self, type: str = 'cross_section', title: str = None, size: Tuple[float] = None):
+        """Correlation coefficients and significance"""
+
+        df_xs = self.df_cs if type == 'cross_section' else self.df_ys
+        dfx = df_xs[~df_xs.index.isin(['PosRatio', 'Mean'])]
+
+        pprobs = np.array([(1 - pv) * (np.sign(cc) + 1) / 2 for pv, cc in zip(dfx['pearson_pval'], dfx['pearson'])])
+        pprobs[pprobs == 0] = 0.01  # token small value for bar
+        kprobs = np.array([(1 - pv) * (np.sign(cc) + 1) / 2 for pv, cc in zip(dfx['kendall_pval'], dfx['kendall'])])
+        kprobs[kprobs == 0] = 0.01  # token small value for bar
+
+        if title is None:
+            title = f'Positive correlation probability of {self.ret} and lagged {self.sig} ' \
+                    f'at {self.dic_freq[self.freq]} frequency'
+        if size is None:
+            size = (np.max([dfx.shape[0]/2, 8]), 6)
+
+        plt.style.use('seaborn')
+        plt.figure(figsize=size)
+        x_indexes = np.arange(len(dfx.index))  # generic x index
+        w = 0.4  # offset parameter, related to width of bar
+        plt.bar(x_indexes - w / 2, pprobs, label='Pearson', width=w, color='lightblue')
+        plt.bar(x_indexes + w / 2, kprobs, label='Kendall', width=w, color='steelblue')
+        plt.xticks(ticks=x_indexes, labels=dfx.index, rotation=0)  # customize x ticks/labels
+        plt.axhline(y=0.95, color='orange', linestyle='--', linewidth=0.5, label='95% probability')
+        plt.axhline(y=0.99, color='red', linestyle='--', linewidth=0.5, label='99% probability')
+        plt.title(title)
+        plt.legend()
+        plt.show()
 
 
 if __name__ == "__main__":
@@ -125,7 +185,10 @@ if __name__ == "__main__":
     dfd = make_qdf(df_cids, df_xcats, back_ar=0.75)
 
     srr = SignalReturnRelations(dfd, sig='CRY', ret='XR')
+    srr.correlation_bars(type='years')
+    srr.accuracy_bars(type='years')
     df_cs_stats = srr.cross_section_table()
     df_ys_stats = srr.yearly_table()
+
 
     print(srr)
