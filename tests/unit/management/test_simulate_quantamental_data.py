ef4f4d60ff8e8b2a0c031fe1774d0182badd810c
lsimonsen <lasse@delaporte.dk>
2021-08-31 14:24:36 +0100

    Patch: fix unittests

--- tests/unit/management/test_simulate_quantamental_data.py
+++ tests/unit/management/test_simulate_quantamental_data.py
@@ -7,13 +7,13 @@ from macrosynergy.management.simulate_quantamental_data import make_qdf, simulat
 cids = ['AUD', 'CAD', 'GBP']
 xcats = ['XR', 'CRY']
 df_cids = pd.DataFrame(index=cids, columns=['earliest', 'latest', 'mean_add', 'sd_mult'])
-df_cids.loc['AUD', ] = ['2010-01-01', '2020-12-31', 0.5, 2]
-df_cids.loc['CAD', ] = ['2011-01-01', '2020-11-30', 0, 1]
-df_cids.loc['GBP', ] = ['2011-01-01', '2020-11-30', -0.2, 0.5]
+df_cids.loc['AUD', :] = ['2010-01-01', '2020-12-31', 0.5, 2]
+df_cids.loc['CAD', :] = ['2011-01-01', '2020-11-30', 0, 1]
+df_cids.loc['GBP', :] = ['2011-01-01', '2020-11-30', -0.2, 0.5]
 
 df_xcats = pd.DataFrame(index=xcats, columns=['earliest', 'latest', 'mean_add', 'sd_mult', 'ar_coef', 'back_coef'])
-df_xcats.loc['XR', ] = ['2010-01-01', '2020-12-31', 0, 1, 0, 0.3]
-df_xcats.loc['CRY', ] = ['2011-01-01', '2020-10-30', 1, 2, 0.9, 0.5]
+df_xcats.loc['XR', :] = ['2010-01-01', '2020-12-31', 0, 1, 0, 0.3]
+df_xcats.loc['CRY', :] = ['2011-01-01', '2020-10-30', 1, 2, 0.9, 0.5]
 
 random.seed(1)
 dfd = make_qdf(df_cids, df_xcats, back_ar=0.75)
