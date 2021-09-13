5d6266ff6e167f3d4b70a58fae5835f95a73d33e
Ksteeds <csteeds@macrosynergy.com>
2021-09-13 12:44:37 +0100

    historic_volatility, update

--- macrosynergy/panel/historic_volality.py
+++ macrosynergy/panel/historic_volality.py
@@ -4,11 +4,10 @@ from collections import defaultdict, deque
 import time
 import matplotlib.pyplot as plt
 from typing import List, Union, Tuple
-from Test_File import make_qdf_, simulate_ar
 from random import choice
+from macrosynergy.management.simulate_quantamental_data import make_qdf
+from macrosynergy.management.shape_dfs import categories_df
 
-## from macrosynergy.management.simulate_quantamental_data import make_qdf
-## from macrosynergy.management.shape_dfs import categories_df
 
 def remove_zeros(df: pd.DataFrame):
     values = df['value'].to_numpy()
@@ -137,6 +136,26 @@ def historic_vol(df: pd.DataFrame, xcat: str, cids: List[str] = None, xcats: Lis
                  lback_meth: str = 'xma', lback_periods: int = 21, start: str = None, end: str = None,
                  zeros: bool = True, cutoff: float = 0.01, postfix: str = 'ASD'):
 
+     """
+    Estimate historic annualized standard deviations of asset returns
+    :param <pd.Dataframe> df: standardized data frame with the following necessary columns:
+    'cid', 'xcats', 'real_date' and 'value.
+    :param <str> xcat:  extended category denoting the return series for which volatility should be calculated.
+    :param <List[str]> cids: cross sections for which volatility is calculated;
+        default is all available for the category.
+    :param <str> start: earliest date in ISO format. Default is None and earliest date in df is used.
+    :param <str> end: latest date in ISO format. Default is None and latest date in df is used.
+    :param <str> lback_meth: Lookback method to calculate the volatility, Default is "xma" (exponential moving average).
+        Alternative is "ma", simple moving average.
+    :param <int>  lback_periods: Number of lookback periods over which volatility is calculated. Default is 21.
+        Refers to half-time for "xma" and full lookback period for "ma".
+    :param <bool> remove_zeros: if True (default) any returns that are exact zeroes will not be included in the
+        lookback window and prior non-zero values are added to the window instead.
+    :param <float> cutoff: share of past observation weights in the exponential moving average that is disregarded.
+        This prevents NaNs in distant history from propagating. Default is 0.01
+    :param <str> postfix: string appended to category name for output; default is "ASD".
+    :return <pd.Dataframe>: standardized dataframe with the estimated annualized standard deviations
+    """
     if zeros:
         df = remove_zeros(df)
     
@@ -163,9 +182,9 @@ if __name__ == "__main__":
     df_xcats.loc['DUXR'] = ['2001-01-01', '2020-10-30', 1, 2, 0.9, 1]
 
 
-    dfd, fields_cats, fields_cids, df_year, df_end, df_missing, cids_cats = make_qdf_(df_cids, df_xcats, back_ar = 0.75)
+    dfd = make_qdf(df_cids, df_xcats, back_ar = 0.75)
 
     start = time.time()
-    df = historic_vol(dfd, choice(fields_cats), fields_cids, fields_cats, 'xma', 30)
+    df = historic_vol(dfd, choice(xcats), cids, xcats, 'xma', 30)
     print(f"Time Elapsed: {time.time() - start}.")
     print(df)
