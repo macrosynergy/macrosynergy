6c7fb83ea06ef835674b1c37654f2577fd71d8c1
Ksteeds <csteeds@macrosynergy.com>
2021-09-23 15:02:11 +0100

    Majority of functionality complete, remains a known 'bug'.

--- macrosynergy/panel/Zn_Scores.py
+++ macrosynergy/panel/Zn_Scores.py
@@ -7,31 +7,20 @@ from itertools import islice
 from bisect import insort
 from macrosynergy.management.simulate_quantamental_data import make_qdf
 
-## Converts panel of indicators to a panel of zn-scores (z-scores around a neutral level), in-sample or out-of-sample.
-
-## zn_ref: reference data for calculating the zn-score, i.e. standard deviations and central points. Default is "panel".
-## Alternative is "cross_section" and "weighted". The latter means a weighted average of standard deviations and central points between panel and cross section.
-## zn_cs_weight: weight of cross section in weighted zn-score calculation. Only relevant is "weighted" option has been chosen for zn_ref.
-## abs_max: maximum absolute value allows for zn-score. Default is None, i.e. no limitation. postfix: string appended to category name for output; default is "ZN".
 
 class RollingMedian(object):
     '''Slow running-median with O(n) updates, because of List's unavoidable shifting, where n is the window size.'''
 
-    ## The parameter, "iterable", represents the entire data series that a rolling median is being computed over.
     def __init__(self, iterable):
         self.it = iter(iterable)
         self.list = list(islice(self.it, 1))
         self.sortedlist = sorted(self.list)
 
-    ## Bitwise Operator: bin(1) = 00..001. The AND Operator requires each Binary Element, in the comparison, to be equal to one.
-    ## All odd Integers' least significant Bit will be one. Therefore, the comparison, using the AND Operator, will return a one if the subroutine receives an odd Integer.
     @staticmethod
     def isOdd(num):
 
         return (num & 1)
 
-    ## The __iter__ defines a method on a Class which return an Iterator: an Object that successfully yields the next item contained by the Object.
-    ## Interrupt the existing framework to iterate through the List and return each median.
     def __iter__(self):
         
         list_ = self.list
@@ -123,10 +112,10 @@ def mult(xcats, list_):
 
 class dfManipulation:
 
-    def __init__(self, df, cids, xcats):
+    def __init__(self, df, cids, xcat):
         self.dfd = dfd = df
         self.cids = cids
-        self.xcats = xcats
+        self.xcats = xcat
         self.d_xcat = defaultdict(list)
         self.cid_xcat = {}
 
@@ -202,76 +191,76 @@ def ordering(df, arr):
     return list_
 
 
-def z_formula(arr, mean_, std):
-    mean_ = mean_[:, np.newaxis]
+def z_formula(arr, neutral, std):
+    neutral = neutral[:, np.newaxis]
     std = std[:, np.newaxis]
     
-    diff = np.subtract(arr, mean_)
+    diff = np.subtract(arr, neutral)
     z_data = np.divide(diff, std)
 
     return z_data        
 
-def cross_section(dfd: pd.DataFrame, cids: List[str] = None, xcats: List[str] = None,
-                  min_obs: int = 252, neutral: str = 'zero'):
+def cross_section(dfd: pd.DataFrame, cids: List[str] = None, min_obs: int = 252,
+                  neutral: str = 'zero', xcat: str = None):
 
-    list_ = []
-
-    for xcat in xcats:
-        df_output = pd.DataFrame(data = None, columns = ['Timestamp', 'cid', 'xcat', 'Z-Score'])
+    df_output = pd.DataFrame(data = None, columns = ['Timestamp', 'cid', 'xcat', 'Z-Score'])
         
-        df_temp = dfd[dfd['xcat'] == xcat]
-        df_pivot = df_temp.pivot(index = 'real_date', columns = 'cid', values = 'value')
-        arr = df_pivot[cids].values
-        dates = df_pivot.index.to_numpy()
+    df_temp = dfd[dfd['xcat'] == xcat]
+    df_pivot = df_temp.pivot(index = 'real_date', columns = 'cid', values = 'value')
+    arr = df_pivot[cids].values
+    dates = df_pivot.index.to_numpy()
     
-        data = np.nan_to_num(arr, copy = True)
-        act_countr = (data != 0.0).sum(axis = 1)
-        index = np.argmax(act_countr == len(df_pivot.columns))
-        columns_ = ordering(df_pivot.iloc[index], arr[index, :])
-        columns_ax = columns_[:, np.newaxis]
+    data = np.nan_to_num(arr, copy = True)
+    act_countr = (data != 0.0).sum(axis = 1)
+    index = np.argmax(act_countr == len(df_pivot.columns))
+    columns_ = ordering(df_pivot.iloc[index], arr[index, :])
+    columns_ax = columns_[:, np.newaxis]
     
-        if not np.all(act_countr > 1):
-            index_ = np.where(act_countr != 1)[0]
-            data = np.take(data, index_, axis = 0)
-            arr = np.take(arr, index_, axis = 0)
-            act_countr = np.take(act_countr, index_)
-            dates = np.take(dates, index_)
+    if not np.all(act_countr > 1):
+        index_ = np.where(act_countr != 1)[0]
+        data = np.take(data, index_, axis = 0)
+        arr = np.take(arr, index_, axis = 0)
+        act_countr = np.take(act_countr, index_)
+        dates = np.take(dates, index_)
 
-        l_dates = len(dates)
-        cids_arr = np.repeat(columns_ax, l_dates, axis = 1)
-        cids_arr = cids_arr.reshape((l_dates * len(columns_), ))
-    
-        mean_ = np.divide(np.sum(data, axis = 1), act_countr)
-        func = standard_dev(mean_)
-        std = np.apply_along_axis(func, axis = 1, arr = data)
-        
+    l_dates = len(dates)
+    cids_arr = np.repeat(columns_ax, l_dates, axis = 1)
+    cids_arr = cids_arr.reshape((l_dates * len(columns_), ))
+
+    mean_ = np.divide(np.sum(data, axis = 1), act_countr)
+    func = standard_dev(mean_)
+    std = np.apply_along_axis(func, axis = 1, arr = data)
+
+    if neutral == 'mean':
         z_data = z_formula(arr, mean_, std)
-        dates = dimension(dates, len(df_pivot.columns))
-        dates = dates.ravel()
-        z_dimension = z_data.shape[0] * z_data.shape[1]
-        z_data = z_data.T
-        z_data = z_data.reshape((z_dimension))
+    elif neutral == 'median':
+        median = np.nanmedian(arr, axis = 1)
+        z_data = z_formula(arr, median, std)
+    else:
+        zero = np.zeros((l_dates, ))
+        z_data = z_formula(arr, zero, std)
+            
+    dates = dimension(dates, len(df_pivot.columns))
+    dates = dates.ravel()
+    z_dimension = z_data.shape[0] * z_data.shape[1]
+    z_data = z_data.T
+    z_data = z_data.reshape((z_dimension))
 
-        df_output['Timestamp'] = dates
-        df_output['cid'] = cids_arr
-        df_output['xcat'] = xcat
-        df_output['Z-Score'] = z_data
+    df_output['Timestamp'] = dates
+    df_output['cid'] = cids_arr
+    df_output['xcat'] = xcat
+    df_output['z_score'] = z_data
 
-        list_.append(df_output)
-
-    result = pd.concat(list_).reset_index()
-    return result
+    return df_output
             
 
-def out_sample(dfd: pd.DataFrame, cids: List[str] = None, xcats: List[str] = None,
-               min_obs: int = 252, neutral: str = 'zero'):
-
-    assert neutral in ['median', 'zero', 'mean']
+def out_sample(dfd: pd.DataFrame, cids: List[str] = None, min_obs: int = 252,
+               neutral: str = 'zero', xcat: str = None):
 
     cid_xcat_cl = {}
     cid_z = {}
-    
-    df_inst = dfManipulation(dfd, cids, xcats)
+
+    df_inst = dfManipulation(dfd, cids, xcat)
     df_inst.fields_iter('cids', 'xcats')
     cid_xcat_cl = df_inst.cid_xcat
     d_xcat = df_inst.d_xcat
@@ -285,8 +274,7 @@ def out_sample(dfd: pd.DataFrame, cids: List[str] = None, xcats: List[str] = Non
     
     for cid in cids:
         df_out = pd.DataFrame(columns = qdf_cols)
-        df_out['xcat'] = np.concatenate(mult(xcats,
-                                             d_xcat[cid]))
+        df_out['xcat'] = np.concatenate(mult(xcat, d_xcat[cid]))
         list_ = tuple(map(merge, cid_xcat_cl[cid]))
         df_out['real_date'] = np.concatenate(list_)
         df_out[['value', stat]] = np.concatenate(cid_z[cid])
@@ -299,20 +287,40 @@ def out_sample(dfd: pd.DataFrame, cids: List[str] = None, xcats: List[str] = Non
 
 def zn_score(dfd: pd.DataFrame, cids: List[str] = None, xcats: List[str] = None,
              oos: bool = False, min_obs: int = 252, cross: bool = False,
-             neutral: str = 'zero', abs_max: float = None):
+             neutral: str = 'zero', abs_max: float = None, weighting: bool = False,
+             c_weight: float = 0.5, xcat: str = None):
+    
+    assert xcat in xcats
+    assert neutral in ['median', 'zero', 'mean']
+    
+    if weighting:
+        assert c_weight < 1.0 and c_weight > 0.0
 
-    if oos:
-        final_df = out_sample(dfd, cids = cids,
-                              xcats = xcats, neutral = neutral)
-        print(final_df)
+    if not weighting:
+        if oos:
+            xcat = [xcat]
+            final_df = out_sample(dfd, cids = cids,
+                                  neutral = neutral, xcat = xcat)
+        else:
+            final_df = cross_section(dfd, cids = cids,
+                                     neutral = neutral, xcat = xcat)
+    else:
+        final_df = pd.DataFrame(data = None, columns =
+                                ['Timestamp', 'cid', 'xcat', 'z_score_time', 'z_cross', 'Weighted'])
+        xcat_ = [xcat]
+        time_df = out_sample(dfd, cids = cids, neutral = neutral, xcat = xcat_)
+        cross_df = cross_section(dfd, cids = cids, neutral = neutral, xcat = xcat)
+
+        final_df['Timestamp'] = time_df['real_date']
+        final_df['cid'] = time_df['cid']
+        final_df['xcat'] = xcat
+        final_df['z_score_time'] = time_df['value']
+        final_df['z_cross'] = cross_df['z_score']
+        weight = (cross_df['z_score'] * c_weight) + ((1 - c_weight) * time_df['value'])
+        final_df['Weighted'] = weight
         
-    start = time.time()    
-    if cross:
-        final_df = cross_section(dfd, cids = cids,
-                                 xcats = xcats, neutral = neutral)
-    print(f"Time Elapsed, cross-section: {time.time() - start}.")
-
     return final_df
+
         
 
 if __name__ == "__main__":
@@ -335,12 +343,15 @@ if __name__ == "__main__":
     df_xcats.loc['GROWTH'] = ['2012-01-01', '2020-10-30', 1, 2, 0.9, 1]
     df_xcats.loc['INFL'] = ['2013-01-01', '2020-10-30', 1, 2, 0.8, 0.5]
     
-    ## Numpy Array.
+    print("Uses Ralph's make_qdf() function.")
     start = time.time()
-    print("Using Ralph's make_qdf() function.")
     dfd = make_qdf(df_cids, df_xcats, back_ar = 0.75)
     print(f"Time Elapsed: {time.time() - start}.")
 
-    ## df = zn_score(dfd, cids = fields_cids, xcats = fields_cats, oos = True, neutral = 'median')
+    ## df = zn_score(dfd, cids = fields_cids, xcats = fields_cats, oos = True, neutral = 'median', xcat = 'CRY')
 
-    df = zn_score(dfd, cids = cids, xcats = xcats, oos = False, cross = True, neutral = 'median')
+    start = time.time()
+    df = zn_score(dfd, cids = cids, xcats = xcats, oos = False, cross = True,
+                  neutral = 'median', weighting = True, c_weight = 0.85, xcat = 'CRY')
+    print(f"Time Elapsed: {time.time() - start}.")
+    print(df)
