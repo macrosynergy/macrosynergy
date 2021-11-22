bd1c51d8edcb6a9f7776845164e637684126f505
Ksteeds <csteeds@macrosynergy.com>
2021-11-22 15:29:01 +0000

    Documentation.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,6 +17,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../../../'))
 sys.path.insert(0, os.path.abspath('../../../macrosynergy'))
+sys.path.insert(0, os.path.abspath('../../../latest'))
 from setup import VERSION, ISRELEASED
 import macrosynergy
 
