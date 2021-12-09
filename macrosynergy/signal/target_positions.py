be536f7f7b45212316a37df9bb8ac597000ce744
Ksteeds <csteeds@macrosynergy.com>
2021-12-09 17:14:54 +0000

    Commencing algorithm.

--- macrosynergy/signal/target_positions.py
+++ macrosynergy/signal/target_positions.py
@@ -1,12 +1,14 @@
 import numpy as np
 import pandas as pd
-from typing import List, Union, Tuple
+from typing import List
+from macrosynergy.panel.make_zn_scores import *
+from macrosynergy.management.shape_dfs import reduce_df_by_ticker
 
 
 def target_positions(df: pd.DataFrame, contracts: List[str],
-                     cat: str, ret: str = 'XR_NSA', blacklist: dict = None,
+                     xcat: str, ret: str = 'XR_NSA', blacklist: dict = None,
                      start: str = None, end: str = None,
-                     scale: str = 'zn', vtarg: float = 0.01,
+                     scale: str = 'prop', vtarg: float = 0.01,
                      lback_periods: int = 21, lback_meth: str = 'ma', half_life=11,
                      signame: str = 'POS'
                      ):
@@ -20,7 +22,7 @@ def target_positions(df: pd.DataFrame, contracts: List[str],
         categories) for which target positions are to be generated.
     :param <str> sig: category postfix that is appended to the contracts in order to
         obtain the tickers that serve as signals.
-        For example: 'SIG' is appended to 'EUR_FX' to give the ticker 'EUR_FXSIG'
+        For example: 'SIG' is appended to 'EUR_FX' to give the ticker 'EUR_FXSIG'.
     :param <str> ret: return category postfix; default is "XR_NSA".
         The returns are necessary for volatility target-based signals.
     :param <dict> blacklist: cross sectional date ranges that should have zero target
@@ -40,9 +42,9 @@ def target_positions(df: pd.DataFrame, contracts: List[str],
         [2] Method 'dig' means 'digital' and sets the individual position to either USD1
             long or short, depending on the sign of the signal.
         [2] Method 'vt' means vol-targeting and implies that the individual position
-        is set such that it meets a specific vol target based on recent historic
-        volatility. The target as annualized SD in USD in the argument `vtarg`.
-        This method supports a form of simple risk parity.
+            is set such that it meets a specific vol target based on recent historic
+            volatility. The target as annualized SD in USD in the argument `vtarg`.
+            This method supports a form of simple risk parity.
     :param <float> vtarg: This allows volatility targeting on the contract level.
         Default is None, but if a value is chosen then for each contract the
         proportionate or digital position is translated into a position that carries
@@ -60,11 +62,39 @@ def target_positions(df: pd.DataFrame, contracts: List[str],
         in USD, using the columns 'cid', 'xcats', 'real_date' and 'value'.
 
     Note: A signal is still different from a position in two principal ways. First,
-        The position signal can only be implemented with some lag. Second, the actual
-        position of the strategy will be affected by other considerations, such as
-        risk management and assets under management.
+          the position signal can only be implemented with some lag. Second, the actual
+          position of the strategy will be affected by other considerations, such as
+          risk management and assets under management.
     """
 
     # Todo: this function draws heavily on make_zn_scores and historic_vol
+    ticks_ret = [c + ret for c in contracts]
 
-    pass
+    # :param <str> sig: category postfix that is appended to the contracts in order to
+    #  obtain the tickers that serve as signals.
+    #  For example: 'SIG' is appended to 'EUR_FX' to give the ticker 'EUR_FXSIG'.
+    #  Is 'SIG' a contrived postfix ? What is the DQ equivalent ?
+    ticks_sig = [c + 'SIG' for c in contracts]
+
+    # 'EUR_FXSIG'.
+    tickers = ticks_ret + ticks_sig
+    dfd = reduce_df_by_ticker(df, start=start, end=end, ticks=tickers,
+                              blacklist=blacklist)
+
+    # Requires understanding the neutral level to use. Building out some of the below
+    # parameters into the main signature.
+    if scale == 'prop':
+        # Rolling signal: zn-score computed for each of the cross-sections.
+
+        prop_sig = make_zn_scores(dfd, xcat='SIG', sequential=True, cids=cids,
+                                  neutral='mean', pan_weight=0)
+
+    # [2] Method 'dig' means 'digital' and sets the individual position to either USD1
+    #  Long or short, depending on the sign of the signal.
+    elif scale == 'dig':
+        # One for long, 0 for short.
+        value = (dfd['value'] > 0).astype(dtype=np.uint8)
+
+    elif scale == 'vt':
+        pass
+
