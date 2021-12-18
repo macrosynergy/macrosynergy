d89fab5490956c8be6a942b135abe0ebe5a6f102
Ksteeds <csteeds@macrosynergy.com>
2021-12-18 15:35:42 +0000

    Work-in-progress.

--- tests/unit/signal/unit_target_positions.py
+++ tests/unit/signal/unit_target_positions.py
@@ -15,6 +15,7 @@ class TestAll(unittest.TestCase):
         self.__dict__['cids'] = ['AUD', 'GBP', 'NZD', 'USD']
         self.__dict__['xcats'] = ['FXXR_NSA', 'EQXR_NSA']
         self.__dict__['ctypes'] = ['FX', 'EQ']
+        self.__dict__['xcat_sig'] = 'FXXR_NSA'
 
         df_cids = pd.DataFrame(index=self.cids, columns=['earliest', 'latest',
                                                          'mean_add', 'sd_mult'])
@@ -43,6 +44,13 @@ class TestAll(unittest.TestCase):
 
         assert 'dfd' in vars(self).keys(), "Instantiation of DataFrame missing from " \
                                            "field dictionary."
+
+        dfd_reduced = reduce_df(df=self.dfd, xcats=[self.xcat_sig], cids=self.cids,
+                                start='2012-01-01', end='2020-10-30',
+                                blacklist=self.blacklist)
+        self.__dict__['dfd_reduced'] = dfd_reduced
+        self.__dict__['df_pivot'] = dfd_reduced.pivot(index="real_date", columns="cid",
+                                                      values="value")
 
     def test_unitary_positions(self):
         self.dataframe_generator()
@@ -76,15 +84,35 @@ class TestAll(unittest.TestCase):
         self.assertTrue(summation == df_unit_pos.shape[0])
 
         # Reduce the dataframe & check the logic is correct.
-        dfd_reduced = reduce_df(df=self.dfd, xcats=[xcat_sig], cids=self.cids,
-                                start='2012-01-01', end='2020-10-30',
-                                blacklist=self.blacklist)
-
-        condition = np.where(dfd_reduced['value'].to_numpy() < 0)[0]
+        condition = np.where(self.dfd_reduced['value'].to_numpy() < 0)[0]
         first_negative_index = next(iter(condition))
 
         val_column = df_unit_pos['value'].to_numpy()
         self.assertTrue(val_column[first_negative_index] == -1)
+
+        # Little need to test the application of make_zn_scores(), as the function has
+        # its own respective Unit Test but test on the dimensions. If the minimum number
+        # of observations parameter is set to zero, the dimensions of the reduced
+        # dataframe should match the output dataframe.
+        df_unit_pos = unit_positions(df=self.dfd, cids=self.cids, xcat_sig=xcat_sig,
+                                     blacklist=self.blacklist, start='2012-01-01',
+                                     end='2020-10-30', scale='prop', min_obs=0,
+                                     thresh=2.5)
+
+        self.assertTrue(df_unit_pos.shape == self.dfd_reduced.shape)
+
+    def unit_time_series(self):
+
+        self.dataframe_generator()
+        ret = 'XR_NSA'
+        contract_returns = [c + ret for c in self.ctypes]
+
+        # Tractable function - requires few tests. Validate each contract type is held in
+        # the dictionary, and its associated value is a tuple.
+        durations = time_series(self.dfd_reduced, contract_returns)
+
+        self.assertTrue(list(durations.keys()) == contract_returns)
+        self.assertTrue(all([isinstance(v, tuple) for v in durations.values()]))
 
     def test_return_series(self):
 
@@ -92,10 +120,22 @@ class TestAll(unittest.TestCase):
 
         sigrels = [1, -1]
         ret = 'XR_NSA'
-        contract_returns = [c + 'XR_NSA' for c in self.ctypes]
+        contract_returns = [c + ret for c in self.ctypes]
+        xcat_sig = 'FXXR_NSA'
 
-        df_pos_vt = return_series(dfd=self.dfd, contract_returns=contract_returns,
-                                  sigrels=sigrels, ret=ret)
+        dates = self.df_pivot.index
+        dfd = reduce_df(df=self.dfd, xcats=self.xcats, cids=self.cids,
+                        start='2012-01-01', end='2020-10-30',
+                        blacklist=self.blacklist)
+
+        df_pos_vt = return_series(dfd=dfd, xcat_sig=xcat_sig,
+                                  contract_returns=contract_returns, sigrels=sigrels,
+                                  time_index=dates, cids=self.cids, ret=ret)
+
+        # First aspect to validate is that the return series, used for volatility
+        # adjusting, is defined over the same time-period as the signal.
+        dfd_sig = dfd[dfd['xcat'] == xcat_sig]
+        self.assertTrue(df_pos_vt.shape == dfd_sig.shape)
 
     def test_target_positions(self):
 
