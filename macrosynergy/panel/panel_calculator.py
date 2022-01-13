8f5325e6407bf7a2f0929aab9c441660ad0ffe29
Ksteeds <csteeds@macrosynergy.com>
2022-01-13 15:54:40 +0000

    Commencing code.

--- macrosynergy/panel/panel_calculator.py
+++ macrosynergy/panel/panel_calculator.py
@@ -3,24 +3,28 @@ import pandas as pd
 from typing import List, Union, Tuple
 from macrosynergy.management.simulate_quantamental_data import make_qdf
 from macrosynergy.management.shape_dfs import reduce_df
+import re
+import random
 
 
 def panel_calculator(df: pd.DataFrame, calcs: List[str], cids: List[str] = None,
-                     start: str = None, end: str = None, blacklist: dict = None):
+                     xcats: List[str] = None, start: str = None, end: str = None,
+                     blacklist: dict = None) -> object:
     """
     Calculates panels based on simple operations in input panels.
 
-    :param <pd.Dataframe> df: standardized data frame with following necessary columns:
+    :param <pd.Dataframe> df: standardized dataframe with following necessary columns:
         'cid', 'xcat', 'real_date' and 'value'.
     :param <List[str]> calcs:  set of calculations in string format with category names
         as arguments.
     :param <List[str]> cids: cross sections for which the new panels are calculated.
+    :param <List[str]> xcats: the categories the panel calculator is applied to.
     :param <str> start: earliest date in ISO format. Default is None and earliest date in
         df is used.
     :param <str> end: latest date in ISO format. Default is None and latest date in df is
         used.
     :param <dict> blacklist: cross sections with date ranges that should be excluded from
-        the data frame. If one cross section has several blacklist periods append numbers
+        the dataframe. If one cross section has several blacklist periods append numbers
         to the cross-section code.
 
     :return <pd.Dataframe>: standardized dataframe with all new categories in standard
@@ -28,16 +32,59 @@ def panel_calculator(df: pd.DataFrame, calcs: List[str], cids: List[str] = None,
 
     Notes:
     Panel calculation strings can use functions and unary mathematical operators
-    in accordance with numpy convention to indicate the desired calculation, such as
-    "NEWCAT = OLDCAT1 + 0.5 * OLDCAT2"
-    "NEWCAT = np.log(OLDCAT1) - np.abs(OLDCAT2) ** 1/2"
+    in accordance with numpy convention to indicate the desired calculation such as:
+        "NEWCAT = OLDCAT1 + 0.5 * OLDCAT2"
+        "NEWCAT = np.log(OLDCAT1) - np.abs(OLDCAT2) ** 1/2"
     Panel calculation can also involve individual indicator series (to be applied
     to all series in the panel by using the @ as prefix, such as:
-    "NEWCAT = OLDCAT1 - np.sqrt(@USD_OLDCAT2)"
+        "NEWCAT = OLDCAT1 - np.sqrt(@USD_OLDCAT2)"
     If more than one new category is calculated, the resulting panels can be used
     sequentially in the calculations, such as:
-    ["NEWCAT1 = 1 + OLDCAT1/100", "NEWCAT2 = OLDCAT2 * NEWCAT1]
+        ["NEWCAT1 = 1 + OLDCAT1/100", "NEWCAT2 = OLDCAT2 * NEWCAT1]
 
     """
+    assert isinstance(xcats, list), f"List of categories expected, and not object type:" \
+                                    " {type(xcats)}."
+    assert isinstance(cids, list), f"Cross-sections passed must be held in a List."
+    assert isinstance(calcs, list), "List of functions expected."
+    assert all([isinstance(elem, str) for elem in calcs]), "Elements must be strings."
 
-    pass
+    dfx = reduce_df(df, xcats=xcats, cids=cids, start=start,
+                    end=end, blacklist=blacklist)
+
+    return dfx
+
+
+if __name__ == "__main__":
+
+    cids = ['AUD', 'CAD', 'GBP', 'USD', 'NZD']
+    xcats = ['XR', 'CRY', 'GROWTH', 'INFL']
+
+    df_cids = pd.DataFrame(index = cids, columns = ['earliest', 'latest', 'mean_add',
+                                                    'sd_mult'])
+
+    df_cids.loc['AUD'] = ['2010-01-01', '2020-12-31', 0.5, 2]
+    df_cids.loc['CAD'] = ['2011-01-01', '2020-11-30', 0, 1]
+    df_cids.loc['GBP'] = ['2012-01-01', '2020-11-30', -0.2, 0.5]
+    df_cids.loc['USD'] = ['2013-01-01', '2020-09-30', -0.2, 0.5]
+    df_cids.loc['NZD'] = ['2002-01-01', '2020-09-30', -0.1, 2]
+
+    df_xcats = pd.DataFrame(index = xcats, columns = ['earliest', 'latest', 'mean_add',
+                                                      'sd_mult', 'ar_coef', 'back_coef'])
+    df_xcats.loc['XR'] = ['2010-01-01', '2020-12-31', 0, 1, 0, 0.3]
+    df_xcats.loc['CRY'] = ['2011-01-01', '2020-10-30', 1, 2, 0.9, 0.5]
+    df_xcats.loc['GROWTH'] = ['2012-01-01', '2020-10-30', 1, 2, 0.9, 1]
+    df_xcats.loc['INFL'] = ['2013-01-01', '2020-10-30', 1, 2, 0.8, 0.5]
+
+    random.seed(2)
+    dfd = make_qdf(df_cids, df_xcats, back_ar=0.75)
+
+    black = {'AUD': ['2000-01-01', '2003-12-31'], 'GBP': ['2018-01-01', '2100-01-01']}
+
+    start = '2010-01-01'
+    end = '2020-12-31'
+
+    filt1 = (dfd['xcat'] == 'XR')
+    dfdx = dfd[filt1]
+    df_calc = panel_calculator(df=dfdx, calcs=["XR + 0.5"], cids=cids, xcats=['XR'],
+                               start=start, end=end, blacklist=black)
