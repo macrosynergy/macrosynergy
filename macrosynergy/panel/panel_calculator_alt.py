618e1212e157a118c8ec1d8e0303710b6f7f8446
Ksteeds <csteeds@macrosynergy.com>
2022-01-24 14:06:32 +0000

    Code development. DataFrame alignment and single cross-section handler.

--- macrosynergy/panel/panel_calculator_alt.py
+++ macrosynergy/panel/panel_calculator_alt.py
@@ -5,6 +5,218 @@ from macrosynergy.management.simulate_quantamental_data import make_qdf
 from macrosynergy.management.shape_dfs import reduce_df
 import re
 import random
+from collections import OrderedDict
+
+
+def symbol_finder(expression: str, index: int = 0):
+    """
+    Method used to understand if the category involved in the expression only concerns a
+    single cross-section.
+
+    :param <str> expression: associated formula.
+    :param <int> index: variable used to track the index of the expression.
+
+    :return <List[int]> list of indices where the "@" symbol occurs.
+    """
+
+    indices = []
+    adjustment = 0
+
+    while index != -1:
+
+        index = expression.find("@")
+        if index != -1:
+            indices.append(index + adjustment)
+            adjustment = (index + 1) + adjustment
+
+            index += 1
+            expression = expression[index:]
+
+    return indices
+
+def iterator_func(expression: str, symbol: str, index: int):
+    """
+    Indefinite iterator until a specific symbol is found. Will return the index of the
+    respective symbol.
+
+    :param <str> expression:
+    :param <str> symbol:
+    :param <int> index: starting index of the iterator.
+
+    :return <int>: index of the associated symbol.
+    """
+
+    elem = expression[index]
+    iterator = index
+    while elem != symbol:
+        iterator += 1
+        elem = expression[iterator]
+
+    return iterator
+
+def formula_reconstruction(formula: str, indices: List[int]):
+    """
+    Reconstruct the formula to handle single cross-sections. The purpose is to return the
+    formula but with the removal of the single cross-section declaration and instead
+    leave the xcat. For instance, "@USD_GROWTH" will be reduced to "GROWTH". The
+    originally referenced cross-section will be stored in a separate data structure and
+    incorporated in the formula in a different capacity.
+
+    :param <str> formula: "((@USD_GROWTH - np.sqrt( @USD_INFL )) / @USD_XR )"
+    :param <List[int]> indices: list of indices where the "@" symbol occurs in the
+        expression.
+
+    :return <str>: returns the updated formula.
+    """
+
+    cid_tracker = {}
+    xcat_tracker = {}
+
+    for index in indices:
+
+        iterator = iterator_func(formula, "_", index)
+        cid_tracker[index] = formula[(index + 1): iterator]
+
+        start = iterator
+        end = iterator_func(formula, " ", index=iterator)
+
+        xcat_tracker[(start + 1)] = formula[(start + 1): end]
+
+    reconstruction = tuple(zip(cid_tracker.keys(), xcat_tracker.keys()))
+    adjustment = 0
+    for tup in reconstruction:
+
+        xcat_start = tup[1] - adjustment
+        cid_start = tup[0] - adjustment
+        adjustment += (xcat_start - cid_start)
+
+        formula = formula[:cid_start] + formula[xcat_start:]
+
+    return formula, cid_tracker
+
+def formula_handler(calcs: List[str]):
+    """
+    Separate the functions, contained in the list, y = f(x), on the equality sign. The
+    codomain will be the key and the function will be the value.
+
+    :param <List[str]> calcs:
+
+    :return <dict>: dictionary hosting the function.
+    """
+
+    ops = {}
+    expression_cid = {}
+    for i, calc in enumerate(calcs):
+
+        calc_parts = calc.split('=', maxsplit=1)
+        value = calc_parts[1].strip()
+        indices = symbol_finder(expression=value, index=0)
+
+        if indices:
+            value, cid_dict = formula_reconstruction(formula=value, indices=indices)
+            expression_cid[i] = cid_dict
+
+        ops[calc_parts[0].strip()] = value
+
+    return ops, expression_cid
+
+def involved_xcats(ops: dict):
+    """
+    Function used to understand the original categories involved in the panel
+    calculations. To isolate the involved categories in the expression, spaces are
+    required either side of the category. For instance, NEWCAT1 = np.abs( XR ).
+    Further, each category will exclusively involve capital letters which aids
+    determining the categories in the string expression.
+
+    :param <dict> ops: dictionary containing the panel calculation where the key is the
+        newly formed category and the value the calculation.
+    """
+
+    xcats_used = []
+
+    new_xcats = list(ops.keys())
+    for op in ops.values():
+        op_list = op.split(' ')
+        xcats_used += [x for x in op_list if re.match('^[A-Z]', x)
+                       and x not in new_xcats]
+
+    return set(xcats_used)
+
+def pandas_alignment(dates_dict: dict, expression: str):
+    """
+    Function designed to return the latest start date and earliest end date across the
+    categories involved in the expression. Reduce all dataframes to be defined over the
+    same time-period.
+
+    :param <dict> dates_dict:
+    :param <str> expression:
+
+    return <pd.Timestamp>:
+    """
+
+    cats_indices = {}
+    for k in dates_dict.keys():
+
+        pattern = re.compile(k)
+        indices = pattern.finditer(expression)
+        result = [match.span() for match in indices]
+
+        if result:
+            cats_indices[k] = result
+
+    s_date = pd.Timestamp.min
+    e_date = pd.Timestamp.max
+
+    for k in cats_indices.keys():
+        if dates_dict[k][0] > s_date:
+            s_date = dates_dict[k][0]
+        if dates_dict[k][1] < e_date:
+            e_date = dates_dict[k][1]
+
+    return s_date, e_date, cats_indices
+
+def cross_section_append(index_cid: dict, expression: str, dates_dict: dict):
+    """
+    Subroutine designed to modify the formula to account for the presence of single
+    cross-sections on certain categories. For instance, np.sqrt(@USD_OLDCAT2).
+    Further, will align the involved dataframes which is required due to the conversion
+    to a np.ndarray.
+
+    :param <dict> index_cid: the dictionary's key will be the concerning category's
+        starting index and the value will be the relevant cross-section.
+    :param <str> expression:
+    :param <dict> dates_dict: dictionary consisting of each category and their respective
+        start and end date across the panel series.
+
+    :return <str> expression: updated formula.
+    """
+    global s_date, e_date
+
+    index_cid = OrderedDict(sorted(index_cid.items()))
+
+    add_length = 0
+    for k, v in index_cid.items():
+
+        end_cat = iterator_func(expression, symbol=" ", index=(k + add_length))
+
+        addition = "['" + v + "'].to_numpy()[:, np.newaxis]"
+        expression = expression[:end_cat] + addition + expression[end_cat:]
+        add_length = len(addition)
+
+    s_date, e_date, cats_indices = pandas_alignment(dates_dict, expression)
+    cats_indices = OrderedDict(sorted(cats_indices.items()))
+
+    trunc = ".loc[s_date:e_date]"
+    length = 0
+    counter = 0
+    for k, v in cats_indices.items():
+        for tup in v:
+            index = tup[1] + (length * counter)
+            expression = expression[:index] + trunc + expression[index:]
+            counter += 1
+            length = len(trunc) * counter
+
+    return expression
 
 
 def panel_calculator(df: pd.DataFrame, calcs: List[str] = None, cids: List[str] = None,
@@ -36,72 +248,66 @@ def panel_calculator(df: pd.DataFrame, calcs: List[str] = None, cids: List[str] 
     category panels, whereby the category is indicated by capital letters, underscores
     and numbers.
     Calculated category and panel operations must be separated by '='.
-    "NEWCAT = (OLDCAT1 + 0.5) * OLDCAT2"
-    "NEWCAT = np.log(OLDCAT1) - np.abs(OLDCAT2) ** 1/2"
+        "NEWCAT = (OLDCAT1 + 0.5) * OLDCAT2"
+        "NEWCAT = np.log(OLDCAT1) - np.abs(OLDCAT2) ** 1/2"
     Panel calculation can also involve individual indicator series (to be applied
-    to all series in the panel by using the @ as prefix), such as:
-    "NEWCAT = OLDCAT1 - np.sqrt(@USD_OLDCAT2)"
+    to all series in the corresponding panel by using the @ as prefix), such as:
+        "NEWCAT = OLDCAT1 - np.sqrt(@USD_OLDCAT2)"
     If more than one new category is calculated, the resulting panels can be used
     sequentially in the calculations, such as:
     ["NEWCAT1 = 1 + OLDCAT1/100", "NEWCAT2 = OLDCAT2 * NEWCAT1"]
 
     """
 
-    # A. Asserts
-
     cols = ['cid', 'xcat', 'real_date', 'value']
     assert set(cols).issubset(set(df.columns))
     assert isinstance(calcs, list), "List of functions expected."
-    assert all([isinstance(elem, str) for elem in calcs]),\
+    assert all([isinstance(elem, str) for elem in calcs]), \
         "Each formula in the panel calculation list must be a string."
     assert isinstance(cids, list), "List of cross-sections expected."
 
-    # B. Collect new category names and their formulas
+    ops, expression_cid = formula_handler(calcs)
 
-    ops = {}
-    for calc in calcs:
-        calc_parts = calc.split('=', maxsplit=1)
-        ops[calc_parts[0].strip()] = calc_parts[1].strip()
+    if not expression_cid:
+        del expression_cid
 
-    # C. Check if all required categories are in the dataframe
+    old_xcats_used = involved_xcats(ops=ops)
 
-    xcats_used = []
-    for op in ops.values():
-        op_list = op.split(' ')
-        xcats_used += [x for x in op_list if re.match('^[A-Z]', x)]
+    available_xcats = set(df['xcat'].unique())
+    missing = sorted(old_xcats_used - available_xcats)
+    assert len(missing) == 0, f"Missing categories: {missing}"
 
-    old_xcats_used = list(set(xcats_used) - set([x for x in ops.keys()]))
-    missing = sorted(set(old_xcats_used) - set(df['xcat'].unique()))
-    assert len(missing) == 0, f"Missing categories: {missing}."
+    # Reduce the dataframe to the cross-sections available in all categories.
+    dfx = reduce_df(df, xcats=list(old_xcats_used), cids=cids, start=start,
+                    end=end, blacklist=blacklist, intersect=True)
 
-    # D. Reduce dataframe with intersection requirement
-
-    dfx = reduce_df(df, xcats=old_xcats_used, cids=cids,
-                    start=start, end=end, blacklist=blacklist,
-                    intersect=True)
-
-    # E. Create all required wide dataframes with category names
-
+    dates_xcat = {}
     for xcat in old_xcats_used:
         dfxx = dfx[dfx['xcat'] == xcat]
         dfw = dfxx.pivot(index='real_date', columns='cid', values='value')
-        exec(f'{xcat} =dfw')
-        # Todo: check if alignment of indices is necessary and implement if so
-        # Todo: add individual cross section option
+        dates_xcat[xcat] = (dfw.index[0], dfw.index[-1])
 
-    # F. Calculate the panels and collect
+        exec(f"{xcat} = dfw")
 
+    output_df = []
+    index = 0
     for new_xcat, formula in ops.items():
+
+        if index in expression_cid.keys():
+            formula = cross_section_append(index_cid=expression_cid[index],
+                                           expression=formula, dates_dict=dates_xcat)
+
         dfw_add = eval(formula)
         df_add = pd.melt(dfw_add.reset_index(), id_vars=['real_date'])
         df_add['xcat'] = new_xcat
-        if new_xcat == list(ops.keys())[0]:
-            df_out = df_add[cols]
-        else:
-            df_out = df_out.append(df_add[cols])
-        exec(f'{new_xcat} = dfw_add')  # we main need a df for subsequent calculations
+        output_df.append(df_add)
+        exec(f'{new_xcat} = dfw_add')
+        index += 1
 
-    return df_out
+    df_calc = pd.concat(output_df)[cols]
+    df_calc.reset_index(drop=True)
+
+    return df_calc
 
 
 if __name__ == "__main__":
@@ -133,14 +339,13 @@ if __name__ == "__main__":
     start = '2010-01-01'
     end = '2020-12-31'
 
-    filt1 = (dfd['xcat'] == 'XR') | (dfd['xcat'] == 'CRY')
-    dfdx = dfd[filt1]
-
     # Start of the testing. Various testcases included to understand the capabilities of
     # the designed function.
-    df_calc = panel_calculator(df=dfdx,
-                               calcs=["NEW1 = np.abs( XR ) + 0.552 + 2 * CRY",
-                                      "NEW2 = NEW1 / XR"],
-                               cids=cids, start=start, end=end)
 
-    df_calc.head()
+    formula_3 = "NEW3 = GROWTH - np.square( @USD_INFL )"
+    formulas = ["NEW1 = np.abs( XR ) + 0.552 + 2 * CRY", "NEW2 = NEW1 * 2", formula_3]
+    df_calc = panel_calculator(df=dfd, calcs=formulas, cids=cids, start=start, end=end)
+
+    # Secondary testcase.
+    formula = "NEW1 = ((GROWTH - np.square( @USD_INFL )) / @USD_INFL )"
+    formulas = [formula]
