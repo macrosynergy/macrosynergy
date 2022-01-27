754b3434016fd133ea01f1cf54e7623f517956f8
Ksteeds <csteeds@macrosynergy.com>
2022-01-27 10:16:26 +0000

    Code development.

--- tests/unit/panel/test_panel_calculator.py
+++ tests/unit/panel/test_panel_calculator.py
@@ -5,11 +5,12 @@ import pandas as pd
 from tests.simulate import make_qdf
 from macrosynergy.panel.panel_calculator_alt2 import panel_calculator
 from random import randint, choice
+from typing import List
 
 class TestAll(unittest.TestCase):
 
     def dataframe_generator(self):
-        self.__dict__['cids'] = ['AUD', 'CAD', 'GBP', 'NZD']
+        self.__dict__['cids'] = ['AUD', 'CAD', 'GBP', 'NZD', 'USD']
         self.__dict__['xcats'] = ['XR', 'CRY', 'GROWTH', 'INFL']
         df_cids = pd.DataFrame(index=self.cids,
                                columns=['earliest', 'latest', 'mean_add', 'sd_mult'])
@@ -18,6 +19,7 @@ class TestAll(unittest.TestCase):
         df_cids.loc['CAD'] = ['2001-01-01', '2020-11-30', 0, 1]
         df_cids.loc['GBP'] = ['2002-01-01', '2020-11-30', 0, 2]
         df_cids.loc['NZD'] = ['2002-01-01', '2020-09-30', -0.1, 2]
+        df_cids.loc['USD'] = ['2001-01-01', '2020-10-30', 0.2, 2]
 
         df_xcats = pd.DataFrame(index=self.xcats,
                                 columns=['earliest', 'latest', 'mean_add', 'sd_mult',
@@ -37,6 +39,28 @@ class TestAll(unittest.TestCase):
         self.__dict__['blacklist'] = black
         self.__dict__['start'] = '2010-01-01'
         self.__dict__['end'] = '2020-12-31'
+
+    @staticmethod
+    def dataframe_pivot(df_calc: pd.DataFrame, xcat: str):
+
+        filt = (df_calc['xcat'] == xcat)
+        df_new = df_calc[filt].pivot(index='real_date', columns='cid', values='value')
+
+        return df_new
+
+    @staticmethod
+    def row_value(filt_df: pd.DataFrame, date: pd.Timestamp, cid: str, xcats: List[str]):
+
+        values = []
+
+        input_values = filt_df[filt_df['real_date'] == date]
+        input_values = input_values[input_values['cid'] == cid]
+        for cat in xcats:
+            val = input_values[input_values['xcat'] == cat]['value']
+            val = np.nan_to_num(val)
+            values.append(val[0])
+
+        return values
 
     def test_panel_calculator(self):
 
@@ -66,8 +90,7 @@ class TestAll(unittest.TestCase):
         # defined over the same time-period but pd.DataFrames will match on the index.
         first_date = np.min(date_column)
         end_date = np.max(date_column)
-        filt_2 = (df_calc['xcat'] == "NEW1")
-        df_new1 = df_calc[filt_2].pivot(index='real_date', columns='cid', values='value')
+        df_new1 = self.dataframe_pivot(df_calc, "NEW1")
         date_range = list(df_new1.index)
 
         self.assertTrue(first_date == date_range[0])
@@ -76,15 +99,21 @@ class TestAll(unittest.TestCase):
         dates = list(self.dfd['real_date'])
         date = choice(dates)
         # Test on Australia.
-        row_value = df_new1.loc[date]['AUD']
-
-        input_values = filt_df[filt_df['real_date'] == date]
-        input_values = input_values[input_values['cid'] == 'AUD']
-        xr = input_values[input_values['xcat'] == 'XR']['value']
-        cry = input_values[input_values['xcat'] == 'CRY']['value']
+        cross_section = 'AUD'
+        row_value = df_new1.loc[date][cross_section]
+        xr, cry = self.row_value(filt_df, date, cross_section, ['XR', 'CRY'])
 
         # Manually produce: "NEW1 = np.abs( XR ) + 0.52 + 2 * CRY".
         self.assertTrue(row_value == (np.abs(float(xr)) + 0.52 + 2 * float(cry)))
+
+        # Check NEW2: "NEW2 = NEW1 / XR".
+        cross_section = 'USD'
+        df_new2 = self.dataframe_pivot(df_calc, "NEW2")
+        row_value = df_new2.loc[date][cross_section]
+        xr = self.row_value(filt_df, date, cross_section, ['XR'])
+        new1_val = df_new1.loc[date][cross_section]
+
+        self.assertTrue(row_value == float(new1_val) / xr[0])
 
 
 if __name__ == '__main__':
