3e7c431f726eb1717f9bb8c9179fda2f8d14aac1
Ksteeds <csteeds@macrosynergy.com>
2022-03-17 19:24:41 +0000

    Unit Test complete.

--- tests/unit/management/test_update_df.py
+++ tests/unit/management/test_update_df.py
@@ -47,7 +47,7 @@ class TestAll(unittest.TestCase):
         # Test the assertion that both dataframes must be in the standardised form.
         with self.assertRaises(AssertionError):
             dfd_1_rv_growth = dfd_1_rv[dfd_1_rv['xcat'] == 'GROWTHRV']
-            dfd_pivot = dfd_1_rv_growth.pivot(index="real_date", columns="ticker",
+            dfd_pivot = dfd_1_rv_growth.pivot(index="real_date", columns="cid",
                                               values="value")
             dfd_add = category_add(dfd=dfd, dfd_add=dfd_pivot)
 
@@ -57,7 +57,58 @@ class TestAll(unittest.TestCase):
                                        rel_xcats=None,
                                        postfix='RV')
 
-        dfd_add = category_add(dfd=self.dfd, dfd_add=dfd_1_rv)
+        # Both categories generated from the make_relative_value() function will not be
+        # present in the aggregated dataframe. Therefore, confirm both categories are
+        # appended to the existing dataframe.
+        dfd_add = category_add(dfd=dfd, dfd_add=dfd_1_rv)
+        # First, confirm the expected dimensions.
+        self.assertTrue(dfd_add.shape[0] == (dfd.shape[0] + dfd_1_rv.shape[0]))
+
+        # Confirm the appended dataframe's categories are a subset of the combined
+        # dataframe.
+        new_categories = set(dfd_1_rv['xcat'])
+        self.assertTrue(new_categories.issubset(set(dfd_1_rv['xcat'])))
+
+        dfd_1_rv_growth = dfd_1_rv[dfd_1_rv['xcat'] == 'GROWTHRV']
+        dfd_1_rv_growth_aud = dfd_1_rv_growth[dfd_1_rv_growth['cid'] == 'AUD']
+        # Choose a random date.
+        date = list(dfd_pivot.index)[1000]
+
+        value = dfd_1_rv_growth_aud[dfd_1_rv_growth_aud['real_date'] == date]['value']
+
+        # Confirm the values do not change during the aggregation mechanism.
+        dfd_add_growth = dfd_add[dfd_add['xcat'] == 'GROWTHRV']
+        dfd_add_aud = dfd_add_growth[dfd_add_growth['cid'] == 'AUD']
+
+        test = dfd_add_aud[dfd_add_aud['real_date'] == date]['value']
+        self.assertTrue(float(test) == float(value))
+
+        # Test the replacement mechanism: categories are already in the dataframe but are
+        # to be replaced by new values.
+        dfd_2_rv_divide = make_relative_value(dfd, xcats=['GROWTH', 'INFL'],
+                                                 cids=None,
+                                                 blacklist=None, rel_meth='divide',
+                                                 rel_xcats=None,
+                                                 postfix='RV')
+
+        dfd_add_2 = category_add(dfd=dfd_add, dfd_add=dfd_2_rv_divide)
+        self.assertTrue(list(set(dfd_add_2['xcat'])) == list(set(dfd_add['xcat'])))
+
+        # Confirm the value does not equal the previous value which will be held in the
+        # original dataframe, "dfd_add".
+        dfd_add_growth_2 = dfd_add_2[dfd_add_2['xcat'] == 'GROWTHRV']
+        dfd_add_aud_2 = dfd_add_growth_2[dfd_add_growth_2['cid'] == 'AUD']
+
+        test_2 = dfd_add_aud_2[dfd_add_aud_2['real_date'] == date]['value']
+        self.assertTrue(float(test_2) != float(value))
+
+        # Hence, confirm the new value stored in "dfd_add_2" is sourced from the latest
+        # dataframe "dfd_1_rv_divide".
+        dfd_2_rv_growth = dfd_2_rv_divide[dfd_2_rv_divide['xcat'] == 'GROWTHRV']
+        dfd_2_rv_growth_aud = dfd_2_rv_growth[dfd_2_rv_growth['cid'] == 'AUD']
+
+        value_2 = dfd_2_rv_growth_aud[dfd_2_rv_growth_aud['real_date'] == date]['value']
+        self.assertTrue(float(test_2) == float(value_2))
 
 
 if __name__ == '__main__':
