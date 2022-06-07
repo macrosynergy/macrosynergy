ca338d2741b241102807c4200a0e1e5ce0bb9359
Ksteeds <csteeds@macrosynergy.com>
2022-06-07 19:18:03 +0100

    Code development.

--- tests/unit/dataquery/test_api.py
+++ tests/unit/dataquery/test_api.py
@@ -1,33 +1,112 @@
-import unittest
-from unittest import mock
-from macrosynergy.dataquery.api import Interface
 
+
+from macrosynergy.dataquery import api
+from macrosynergy.dataquery.auth import OAuth, CertAuth
+from datetime import datetime
+from pandas import Timestamp
+from pandas.tseries.offsets import BDay
+import unittest
+import yaml
 
 class TestDataQueryInterface(unittest.TestCase):
-    def test_check_connection(self):
-        with mock.patch(
-            "macrosynergy.dataquery.auth.OAuth.get_dq_api_result",
-            return_value={"info": {"code": 200}}  # TODO check exact response from DataQuery Swagger docs
-        ) as p_request:
-            with Interface(client_id="client1", client_secret="123", oauth=True) as dq:
-                self.assertTrue(dq.check_connection())
-                p_request.assert_called_with(url=dq.access.base_url + "/services/heartbeat")
 
-            p_request.assert_called_once()
+    @staticmethod
+    def s_date_calc():
+        """
+        Dynamic calculation of the start date.
+        """
+        day = datetime.today().strftime('%Y-%m-%d')
+        day = str(day)
+        day = Timestamp(day)
+        # Subtract a business week from today's date.
+        new_date = day - BDay(5)
 
-    def test_check_connection_fail(self):
-        with mock.patch(
-            "macrosynergy.dataquery.auth.OAuth.get_dq_api_result",
-            # TODO check exact response from DataQuery Swagger docs
-            return_value={
-                "info": {"code": 400, "message": "TODO check message from DQ", "description": "description here."}
-            }
-        ) as p_request:
-            with Interface(client_id="client1", client_secret="123", oauth=True) as dq:
-                self.assertFalse(dq.check_connection())
-                p_request.assert_called_with(url=dq.access.base_url + "/services/heartbeat")
+        return str(new_date.strftime('%Y-%m-%d'))
 
-            p_request.assert_called_once()
+    def constructor(self):
+
+        # Auth Connection.
+        self.__dict__['client_id'] = "e5qY4ZXMUQOZ0tVK"
+        s = "sug9OVlfem54ep7blgbknzc3Xj5aqko4el71pyf09t6mcbdmax8lzZlsKZ5oUvY2j4qQjjhrtxevgMn"
+        self.__dict__['client_secret'] = s
+
+        # Certificate & key connection.
+        with open("../../../config.yml", 'r') as f:
+            cf = yaml.load(f, Loader=yaml.FullLoader)
+            self.cf = cf
+
+        self.__dict__['endpoint'] = "/expressions/time-series"
+        self.__dict__['params'] = {"format": "JSON", "start-date": self.s_date_calc(),
+                                   "end-date": None, "calendar": "CAL_ALLDAYS",
+                                   "frequency": "FREQ_DAY",
+                                   "conversion": "CONV_LASTBUS_ABS",
+                                   "nan_treatment": "NA_NOTHING",
+                                   "data": "NO_REFERENCE_DATA"}
+
+        cids_dmca = ['AUD', 'CAD', 'CHF', 'EUR', 'GBP']
+        xcats = ['EQXR_NSA']
+        metric = ['value']
+        self.__dict__['tickers'] = [cid + '_' + xcat for xcat in xcats
+                                    for cid in cids_dmca]
+        expression = ["DB(JPMAQS," + tick + f",{metric})" for tick in self.tickers]
+        self.__dict__["expression"] = expression
+
+    def test_oauth_condition(self):
+
+        self.constructor()
+
+        # Accessing DataQuery can be achieved via two methods: OAuth or Certificates /
+        # Keys. To handle for the idiosyncrasies of the two access methods, split the
+        # methods across individual Classes. The usage of each Class is controlled by the
+        # parameter "oauth".
+        dq_access = api.Interface(oauth=True,
+                                  client_id=self.client_id,
+                                  client_secret=self.client_secret)
+        self.assertTrue(isinstance(dq_access.access, OAuth))
+
+        # Default is to use the Certificates / Keys: oauth = False.
+        dq_access = api.Interface(username=self.cf["dq"]["username"],
+                                  password=self.cf["dq"]["password"],
+                                  crt="../../../api_macrosynergy_com.crt",
+                                  key="../../../api_macrosynergy_com.key")
+
+        self.assertTrue(isinstance(dq_access.access, CertAuth))
+
+    def test_fetch_threading(self):
+
+        self.constructor()
+
+        # Instantiate a local variable.
+        params = self.params
+        params["expressions"] = self.expression
+
+        dq = api.Interface(username=self.cf["dq"]["username"],
+                           password=self.cf["dq"]["password"],
+                           crt="../../../api_macrosynergy_com.crt",
+                           key="../../../api_macrosynergy_com.key")
+
+        results = dq._fetch_threading(endpoint=self.endpoint,
+                                      params=params)
+
+        # Confirm the included tickers.
+        test_ticker = []
+        date_checker = []
+        for elem in results:
+            jpm_expression = elem["attributes"][0]['expression']
+            f_date = elem["attributes"][0]['time-series'][0][0]
+            f_date = str(Timestamp(f_date).strftime('%Y-%m-%d'))
+            date_checker.append(f_date)
+            test_ticker.append(jpm_expression.split(',')[1])
+
+        self.assertTrue(len(self.tickers) == len(test_ticker))
+
+        test_ticker = sorted(test_ticker)
+        condition = test_ticker == sorted(self.tickers)
+        self.assertTrue(condition)
+
+        # Confirm the application of the start_date parameter.
+        first_date = self.s_date_calc()
+        self.assertTrue(first_date == next(iter(set(date_checker))))
 
 
 if __name__ == '__main__':
