1ed25c93394bbd77dd4aa8a2b06d4e053d8e1313
Ksteeds <csteeds@macrosynergy.com>
2022-07-07 10:23:07 +0100

    Python black alignment.

--- macrosynergy/dataquery/auth.py
+++ macrosynergy/dataquery/auth.py
@@ -7,13 +7,16 @@ from typing import Optional, Dict, Tuple
 from datetime import datetime
 
 CERT_BASE_URL: str = "https://platform.jpmorgan.com/research/dataquery/api/v2"
-OAUTH_BASE_URL: str = "https://api-developer.jpmorgan.com/research/dataquery-authe/api/v2/"
+OAUTH_BASE_URL: str = (
+    "https://api-developer.jpmorgan.com/research/dataquery-authe/api/v2/"
+)
 OAUTH_TOKEN_URL: str = "https://authe.jpmchase.com/as/token.oauth2"
 OAUTH_DQ_RESOURCE_ID: str = "JPMC:URI:RS-06785-DataQueryExternalApi-PROD"
 
 
 def valid_response(r: requests.Response) -> dict:
-    """Prior to requesting any data, the function will confirm if a connection to the
+    """
+    Prior to requesting any data, the function will confirm if a connection to the
     DataQuery API is able to be established given the credentials passed. If the status
     code is 200, able to access DataQuery's API.
     """
@@ -25,8 +28,8 @@ def valid_response(r: requests.Response) -> dict:
     elif r.text[0] != "{":
 
         # Authentication check.
-        condition: str = r.text.split('-')[1].strip().split('<')[0]
-        if condition == 'Authentication Failure':
+        condition: str = r.text.split("-")[1].strip().split("<")[0]
+        if condition == "Authentication Failure":
             raise RuntimeError(
                 condition + " - unable to access DataQuery. Password expired."
             )
@@ -36,10 +39,14 @@ def valid_response(r: requests.Response) -> dict:
     return r.json()
 
 
-def dq_request(url: str, headers: dict = None, params: dict = None,
-               method: str = "get", cert: Optional[Tuple[str, str]] = None,
-               **kwargs
-               ) -> Tuple[dict, str, str]:
+def dq_request(
+    url: str,
+    headers: dict = None,
+    params: dict = None,
+    method: str = "get",
+    cert: Optional[Tuple[str, str]] = None,
+    **kwargs
+) -> Tuple[dict, str, str]:
     """Will return the request from DataQuery.
 
     """
@@ -47,12 +54,12 @@ def dq_request(url: str, headers: dict = None, params: dict = None,
     assert method in ("get", "post"), request_error
 
     with requests.request(
-            method=method,
-            url=url,
-            cert=cert,
-            headers=headers,
-            params=params,
-            **kwargs,
+        method=method,
+        url=url,
+        cert=cert,
+        headers=headers,
+        params=params,
+        **kwargs,
     ) as r:
         text: str = r.text
         last_url: str = r.url
@@ -75,12 +82,14 @@ class CertAuth(object):
 
     """
 
-    def __init__(self,
-                 username: str,
-                 password: str,
-                 crt: str = "api_macrosynergy_com.crt",
-                 key: str = "api_macrosynergy_com.key",
-                 base_url: str = CERT_BASE_URL):
+    def __init__(
+        self,
+        username: str,
+        password: str,
+        crt: str = "api_macrosynergy_com.crt",
+        key: str = "api_macrosynergy_com.key",
+        base_url: str = CERT_BASE_URL
+    ):
 
         error_user = f"username must be a <str> and not <{type(username)}>."
         assert isinstance(username, str), error_user
@@ -89,8 +98,8 @@ class CertAuth(object):
         assert isinstance(password, str), error_password
 
         self.auth: str = base64.b64encode(
-            bytes(f'{username:s}:{password:s}', "utf-8")
-        ).decode('ascii')
+            bytes(f"{username:s}:{password:s}", "utf-8")
+        ).decode("ascii")
 
         self.headers: Dict[str, str] = {"Authorization": f"Basic {self.auth:s}"}
         self.base_url: str = base_url
@@ -133,10 +142,7 @@ class CertAuth(object):
             ticker series.
         """
         js, self.last_response, self.last_url = dq_request(
-            url=url,
-            cert=(self.crt, self.key),
-            headers=self.headers,
-            params=params
+            url=url, cert=(self.crt, self.key), headers=self.headers, params=params
         )
 
         return js
@@ -153,12 +159,12 @@ class OAuth(object):
     """
 
     def __init__(
-            self,
-            client_id: str,
-            client_secret: str,
-            url: str = OAUTH_BASE_URL,
-            token_url: str = OAUTH_TOKEN_URL,
-            dq_resource_id: str = OAUTH_DQ_RESOURCE_ID
+        self,
+        client_id: str,
+        client_secret: str,
+        url: str = OAUTH_BASE_URL,
+        token_url: str = OAUTH_TOKEN_URL,
+        dq_resource_id: str = OAUTH_DQ_RESOURCE_ID
     ):
 
         self.base_url: str = url
@@ -175,10 +181,10 @@ class OAuth(object):
         self.client_secret: str = client_secret
         self._stored_token: Optional[dict] = None
         self.token_data = {
-            'grant_type': 'client_credentials',
-            'client_id': self.client_id,
-            'client_secret': self.client_secret,
-            'aud': self.__dq_api_resource_id
+            "grant_type": "client_credentials",
+            "client_id": self.client_id,
+            "client_secret": self.client_secret,
+            "aud": self.__dq_api_resource_id
         }
 
         # For debugging purposes save last request response.
@@ -189,8 +195,8 @@ class OAuth(object):
     def _active_token(self) -> bool:
         """Confirms if the token being used has not expired.
         """
-        created: datetime = self._stored_token['created_at']
-        expires: int = self._stored_token['expires_in']
+        created: datetime = self._stored_token["created_at"]
+        expires: int = self._stored_token["expires_in"]
 
         return (datetime.now() - created).total_seconds() / 60 >= (expires - 1)
 
@@ -211,12 +217,12 @@ class OAuth(object):
                 proxies={},
                 method="post")
             self._stored_token: dict = {
-                'created_at': datetime.now(),
-                'access_token': js['access_token'],
-                'expires_in': js['expires_in']
+                "created_at": datetime.now(),
+                "access_token": js["access_token"],
+                "expires_in": js["expires_in"]
             }
 
-        return self._stored_token['access_token']
+        return self._stored_token["access_token"]
 
     def get_dq_api_result(self, url: str, params: dict = None) -> dict:
         """Method used exclusively to request data from the API.
@@ -229,8 +235,8 @@ class OAuth(object):
         js, self.last_response, self.last_url = dq_request(
             url=url,
             params=params,
-            headers={'Authorization': 'Bearer ' + self._get_token()},
-            proxies={}
+            headers={"Authorization": "Bearer " + self._get_token()},
+            proxies={},
         )
 
         return js
