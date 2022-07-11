34886e5291ea4ad38959da4ec023c17fd19242a1
Ksteeds <csteeds@macrosynergy.com>
2022-07-11 11:35:15 +0100

    Removed context manager to explore connection issues.

--- tests/integration/dataquery/test_dataquery.py
+++ tests/integration/dataquery/test_dataquery.py
@@ -8,13 +8,13 @@ from macrosynergy.dataquery.api import Interface
 class TestDataQueryOAuth(unittest.TestCase):
 
     def test_authentication_error(self):
-        with Interface(
+        dq = Interface(
             oauth=True,
             client_id="WRONG_CLIENT_ID",
             client_secret="NOT_A_SECRET"
-        ) as dq:
-            with self.assertRaises(RuntimeError):
-                self.assertFalse(dq.check_connection())
+        )
+        with self.assertRaises(RuntimeError):
+            self.assertFalse(dq.check_connection())
 
     def test_connection(self):
         with Interface(
@@ -27,12 +27,12 @@ class TestDataQueryOAuth(unittest.TestCase):
                             )
 
     def test_download_jpmaqs_data(self):
-        with Interface(
+        dq = Interface(
             oauth=True,
             client_id=os.getenv("DQ_CLIENT_ID"),
-            client_secret=os.getenv("DQ_CLIENT_SECRET")
-        ) as dq:
-            data = dq.download(
+            client_secret=os.getenv("DQ_CLIENT_SECRET"))
+
+        data = dq.download(
                 tickers="EUR_FXXR_NSA",
                 start_date=(datetime.date.today() - datetime.timedelta(days=10)).isoformat()
             )
