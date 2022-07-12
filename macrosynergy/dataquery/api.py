e4cf6cf985a199b533464bdf5076210d2006de6f
lsimonsen <lasse@delaporte.dk>
2022-07-12 20:27:23 +0100

    Fine tune response

--- macrosynergy/dataquery/api.py
+++ macrosynergy/dataquery/api.py
@@ -75,11 +75,13 @@ class Interface(object):
         try:
             results: dict = js["info"]
         except KeyError:
-            url = self.access.base_url
-            ip_addr = socket.gethostbyname(url)
+            dq_url = self.access.base_url
+            ip_addr = socket.gethostbyname(dq_url)
+            url_request = self.access.last_url
             now = datetime.datetime.utcnow()
             raise ConnectionError(
-                f"DataQuery base url {url:s} (IP address {ip_addr} error response at {now.isoformat()}: {js}"
+                f"DataQuery request {url_request:s} (base url {dq_url:s}, IP address {ip_addr})"
+                f" error response at {now.isoformat()}: {js}"
             )
         else:
 
