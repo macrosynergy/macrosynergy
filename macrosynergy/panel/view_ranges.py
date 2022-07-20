e514639e1adb3aad62020fbe99571bce35725e32
Ksteeds <csteeds@macrosynergy.com>
2022-07-20 07:48:43 +0100

    Reconfiguring.

--- macrosynergy/panel/view_ranges.py
+++ macrosynergy/panel/view_ranges.py
@@ -10,8 +10,8 @@ from macrosynergy.management.check_availability import reduce_df
 
 def view_ranges(df: pd.DataFrame, xcats: List[str] = None,  cids: List[str] = None,
                 start: str = '2000-01-01', end: str = None, val: str = 'value',
-                kind: str = 'bar', sort_cids_by: str = None,
-                title: str = None, ylab: str = None, size: Tuple[float] = (16, 8),
+                kind: str = 'bar', sort_cids_by: str = None, title: str = None,
+                ylab: str = None, size: Tuple[float] = (16, 8),
                 xcat_labels: List[str] = None):
 
     """Plots averages and various ranges across sections for one or more categories.
@@ -26,7 +26,10 @@ def view_ranges(df: pd.DataFrame, xcats: List[str] = None,  cids: List[str] = No
     :param <str> val: name of column that contains the values. Default is 'value'.
     :param <str> kind: type of range plot. Default is 'bar'; other option is 'box'.
     :param <str> sort_cids_by: criterion for sorting cids on x-axis;
-        Arguments can be 'mean' and 'std'. Default is None, i.e. original order.
+        Arguments can be 'mean' and 'std'. Default is None, i.e. original order. Ordering
+        will be based on the first category if the category is defined over the complete
+        panel. Otherwise, mean and standard deviation calculated, of the cross-sections,
+        computed across all categories.
     :param <str> title: string of chart title; defaults depend on type of range plot.
     :param <str> ylab: y label. Default is no label.
     :param <Tuple[float]> size: Tuple of width and height of graph. Default is (16, 8). 
@@ -34,12 +37,18 @@ def view_ranges(df: pd.DataFrame, xcats: List[str] = None,  cids: List[str] = No
 
     """
 
+    if sort_cids_by is not None:
+        assert isinstance(sort_cids_by, str)
+        sort_error = "Sorting parameter must either be 'mean' or 'std'."
+        assert sort_cids_by in ['mean', 'std'], sort_error
+
     error_message = "The number of custom labels must match the defined number of " \
                     "categories in pnl_cats."
     if xcat_labels is not None:
         assert (len(xcat_labels) == len(xcats)), error_message
 
     # Unique cross-sections across the union of categories passed - not the intersection.
+    # Order of categories will be preserved.
     df, xcats, cids = reduce_df(df, xcats, cids, start, end, out_all=True)
 
     s_date = df['real_date'].min().strftime('%Y-%m-%d')
@@ -56,11 +65,18 @@ def view_ranges(df: pd.DataFrame, xcats: List[str] = None,  cids: List[str] = No
     if ylab is None:
         ylab = ""
 
-    if sort_cids_by == 'mean':
-        dfx = dfd.groupby(['cid'])[val].mean()
+    filt_1 = df['xcat'] == xcats[0]
+    first_xcat_cids = set(df[filt_1]['cid'])
+    # First category is not defined over all cross-sections.
+    order_condition = list(set(cids)) == list(first_xcat_cids)
+
+    if order_condition:
+        # Sort exclusively on the first category.
+        dfx = df[filt_1].groupby(['cid'])[val].apply(sort_cids_by)
         order = dfx.sort_values(ascending=False).index
-    elif sort_cids_by == 'std':
-        dfx = dfd.groupby(['cid'])[val].std()
+    elif not order_condition:
+        # Sort across all categories on the available cross-sections.
+        dfx = df.groupby(['cid'])[val].apply(sort_cids_by)
         order = dfx.sort_values(ascending=False).index
     else:
         order = None
