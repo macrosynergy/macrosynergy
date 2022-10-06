28f8dfb689098650480f4daba0368475fb37c6ad
Palash Tyagi <ptyagi@macrosynergy.com>
2022-10-06 15:05:39 +0100

    Update test_testpypi_version.py
    Updated in accordance to inputs from @lsimonsen

--- tests/integration/test_testpypi_version.py
+++ tests/integration/test_testpypi_version.py
@@ -1,16 +1,7 @@
 import unittest
 import requests
 from packaging import version
-# already part of python
-
-try:
-    from .version import git_revision as __git_revision__
-    from .version import version as __version__
-except ModuleNotFoundError:
-    from setup import get_version_info
-    FULLVERSION, GIT_REVISION = get_version_info()
-    __version__ = FULLVERSION
-    __git_revision__ = GIT_REVISION
+from macrosynergy import __version__
 
 
 class TestPyPIVersionNumber(unittest.TestCase):
@@ -19,14 +10,12 @@ class TestPyPIVersionNumber(unittest.TestCase):
 
         # test for TestPyPI version number
 
-        package = 'macrosynergy'
-        with requests.get(f'https://test.pypi.org/pypi/{package}/json') as r:
+        with requests.get(f'https://test.pypi.org/pypi/macrosynergy/json') as r:
             self.assertEqual(200, r.status_code, f"Incorrect: {r.status_code}, {r.text}")
             js = r.json()
 
         latest_version = js['info']['version']
-        self.assertGreater(version.parse(__version__), version.parse(latest_version),
-                           f"The version being published to TestPyPI is not newer than the latest version already on TestPyPI. -- {__version__} <= {latest_version}")
+        self.assertGreater(version.parse(__version__), version.parse(latest_version))
 
         # prevents publishing a version that is older or equal to the latest version on TestPyPI
 
