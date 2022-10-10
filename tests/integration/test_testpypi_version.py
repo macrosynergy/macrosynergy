40d4da23aa49349a1145f4284a3383465ce6f8b2
Lasse de la Porte Simonsen <lsimonsen@macrosynergy.com>
2022-10-10 11:54:41 +0100

    Suggestion for PyPI version checks

--- tests/integration/test_testpypi_version.py
+++ tests/integration/test_testpypi_version.py
@@ -4,20 +4,34 @@ from packaging import version
 from macrosynergy import __version__
 
 
+def get_version(test: bool = False):
+    if test:
+        url = "https://test.pypi.org/pypi/macrosynergy/json"
+    else:
+        url = "https://pypi.org/pypi/macrosynergy/json"
+
+    with requests.get(url) as r:
+        assert r.ok, f"Incorrect: {r.status_code}, {r.text}"
+        js = r.json()
+
+    return js['info']['version']
+
+
 class TestPyPIVersionNumber(unittest.TestCase):
-
-    def test_version_number(self):
-
+    def test_version_number_test_pypi(self):
         # test for TestPyPI version number
+        self.assertGreaterEqual(
+            version.parse(__version__),
+            version.parse(get_version(test=True))
+        )
 
-        with requests.get(f'https://test.pypi.org/pypi/macrosynergy/json') as r:
-            self.assertEqual(200, r.status_code, f"Incorrect: {r.status_code}, {r.text}")
-            js = r.json()
+    def test_version_number_pypi(self):
+        # test for PyPI version number
+        self.assertGreater(
+            version.parse(__version__),
+            version.parse(get_version(test=False))
+        )
 
-        latest_version = js['info']['version']
-        self.assertGreater(version.parse(__version__), version.parse(latest_version))
-
-        # prevents publishing a version that is older or equal to the latest version on TestPyPI
 
 if __name__ == "__main__":
     unittest.main()
