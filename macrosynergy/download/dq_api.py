bcb54cb9196ce37e30add88af1a24458d1e26f78
Palash Tyagi <ptyagi@macrosynergy.com>
2022-12-08 17:13:03 +0000

    testing new changes to dq_api.oauth._get_token

--- macrosynergy/download/dq_api.py
+++ macrosynergy/download/dq_api.py
@@ -218,7 +218,8 @@ class OAuth(object):
     def _get_token(self) -> str:
         """Retrieves the token which is used to access DataQuery via OAuth method."""
         i = 0
-        while (not self._valid_token()) and i < 3:
+        success = False
+        while (not success) and (i < 3):
             js, success, self.last_url, msg = dq_request(
                 url=self.__token_url,
                 data=self.token_data,
@@ -226,16 +227,22 @@ class OAuth(object):
                 proxies=self.token_proxy,
             )
             i += 1
-        if not (success and self._valid_token()):
-            raise RuntimeError(
-                f"Unable to retrieve token. Error: {msg}. ")
-        self._stored_token: dict = {
-            "created_at": datetime.now(),
-            "access_token": js["access_token"],
-            "expires_in": js["expires_in"],
-        }
-
-        return self._stored_token["access_token"]
+            
+        runtime_error = RuntimeError(f"Unable to retrieve token. Error: {msg}. ")
+        if success:
+            try:
+                self._stored_token: dict = {
+                    "created_at": datetime.now(),
+                    "access_token": js["access_token"],
+                    "expires_in": js["expires_in"],
+                }
+            except:
+                raise runtime_error
+            else:
+                if self._valid_token():
+                    return self._stored_token["access_token"]
+        else:
+            raise runtime_error
 
     def get_dq_api_result(
         self, url: str, params: dict = None, proxy: Optional[dict] = None
