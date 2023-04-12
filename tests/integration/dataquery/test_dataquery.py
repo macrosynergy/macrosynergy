2eee6c22e45cfa0e13dd95c063b2cf7c94b297b6
Palash Tyagi <23239946+Magnus167@users.noreply.github.com>
2023-04-12 12:01:23 +0100

    updated integration tests for "big" download

--- tests/integration/dataquery/test_dataquery.py
+++ tests/integration/dataquery/test_dataquery.py
@@ -2,6 +2,7 @@ import unittest
 import os
 import pandas as pd
 import datetime
+from typing import List, Dict, Union, Optional
 from macrosynergy.download import JPMaQSDownload
 from macrosynergy.download.dataquery import DataQueryInterface, AuthenticationError
 
@@ -40,6 +41,52 @@ class TestDataQueryOAuth(unittest.TestCase):
                 start_date=(
                     datetime.date.today() - datetime.timedelta(days=30)
                 ).isoformat(),
+            )
+
+        self.assertIsInstance(data, pd.DataFrame)
+
+        self.assertFalse(data.empty)
+
+        self.assertGreater(data.shape[0], 0)
+
+    def test_download_jpmaqs_data_big(self):
+        # This test is to check that the download works for a large number of tickers.
+        # This is to specifically test the multi-threading functionality.
+        cids: List[str] = [
+            "AUD",
+            "CAD",
+            "CHF",
+            "EUR",
+            "GBP",
+            "NZD",
+            "USD",
+        ]
+
+        xcats: List[str] = [
+            "EQXR_NSA",
+            "FXXR_NSA",
+            "FXXR_VT10",
+            "FXTARGETED_NSA",
+            "FXUNTRADABLE_NSA",
+        ]
+
+        metrics: List[str] = ["all"]
+        start_date: str = "2020-01-01"
+        end_date: str = "2020-02-01"
+
+        data: pd.DataFrame
+        with JPMaQSDownload(
+            oauth=True,
+            client_id=os.getenv("DQ_CLIENT_ID"),
+            client_secret=os.getenv("DQ_CLIENT_SECRET"),
+            check_connection=True,
+        ) as jpmaqs:
+            data = jpmaqs.download(
+                cids=cids,
+                xcats=xcats,
+                metrics=metrics,
+                start_date=start_date,
+                end_date=end_date,
             )
 
         self.assertIsInstance(data, pd.DataFrame)
