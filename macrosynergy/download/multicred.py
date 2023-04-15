8166b8afa6d81c7ac807fcd32dd12b69360a7bb7
Palash Tyagi <ptyagi@macrosynergy.com>
2023-04-15 20:07:23 +0100

    more changes to mutlicred

--- macrosynergy/download/multicred.py
+++ macrosynergy/download/multicred.py
@@ -12,11 +12,29 @@ from joblib import Parallel, delayed
 
 
 class MultiCredentialDownload:
-    def __init__(self, config: Config, base_url: str = OAUTH_BASE_URL):
+    def __init__(
+        self, config: Config, base_url: str = OAUTH_BASE_URL, max_workers: int = 4
+    ):
         self.config = config
         self._base_url = base_url
+        self._max_workers = max_workers
 
-    def _download(
+    def _download_chunk(
+        self,
+        credentials: Config,
+        download_args: dict,
+    ):
+        # force the download skip the dataframe conversion
+        download_args["as_dataframe"] = False
+
+        try:
+            with JPMaQSDownload(**credentials.oauth(mask=False)) as jpm:
+                return jpm.download(**download_args)
+        except Exception as e:
+            print(e)
+            return -1
+
+    def download(
         self,
         tickers: Optional[List[str]] = None,
         cids: Optional[List[str]] = None,
@@ -24,11 +42,11 @@ class MultiCredentialDownload:
         metrics: List[str] = ["all"],
         start_date: str = "2000-01-01",
         end_date: Optional[str] = None,
-        show_progress: bool = True,
-        debug: bool = False,
-        suppress_warning: bool = False,
         report_time_taken: bool = True,
+        report_egress: bool = True,
     ) -> Dict[str, dict]:
+        if metrics==["all"]:
+            metrics = ["value", "grading", "eop_lag", "mop_lag"]
 
         all_expressions = JPMaQSDownload.construct_expressions(
             tickers=tickers, cids=cids, xcats=xcats, metrics=metrics
@@ -42,26 +60,60 @@ class MultiCredentialDownload:
             all_expressions[i : i + chunk_size]
             for i in range(0, len(all_expressions), chunk_size)
         ]
-
         results = []
         start: float = timer()
-        results = Parallel(n_jobs=4)(
-            delayed(JPMaQSDownload(**cred.oauth(mask=False)).download)(
-                expressions=chunk,
-                start_date=start_date,
-                end_date=end_date,
-                as_dataframe=False,
-                show_progress=show_progress,
+        results = Parallel(n_jobs=self._max_workers)(
+            delayed(self._download_chunk)(
+                credentials=credx,
+                download_args={
+                    "expressions": chunk,
+                    "start_date": start_date,
+                    "end_date": end_date,
+                    "report_time_taken": report_time_taken,
+                    "report_egress": report_egress,
+                },
             )
-            for cred, chunk in tqdm(zip(mcred, chunks), total=len(mcred))
+            for credx, chunk in tqdm(zip(mcred, chunks), total=len(chunks))
         )
         end: float = timer()
         if report_time_taken:
             print(f"MultiCred time taken: {end - start} seconds")
 
-        # combine the results
-        results = [item for sublist in results for item in sublist]
-        return results
+        failed = [i for i, x in enumerate(results) if x == -1]
+        if len(failed) > 0:
+            # form the failed expressions
+            print(f"Failed to download {len(failed)} chunks")
+            print("Retrying...")
+            failed_chunks = [chunks[i] for i in failed]
+            f_mcred = mcred[: len(failed_chunks)]
+            start: float = timer()
+            f_results = Parallel(n_jobs=self._max_workers)(
+                delayed(self._download_chunk)(
+                    credentials=credx,
+                    download_args={
+                        "expressions": chunk,
+                        "start_date": start_date,
+                        "end_date": end_date,
+                        "report_time_taken": report_time_taken,
+                        "report_egress": report_egress,
+                    },
+                )
+                for credx, chunk in tqdm(
+                    zip(f_mcred, failed_chunks), total=len(failed_chunks)
+                )
+            )
+
+            end: float = timer()
+            if report_time_taken:
+                print(f"MultiCred time taken: {end - start} seconds")
+            results = [x if x != -1 else y for x, y in zip(results, f_results)]
+
+            # print the failed expressions
+            print(f"Failed to download {len(failed)} chunks. No longer retrying.")
+
+        r = [x for x in results if x != -1]
+        r = [item for sublist in r for item in sublist]
+        return r
 
 
 if __name__ == "__main__":
@@ -96,6 +148,4 @@ if __name__ == "__main__":
         metrics=["all"],
         start_date="1990-01-01",
         end_date="2020-01-01",
-        show_progress=True,
-        report_time_taken=True,
     )
