480911b1155459664cb4d3dc9577def57010c176
Palash Tyagi <23239946+Magnus167@users.noreply.github.com>
2023-04-17 00:59:17 +0100

    updated multicred.py

--- macrosynergy/download/multicred.py
+++ macrosynergy/download/multicred.py
@@ -2,6 +2,7 @@ from typing import Dict, List, Optional, Union, Any
 from macrosynergy.download import JPMaQSDownload
 from macrosynergy.management.utils import Config
 import pandas as pd
+import os
 import pickle
 from timeit import default_timer as timer
 from tqdm import tqdm
@@ -12,11 +13,14 @@ from joblib import Parallel, delayed
 
 
 class MultiCredentialDownload:
-    def __init__(self, config: Config, max_workers: int = 4, save_path: str = None):
+    def __init__(
+        self, config: Config, max_workers: int = 4, save_path: str = "./pickledata"
+    ):
         self.config = config
         self._max_workers = max_workers
         self.save_path = save_path
         self.chunk_len = 2000
+        os.makedirs(self.save_path, exist_ok=True)
 
     def __enter__(self):
         return self
@@ -33,11 +37,16 @@ class MultiCredentialDownload:
         # force the download skip the dataframe conversion
         download_args["as_dataframe"] = False
         try:
-            with open(f"{self.save_path}/{save_id}.pkl", "rb") as f:
-                with JPMaQSDownload(
-                    **credentials.oauth(mask=False),
-                ) as jpm:
-                    pickle.dump(jpm.download(**download_args))
+            start = timer()
+            with open(f"{self.save_path}/{save_id}.pkl", "wb") as f:
+                pickle.dump(
+                    obj=JPMaQSDownload(**credentials.oauth(mask=False)).download(
+                        **download_args
+                    ),
+                    file=f,
+                )
+            end = timer()
+            print(f"Download and save from {save_id} : {end - start} seconds")
 
             return True
         except Exception as e:
@@ -83,50 +92,56 @@ class MultiCredentialDownload:
         save_ids: List[str] = [f"{i}" for i in range(len(download_args_list))]
 
         # create a list of creds = [cred1, cred,2, ... credn, cred1, cred2, ... credn] to the length of the download args. there are a limited number of creds, so we will just loop through them
-        
-        creds : List[Config] = []
+
+        creds: List[Config] = []
         available_creds_count = len(self.config.multi_credentials())
         for i in range(len(download_args_list)):
             creds.append(self.config.multi_credentials()[i % available_creds_count])
 
-        results : List[bool] = Parallel(n_jobs=self._max_workers)(
+        results: List[bool] = Parallel(n_jobs=self._max_workers)(
             delayed(self._download_chunk)(
                 credentials=cred,
                 download_args=download_args,
                 save_id=save_id,
             )
-            for cred, download_args, save_id in zip(creds, download_args_list, save_ids)
+            for cred, download_args, save_id in tqdm(
+                zip(creds, download_args_list, save_ids), total=len(download_args_list)
+            )
         )
 
         # if any of the results are false, then we need to re-run the failed ones
         if not all(results):
             failed_ids: List[str] = [
-                save_id for save_id, result in zip(save_ids, results) if not result
+                "F" + save_id
+                for save_id, result in zip(save_ids, results)
+                if not result
             ]
             failed_args: List[dict] = [
                 download_args
                 for download_args, result in zip(download_args_list, results)
                 if not result
             ]
-            creds : List[Config] = []
+            creds: List[Config] = []
             for i in range(len(failed_args)):
                 creds.append(self.config.multi_credentials()[i % available_creds_count])
 
-            results : List[bool] = Parallel(n_jobs=self._max_workers)(
+            results: List[bool] = Parallel(n_jobs=self._max_workers)(
                 delayed(self._download_chunk)(
                     credentials=cred,
                     download_args=download_args,
                     save_id=save_id,
                 )
-                for cred, download_args, save_id in zip(creds, failed_args, failed_ids)
+                for cred, download_args, save_id in tqdm(
+                    zip(creds, failed_args, failed_ids), total=len(failed_args)
+                )
             )
 
             if not all(results):
-                print(f"Failed to download {len(failed_ids)*self.chunk_len} expressions")
+                print(
+                    f"Failed to download {len(failed_ids)*self.chunk_len} expressions"
+                )
 
-            return             
-
-           
+            return
 
 
 if __name__ == "__main__":
@@ -154,7 +169,8 @@ if __name__ == "__main__":
         "CHF_DU02YXRxEASD_NSA",
     ]
 
-    mcd = MultiCredentialDownload(config=Config("./config_multicred.yml"))
+    mcd = MultiCredentialDownload(config=Config("./config_multicred.yml"), 
+                                    save_path="./pickle_data",)
 
     x = mcd.download(
         tickers=tickers,
