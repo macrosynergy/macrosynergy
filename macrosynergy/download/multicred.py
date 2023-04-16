00ef17e625df6988db6c8dc5d050cb8691d101e5
Palash Tyagi <ptyagi@macrosynergy.com>
2023-04-17 00:28:53 +0100

    updated multicred

--- macrosynergy/download/multicred.py
+++ macrosynergy/download/multicred.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Optional, Union, Any 
+from typing import Dict, List, Optional, Union, Any
 from macrosynergy.download import JPMaQSDownload
 from macrosynergy.management.utils import Config
 import pandas as pd
@@ -12,15 +12,17 @@ from joblib import Parallel, delayed
 
 
 class MultiCredentialDownload:
-    def __init__(
-        self, config: Config, 
-        max_workers: int = 4,
-        save_path: str = None
-    ):
+    def __init__(self, config: Config, max_workers: int = 4, save_path: str = None):
         self.config = config
         self._max_workers = max_workers
         self.save_path = save_path
-        self.chunk_len = 1000
+        self.chunk_len = 2000
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        pass
 
     def _download_chunk(
         self,
@@ -32,20 +34,15 @@ class MultiCredentialDownload:
         download_args["as_dataframe"] = False
         try:
             with open(f"{self.save_path}/{save_id}.pkl", "rb") as f:
-                with JPMaQSDownload(**credentials.oauth(mask=False), 
-                                    ) as jpm:
+                with JPMaQSDownload(
+                    **credentials.oauth(mask=False),
+                ) as jpm:
                     pickle.dump(jpm.download(**download_args))
 
             return True
         except Exception as e:
             print(e)
             return False
-        
-    def _get_next_credentials(self):
-        while True:
-            for cred in self.config.multi_credentials():
-                yield cred
-
 
     def download(
         self,
@@ -58,68 +55,78 @@ class MultiCredentialDownload:
         report_time_taken: bool = True,
         report_egress: bool = True,
     ) -> Dict[str, dict]:
-        if metrics==["all"]:
-            metrics : List[str] = ["value", "grading", "eop_lag", "mop_lag"]
+        if metrics == ["all"]:
+            metrics: List[str] = ["value", "grading", "eop_lag", "mop_lag"]
 
-        all_expressions : List[str] = JPMaQSDownload.construct_expressions(
+        all_expressions: List[str] = JPMaQSDownload.construct_expressions(
             tickers=tickers, cids=cids, xcats=xcats, metrics=metrics
         )
-        
+
         # form "chunks" of 1000
-        chunks : List[List[str]] = [
-            all_expressions[i : i + self.chunk_len] for i in range(0, len(all_expressions), self.chunk_len)
+        chunks: List[List[str]] = [
+            all_expressions[i : i + self.chunk_len]
+            for i in range(0, len(all_expressions), self.chunk_len)
         ]
 
-        download_args_list : List[dict] = [{
-                        "expressions": chunk,
-                        "start_date": start_date,
-                        "end_date": end_date,
-                        "as_dataframe": False,
-                        "report_time_taken": report_time_taken,
-                        "report_egress": report_egress,
-                    } for chunk in chunks]
+        download_args_list: List[dict] = [
+            {
+                "expressions": chunk,
+                "start_date": start_date,
+                "end_date": end_date,
+                "as_dataframe": False,
+                "report_time_taken": report_time_taken,
+                "report_egress": report_egress,
+            }
+            for chunk in chunks
+        ]
+
+        save_ids: List[str] = [f"{i}" for i in range(len(download_args_list))]
+
+        # create a list of creds = [cred1, cred,2, ... credn, cred1, cred2, ... credn] to the length of the download args. there are a limited number of creds, so we will just loop through them
         
-        save_ids : List[str] = [f"{i}" for i in range(len(download_args_list))]
-        
-        results : List[bool] = \
-            Parallel(n_jobs=self._max_workers)(
-                delayed(self._download_chunk)(
-                    credentials=self._get_next_credentials(),
-                    download_args=download_args,
-                    save_id=save_id,    
-                ) for save_id, download_args in zip(save_ids, download_args_list)
+        creds : List[Config] = []
+        available_creds_count = len(self.config.multi_credentials())
+        for i in range(len(download_args_list)):
+            creds.append(self.config.multi_credentials()[i % available_creds_count])
+
+        results : List[bool] = Parallel(n_jobs=self._max_workers)(
+            delayed(self._download_chunk)(
+                credentials=cred,
+                download_args=download_args,
+                save_id=save_id,
             )
-        
+            for cred, download_args, save_id in zip(creds, download_args_list, save_ids)
+        )
 
         # if any of the results are false, then we need to re-run the failed ones
         if not all(results):
-            failed_ids : List[str] = [save_id for save_id, result in zip(save_ids, results) if not result]
-            failed_args : List[dict] = [download_args for download_args, result in zip(download_args_list, results) if not result]
-            
-            results : List[bool] = \
-                Parallel(n_jobs=self._max_workers)(
-                    delayed(self._download_chunk)(
-                        credentials=self._get_next_credentials(),
-                        download_args=download_args,
-                        save_id=save_id,    
-                    ) for save_id, download_args in zip(failed_ids, failed_args)
+            failed_ids: List[str] = [
+                save_id for save_id, result in zip(save_ids, results) if not result
+            ]
+            failed_args: List[dict] = [
+                download_args
+                for download_args, result in zip(download_args_list, results)
+                if not result
+            ]
+            creds : List[Config] = []
+            for i in range(len(failed_args)):
+                creds.append(self.config.multi_credentials()[i % available_creds_count])
+
+            results : List[bool] = Parallel(n_jobs=self._max_workers)(
+                delayed(self._download_chunk)(
+                    credentials=cred,
+                    download_args=download_args,
+                    save_id=save_id,
                 )
-                
-            
+                for cred, download_args, save_id in zip(creds, failed_args, failed_ids)
+            )
+
             if not all(results):
-                # failed to download n expressions. no longer retrying
-                print(f"Failed to download {(len(results) - sum(results))*self.chunk_len}"
-                                " expressions. No longer retrying.")
-            
-            return
-            
-        
-        
-        
+                print(f"Failed to download {len(failed_ids)*self.chunk_len} expressions")
 
+            return             
 
-
-
+           
 
 
 if __name__ == "__main__":
