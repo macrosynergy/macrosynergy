4ffdd6fe446b47c949d620389e7bec4fe96b138a
Palash Tyagi <ptyagi@macrosynergy.com>
2023-04-21 13:19:31 +0100

    Changed macrosynergy.dataquery related deprecations

--- macrosynergy/dataquery/api.py
+++ macrosynergy/dataquery/api.py
@@ -1,7 +1,17 @@
 """Old DataQuery Interface path"""
 import warnings
-from ..download.jpmaqs import JPMaQSDownload as Interface
+from ..download.jpmaqs import JPMaQSDownload
 
+class Interface(JPMaQSDownload):
+    def __init__(self, *args, **kwargs):
+        warnings.warn(
+            "api.Interface has been moved to macrosynergy.download.jpmaqs - will be removed in v0.1.0",
+            DeprecationWarning,
+            stacklevel=2
+        )
+
+        super().__init__(*args, **kwargs)
+        
 warnings.warn(
     "api.Interface has been moved to macrosynergy.download.jpmaqs - will be removed in v0.1.0",
     DeprecationWarning,
