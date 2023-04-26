e8755c66f4e32f3642aad59385496deeccebfcb5
Palash Tyagi <ptyagi@macrosynergy.com>
2023-04-26 13:01:50 +0100

    changes around warnings

--- macrosynergy/download/jpmaqs.py
+++ macrosynergy/download/jpmaqs.py
@@ -263,10 +263,13 @@ class JPMaQSDownload(object):
                 f"Some expressions are missing from the downloaded data."
                 " Check logger output for complete list. \n"
                 f"{len(expr_missing)} out of {len(expr_expected)} expressions are missing."
+                f"To download the catalogue of all available expressions and filter the"
+                " unavailable expressions, set `get_catalogue=True` in the "
+                " call to `JPMaQSDownload.download()`." 
             )
 
-            logger.warning(log_str)
-            logger.warning(f"Missing expressions: {expr_missing}")
+            logger.info(log_str)
+            logger.info(f"Missing expressions: {expr_missing}")
             if verbose:
                 print(log_str)
 
@@ -463,6 +466,7 @@ class JPMaQSDownload(object):
         metrics: List[str],
         start_date: str,
         end_date: str,
+        get_catalogue: bool,
         expressions: List[str],
         show_progress: bool,
         as_dataframe: bool,
@@ -491,6 +495,9 @@ class JPMaQSDownload(object):
 
         if not isinstance(report_egress, bool):
             raise TypeError("`report_egress` must be a boolean.")
+
+        if not isinstance(get_catalogue, bool):
+            raise TypeError("`get_catalogue` must be a boolean.")
 
         if all([tickers is None, cids is None, xcats is None, expressions is None]):
             raise ValueError(
@@ -586,7 +593,7 @@ class JPMaQSDownload(object):
         start_date: str = "2000-01-01",
         end_date: Optional[str] = None,
         expressions: Optional[List[str]] = None,
-        filter_from_catalogue: bool = False,
+        get_catalogue: bool = False,
         show_progress: bool = False,
         debug: bool = False,
         suppress_warning: bool = False,
@@ -610,7 +617,7 @@ class JPMaQSDownload(object):
         :param <str> end_date: end date of the data to download in the ISO
             format - YYYY-MM-DD.
         :param <list[str]> expressions: list of DataQuery expressions.
-        :param <bool> filter_from_catalogue: If True, the JPMaQS catalogue is
+        :param <bool> get_catalogue: If True, the JPMaQS catalogue is
             downloaded and used to filter the list of tickers. Default is
             False.
         :param <bool> show_progress: True if progress bar should be shown,
@@ -669,6 +676,7 @@ class JPMaQSDownload(object):
             start_date=start_date,
             end_date=end_date,
             expressions=expressions,
+            get_catalogue=get_catalogue,
             show_progress=show_progress,
             as_dataframe=as_dataframe,
             report_time_taken=report_time_taken,
@@ -694,7 +702,7 @@ class JPMaQSDownload(object):
             metrics=metrics,
         )
 
-        if filter_from_catalogue:
+        if get_catalogue:
             expressions = self.filter_expressions_from_catalogue(expressions)
 
         # Download data.
@@ -834,7 +842,7 @@ if __name__ == "__main__":
             xcats=xcats,
             metrics=metrics,
             start_date=start_date,
-            filter_from_catalogue=True,
+            get_catalogue=True,
             end_date=end_date,
             show_progress=True,
             suppress_warning=False,
