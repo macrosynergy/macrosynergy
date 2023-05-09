1bb94281f871c749c6fc13ac6d6300e18b4642d9
Palash Tyagi <23239946+Magnus167@users.noreply.github.com>
2023-05-09 23:48:33 +0100

    try...

--- setup.py
+++ setup.py
@@ -176,6 +176,7 @@ def nuitka_args(packages: List[str]) -> Dict[str, Any]:
             # "--include-module": packages + extra_packages,
             # "--include-package": packages + extra_packages,
             # "--follow-import-to": extra_packages,
+            "--follow-import-to": ["pandas", "numpy",],
             # "--enable-plugin": [ # "numpy",
             #                     "matplotlib", "multiprocessing", "anti-bloat", "data-files", "implicit-imports"],
             "--enable-plugin": ["matplotlib", "multiprocessing", #"anti-bloat", 
