5aff03cdb41759b38bcd926a6a5368360aa9c64d
Palash Tyagi <ptyagi@macrosynergy.com>
2023-05-25 00:27:10 +0100

    trying with Cython now

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ import warnings
 from pathlib import Path
 from typing import List, Dict, Any
 import importlib
+from Cython.Build import cythonize
 
 DOCLINES = (__doc__ or "").split("\n")
 
@@ -201,6 +202,22 @@ def nuitka_args(packages: List[str]) -> Dict[str, Any]:
     return command_options
 
 
+def get_ext_paths(root_dir, exclude_files):
+    """get filepaths for compilation"""
+    paths = []
+
+    for root, dirs, files in os.walk(root_dir):
+        for filename in files:
+            if os.path.splitext(filename)[1] != '.py':
+                continue
+
+            file_path = os.path.join(root, filename)
+            if file_path in exclude_files:
+                continue
+
+            paths.append(file_path)
+    return paths
+
 def setup_package():
     from setuptools import setup, find_packages
 
@@ -213,15 +230,19 @@ def setup_package():
     write_version_py()
     # write_version_py(filename='macrosynergy.build/version.py')
     # move ./tests to ./macrosynergy/tests
-
+    EXCLUDE_FILES = []
     metadata = dict(
         platforms=["Windows", "Linux", "Mac OS-X"],
         test_suite="pytest",
         python_requires=">=3.6",
         install_requires=REQUIREMENTS.split("\n"),
         include_package_data=True,
-        # packages=find_packages(),
         version=get_version_info()[0],
+        packages=find_packages(),
+        ext_modules=cythonize(
+        get_ext_paths('macrosynergy', EXCLUDE_FILES),
+        compiler_directives={'language_level': 3}
+    )
     )
     # __copyright__ = 'Copyright 2020 Macrosynergy Ltd'
 
@@ -229,7 +250,7 @@ def setup_package():
     try:
         import nuitka
 
-        nuitka_available = True
+        # nuitka_available = True
     except:
         pass
 
