df305e01e4cb516525cf0207fd76b9042f25edc9
Palash Tyagi <ptyagi@macrosynergy.com>
2023-06-08 17:27:58 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ Development Status :: 4 - Beta
 MAJOR = 0
 MINOR = 0
 MICRO = 32
-ISRELEASED = False
+ISRELEASED = True
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
 if sys.version_info >= (3, 12):
