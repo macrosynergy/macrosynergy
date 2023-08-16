57cb1c6943ab59a1185ce8f9ab494f64bce6b0e6
Palash Tyagi <ptyagi@macrosynergy.com>
2023-08-16 13:06:29 +0100

    modified subpackage init

--- macrosynergy/visuals/__init__.py
+++ macrosynergy/visuals/__init__.py
@@ -1,5 +1,6 @@
 from .facetplot import FacetPlot
 from .lineplot import LinePlot
 from .plotter import Plotter
+from . import view
 
-__all__ = ["Plotter", "LinePlot", "FacetPlot"]
+__all__ = ["Plotter", "LinePlot", "FacetPlot", "view"]
