c451a683e603056e007aace69675416934b01166
Palash Tyagi <ptyagi@macrosynergy.com>
2023-08-17 16:54:18 +0100

    made docstring prettier

--- macrosynergy/visuals/view.py
+++ macrosynergy/visuals/view.py
@@ -45,6 +45,8 @@ def timelines(
 ):
     """Displays a facet grid of time line charts of one or more categories.
 
+    Parameters
+    ----------
     :param <pd.Dataframe> df: standardized DataFrame with the necessary columns:
         'cid', 'xcats', 'real_date' and at least one column with values of interest.
     :param <List[str]> xcats: extended categories to plot. Default is all in DataFrame.
