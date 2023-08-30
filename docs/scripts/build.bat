c3706cc33853aeee160f7b4654b2772b9e3e708c
Palash Tyagi <ptyagi@macrosynergy.com>
2023-08-30 10:15:17 +0100

    added folder level to build.bat

--- docs/scripts/build.bat
+++ docs/scripts/build.bat
@@ -1,10 +1,10 @@
-python.exe .\docs\build_md.py
-python.exe .\docs\build_jpb.py
+python.exe .\docs\scripts\build_md.py
+python.exe .\docs\scripts\build_jpb.py
 cd .\docs\build\
 jupyter-book build macrosynergy
 jupyter-book config sphinx macrosynergy
 cd ..\..
 
-python.exe .\docs\post_build.py
+python.exe .\docs\scripts\post_build.py
 
 @REM .\scripts\build.bat
