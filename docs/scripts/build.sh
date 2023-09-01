77dee604184a57ac40bd3d1e5f92d91f7d4f999c
Palash Tyagi <ptyagi@macrosynergy.com>
2023-09-01 13:10:41 +0100

    small bugfix in build.sh

--- docs/scripts/build.sh
+++ docs/scripts/build.sh
@@ -1,5 +1,5 @@
 python docs/scripts/build_md.py
 python docs/scripts/build_jpb.py
-jupyter-book build ./docs/build/macrosynergy
+jupyter-book build ./docs/build/
 python docs/scripts/post_build.py
 
