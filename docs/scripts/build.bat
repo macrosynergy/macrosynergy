b09b7825c4f6dd407c41bc54c70534654f82f158
Palash Tyagi <ptyagi@macrosynergy.com>
2023-09-01 13:03:56 +0100

    changed commands

--- docs/scripts/build.bat
+++ docs/scripts/build.bat
@@ -1,9 +1,6 @@
 python.exe .\docs\scripts\build_md.py
 python.exe .\docs\scripts\build_jpb.py
-cd .\docs\build\
-jupyter-book build macrosynergy
-cd ..\..
-
+jupyter-book build .\docs\build\
 python.exe .\docs\scripts\post_build.py
 
 @REM jupyter-book config sphinx macrosynergy
