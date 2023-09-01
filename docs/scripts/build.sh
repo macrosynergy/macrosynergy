1fe9ad2a3e96b726fe279f9459a8fc6ae18c3dec
Palash Tyagi <ptyagi@macrosynergy.com>
2023-09-01 13:06:10 +0100

    modified build.sh

--- docs/scripts/build.sh
+++ docs/scripts/build.sh
@@ -1,11 +1,5 @@
 python docs/scripts/build_md.py
 python docs/scripts/build_jpb.py
-
-cd ./docs/build/
-jupyter-book build macrosynergy
-jupyter-book config sphinx macrosynergy
-
-cd ../..
-
+jupyter-book build ./docs/build/macrosynergy
 python docs/scripts/post_build.py
 
