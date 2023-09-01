59386bce5426566ff7533abc48c2fd32b4886fe3
Palash Tyagi <ptyagi@macrosynergy.com>
2023-09-01 11:38:38 +0100

    removed sphinx conf.py generation

--- docs/scripts/build.bat
+++ docs/scripts/build.bat
@@ -2,9 +2,9 @@ python.exe .\docs\scripts\build_md.py
 python.exe .\docs\scripts\build_jpb.py
 cd .\docs\build\
 jupyter-book build macrosynergy
-jupyter-book config sphinx macrosynergy
 cd ..\..
 
 python.exe .\docs\scripts\post_build.py
 
+@REM jupyter-book config sphinx macrosynergy
 @REM .\scripts\build.bat
