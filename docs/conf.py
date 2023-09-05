14f1a85e7f5185eb34fa010a3ef94cefae9034ca
Palash Tyagi <ptyagi@macrosynergy.com>
2023-09-05 14:18:49 +0100

    applied formatting

--- docs/conf.py
+++ docs/conf.py
@@ -3,34 +3,81 @@
 # If you wish to continue using _config.yml, make edits to that file and
 # re-generate this one.
 ###############################################################################
-author = 'Macrosynergy Ltd.'
-comments_config = {'hypothesis': False, 'utterances': False}
-copyright = '2023'
-exclude_patterns = ['**.ipynb_checkpoints', '.DS_Store', 'Thumbs.db', '_build']
-extensions = ['sphinx_togglebutton', 'sphinx_copybutton', 'myst_nb', 'jupyter_book', 'sphinx_thebe', 'sphinx_comments', 'sphinx_external_toc', 'sphinx.ext.intersphinx', 'sphinx_design', 'sphinx_book_theme', 'sphinx_multiversion', 'sphinx_jupyterbook_latex']
+author = "Macrosynergy Ltd."
+comments_config = {"hypothesis": False, "utterances": False}
+copyright = "2023"
+exclude_patterns = ["**.ipynb_checkpoints", ".DS_Store", "Thumbs.db",]
+extensions = [
+    "sphinx_togglebutton",
+    "sphinx_copybutton",
+    "myst_nb",
+    "jupyter_book",
+    "sphinx_thebe",
+    "sphinx_comments",
+    "sphinx_external_toc",
+    "sphinx.ext.intersphinx",
+    "sphinx_design",
+    "sphinx_book_theme",
+    "sphinx_multiversion",
+    "sphinx_jupyterbook_latex",
+]
 external_toc_exclude_missing = False
-external_toc_path = 'build/_toc.yml'
-html_baseurl = 'https://docs.macrosynergy.com/'
-html_css_files = ['custom_css.css']
-html_favicon = 'https://macrosynergy.com/wp-content/uploads/2022/10/macrosynergy-logo-favicon-300x300.png'
-html_logo = ''
-html_sourcelink_suffix = ''
-html_static_path = ['_static']
-html_theme = 'sphinx_book_theme'
-html_theme_options = {'search_bar_text': 'Search this book...', 'launch_buttons': {'notebook_interface': 'classic', 'binderhub_url': '', 'jupyterhub_url': '', 'thebe': False, 'colab_url': ''}, 'path_to_docs': '', 'repository_url': 'https://github.com/macrosynergy/macrosynergy', 'repository_branch': 'master', 'extra_footer': '', 'home_page_in_toc': True, 'announcement': '<p class=\'heading-banner\'>\n  <img src=\'https://github.com/macrosynergy/macrosynergy/blob/feature/docs_book/docs/assets/MACROSYNERGY_Logo_White.png?raw=true\'\n      width=\'35%\' height=\'auto\' style=\'display: block; margin-left: auto ; margin-right: auto ; \' class=\'bd-header-announcement-img\'\n      onclick="window.location.href=\'https://macrosynergy.com/\'">\n</p>\n', 'analytics': {'google_analytics_id': ''}, 'use_repository_button': True, 'use_edit_page_button': False, 'use_issues_button': True}
-html_title = 'Macrosynergy Package Documentation'
-latex_engine = 'pdflatex'
-myst_enable_extensions = ['amsmath', 'dollarmath', 'attrs_inline', 'colon_fence', 'deflist', 'fieldlist', 'html_admonition', 'html_image', 'linkify', 'replacements', 'smartquotes', 'strikethrough', 'substitution', 'tasklist']
-myst_url_schemes = ['mailto', 'http', 'https']
+external_toc_path = "build/_toc.yml"
+html_baseurl = "https://docs.macrosynergy.com/"
+html_css_files = ["custom_css.css"]
+html_favicon = "https://macrosynergy.com/wp-content/uploads/2022/10/macrosynergy-logo-favicon-300x300.png"
+html_logo = ""
+html_sourcelink_suffix = ""
+html_static_path = ["_static"]
+html_theme = "sphinx_book_theme"
+html_theme_options = {
+    "search_bar_text": "Search this book...",
+    "launch_buttons": {
+        "notebook_interface": "classic",
+        "binderhub_url": "",
+        "jupyterhub_url": "",
+        "thebe": False,
+        "colab_url": "",
+    },
+    "path_to_docs": "",
+    "repository_url": "https://github.com/macrosynergy/macrosynergy",
+    "repository_branch": "master",
+    "extra_footer": "",
+    "home_page_in_toc": True,
+    "announcement": "<p class='heading-banner'>\n  <img src='https://github.com/macrosynergy/macrosynergy/main/docs/assets/MACROSYNERGY_Logo_White.png?raw=true'\n      width='35%' height='auto' style='display: block; margin-left: auto ; margin-right: auto ; ' class='bd-header-announcement-img'\n      onclick=\"window.location.href='https://macrosynergy.com/'\">\n</p>\n",
+    "analytics": {"google_analytics_id": ""},
+    "use_repository_button": True,
+    "use_edit_page_button": False,
+    "use_issues_button": True,
+}
+html_title = "Macrosynergy Package Documentation"
+latex_engine = "pdflatex"
+myst_enable_extensions = [
+    "amsmath",
+    "dollarmath",
+    "attrs_inline",
+    "colon_fence",
+    "deflist",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+    "linkify",
+    "replacements",
+    "smartquotes",
+    "strikethrough",
+    "substitution",
+    "tasklist",
+]
+myst_url_schemes = ["mailto", "http", "https"]
 nb_execution_allow_errors = False
-nb_execution_cache_path = ''
+nb_execution_cache_path = ""
 nb_execution_excludepatterns = []
 nb_execution_in_temp = False
-nb_execution_mode = 'off'
+nb_execution_mode = "off"
 nb_execution_timeout = 30
-nb_output_stderr = 'show'
+nb_output_stderr = "show"
 numfig = True
-pygments_style = 'sphinx'
-suppress_warnings = ['myst.domains']
+pygments_style = "sphinx"
+suppress_warnings = ["myst.domains"]
 use_jupyterbook_latex = True
 use_multitoc_numbering = True
