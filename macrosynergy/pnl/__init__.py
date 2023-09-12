9409ada35cd1dbcd01f195a3cbf90b79987d1a13
Palash Tyagi <ptyagi@macrosynergy.com>
2023-09-12 17:14:50 +0100

    added numeric and nonetype to subpackage init

--- macrosynergy/pnl/__init__.py
+++ macrosynergy/pnl/__init__.py
@@ -1,4 +1,12 @@
+from typing import List, Union, Tuple, Optional, SupportsInt, SupportsFloat
+import numpy as np
+
+
 from macrosynergy.pnl.naive_pnl import NaivePnL
 
 
-__all__ = ['NaivePnL']
+NoneType = type(None)
+Numeric = Union[int, float, np.int64, np.float64, SupportsInt, SupportsFloat]
+
+
+__all__ = ['Numeric', 'NoneType', 'NaivePnL']
