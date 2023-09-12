74b6f3916269a8b72af078a2cbc35438db322e60
Palash Tyagi <ptyagi@macrosynergy.com>
2023-09-12 16:04:24 +0100

    added flag to build.bat

--- docs/scripts/build.bat
+++ docs/scripts/build.bat
@@ -1,7 +1,24 @@
+@REM  if there is a flag for --clean, pass it to post_build.py
+
+set "flag=%1"
+if "%flag%"=="--clean" (
+    set "flag=--clean"
+) else (
+    set "flag="
+)
+
+
 python.exe .\docs\scripts\build_md.py
 python.exe .\docs\scripts\build_jpb.py
 jupyter-book build .\docs\build\
-python.exe .\docs\scripts\post_build.py
+@REM python.exe .\docs\scripts\post_build.py
+
+if "%flag%"=="--clean" (
+    python.exe .\docs\scripts\post_build.py --clean
+) else (
+    python.exe .\docs\scripts\post_build.py
+)
+
 
 @REM jupyter-book config sphinx macrosynergy
 @REM .\scripts\build.bat
