b4497f273ab56447882f6dbc6c26771044c1f6ac
Palash Tyagi <ptyagi@macrosynergy.com>
2023-09-12 16:04:34 +0100

    added --clean flag to build.sh

--- docs/scripts/build.sh
+++ docs/scripts/build.sh
@@ -1,5 +1,24 @@
-python docs/scripts/build_md.py
-python docs/scripts/build_jpb.py
-jupyter-book build ./docs/build/
-python docs/scripts/post_build.py
+#!/bin/bash
 
+flag=$1
+
+if [ "$flag" == "--clean" ]; then
+    flag="--clean"
+else
+    flag=""
+fi
+
+python.exe ./docs/scripts/build_md.py
+python.exe ./docs/scripts/build_jpb.py
+jupyter-book build ./docs/build/
+
+# python.exe ./docs/scripts/post_build.py
+
+if [ "$flag" == "--clean" ]; then
+    python.exe ./docs/scripts/post_build.py --clean
+else
+    python.exe ./docs/scripts/post_build.py
+fi
+
+# jupyter-book config sphinx macrosynergy
+# ./scripts/build.sh
