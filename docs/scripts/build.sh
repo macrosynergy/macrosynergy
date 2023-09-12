9c1a6c05287587d6bd1ba0446b4bacef50663874
Palash Tyagi <ptyagi@macrosynergy.com>
2023-09-12 16:11:50 +0100

    smol bug

--- docs/scripts/build.sh
+++ docs/scripts/build.sh
@@ -8,16 +8,16 @@ else
     flag=""
 fi
 
-python.exe ./docs/scripts/build_md.py
-python.exe ./docs/scripts/build_jpb.py
+python ./docs/scripts/build_md.py
+python ./docs/scripts/build_jpb.py
 jupyter-book build ./docs/build/
 
-# python.exe ./docs/scripts/post_build.py
+# python ./docs/scripts/post_build.py
 
 if [ "$flag" == "--clean" ]; then
-    python.exe ./docs/scripts/post_build.py --clean
+    python ./docs/scripts/post_build.py --clean
 else
-    python.exe ./docs/scripts/post_build.py
+    python ./docs/scripts/post_build.py
 fi
 
 # jupyter-book config sphinx macrosynergy
