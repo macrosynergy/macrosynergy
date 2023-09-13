b0b12b9c64349db60d094c2d6907b7dcd8d1c9b8
Palash Tyagi <ptyagi@macrosynergy.com>
2023-09-13 15:20:02 +0100

    cleaning up subpackage init

--- macrosynergy/visuals/__init__.py
+++ macrosynergy/visuals/__init__.py
@@ -9,5 +9,8 @@ from typing import Union, SupportsInt, SupportsFloat
 NoneType = type(None)
 Numeric = Union[int, float, np.int64, np.float64, SupportsInt, SupportsFloat]
 
+TYPES = ["NoneType", "Numeric"]
+CLASSES = ["FacetPlot", "LinePlot", "Plotter"]
+MODULES = ["view"]
 
-__all__ = ["Plotter", "LinePlot", "FacetPlot", "view"]
+__all__ = TYPES + CLASSES + MODULES
