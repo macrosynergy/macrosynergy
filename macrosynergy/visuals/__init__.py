3e7252122fdf1957fa6cdbd5ec005bdee4386bcd
Palash Tyagi <ptyagi@macrosynergy.com>
2023-09-13 15:19:26 +0100

    added NoneType and Numeric to sub-package init

--- macrosynergy/visuals/__init__.py
+++ macrosynergy/visuals/__init__.py
@@ -3,4 +3,11 @@ from .lineplot import LinePlot
 from .plotter import Plotter
 from . import view
 
+import numpy as np
+from typing import Union, SupportsInt, SupportsFloat
+
+NoneType = type(None)
+Numeric = Union[int, float, np.int64, np.float64, SupportsInt, SupportsFloat]
+
+
 __all__ = ["Plotter", "LinePlot", "FacetPlot", "view"]
