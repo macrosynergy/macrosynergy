027d8abfc5a1f9338b7d314dd4ced8e326e68c6f
Palash Tyagi <ptyagi@macrosynergy.com>
2023-09-13 10:44:13 +0100

    changed some type hints and docs

--- macrosynergy/pnl/contract_signals.py
+++ macrosynergy/pnl/contract_signals.py
@@ -3,8 +3,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-from typing import List, Union, Tuple
+from typing import List, Union, Tuple, Optional
 
+from macrosynergy.pnl import Numeric
 from macrosynergy.management.simulate_quantamental_data import make_qdf
 from macrosynergy.management.shape_dfs import reduce_df
 
@@ -14,15 +15,15 @@ def contract_signals(
     sig: str,
     cids: List[str],
     ctypes: List[str],
-    cscales: Union(List[float], List[str]) = None,
-    csigns: List[int] = None,
-    hbasket: List[str] = None,
-    hscales: Union(List[float], List[str]) = None,
-    hratio: str = None,
-    start: str = None,
-    end: str = None,
-    blacklist: dict = None,
-    sname = "STRAT",
+    cscales: Optional[List[Numeric]] = None,
+    csigns: Optional[List[int]] = None,
+    hbasket: Optional[List[str]] = None,
+    hscales: Optional[List[Numeric]] = None,
+    hratio: Optional[str] = None,
+    start: Optional[str] = None,
+    end: Optional[str] = None,
+    blacklist: Optional[dict] = None,
+    sname: str = "STRAT",
 ):
     """
     Caclulate contract specific signals based on cross-section-specific signals
@@ -34,21 +35,21 @@ def contract_signals(
         in the hedging basket, as well as cross-section specific hedge ratios
     :param <str> sig: the cross-section-specific signal that serves as the basis of
         contract signals.
-    :param <list[str]> cids: list of cross-sections whose signal is to be used.
-    :param <list[str]> ctypes: list of identifiers for the contract types that are
+    :param <List[str]> cids: list of cross-sections whose signal is to be used.
+    :param <List[str]> ctypes: list of identifiers for the contract types that are
         to be traded. They typically correspond to the contract type acronyms
         that are used in JPMaQS for  generic returns, carry and volatility, such as
         "FX" for FX forwards or "EQ" for equity index futures.
         N.B. Overall a contract is identified by the combination of its cross-section
         and its contract type "<cid>_<ctype>".
-    :param <list[float]> cscales: list of scaling factors for the contract signals.
+    :param <List[float]> cscales: list of scaling factors for the contract signals.
         These can be eigher a list of floats or a list of category tickers that serve
         as basis of translation. The former are fixed across time, the latter variable.
-    :param <list[float]> csigns: list of signs for the contract signals. These must be 
+    :param <List[float]> csigns: list of signs for the contract signals. These must be
         either 1 for long position or -1 for short position.
-    :param <list[str]> hbasket: list of contract identifiers in the format "<cid>_<ctype>"
+    :param <List[str]> hbasket: list of contract identifiers in the format "<cid>_<ctype>"
         that serve as constituents of the hedging basket.
-    param <list[float]> cscales: list of scaling factors (weights) for the basket.
+    param <List[float]> cscales: list of scaling factors (weights) for the basket.
         These can be eigher a list of floats or a list of category tickers that serve
         as basis of translation. The former are fixed across time, the latter variable.
     :param <str> hratio: category names for cross-section-specific hedge ratios.
@@ -60,11 +61,9 @@ def contract_signals(
         from the dataframe.
     :param <str> sname: name of the strategy. Default is "STRAT".
 
-
-    :return: <pd.DataFrame> with the contract signals for all traded contracts and the
+    :return <pd.DataFrame>: with the contract signals for all traded contracts and the
         specified strategy. It has the standard JPMaQS DataFrame. The contract signals
         have the following format "<cid>_<ctype>_<sname>_CSIG".
-              
     """
 
     pass
