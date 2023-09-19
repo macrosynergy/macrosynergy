d7f34e4ebd0bd175f207de84b6bbd70aa88f371c
Palash Tyagi <ptyagi@macrosynergy.com>
2023-09-19 17:26:26 +0100

    changed imports

--- macrosynergy/pnl/__init__.py
+++ macrosynergy/pnl/__init__.py
@@ -1,12 +1,19 @@
-from typing import List, Union, SupportsInt, SupportsFloat
-import numpy as np
-
-
 from macrosynergy.pnl.naive_pnl import NaivePnL
 
+from macrosynergy.pnl.common import Numeric, NoneType, _short_xcat
 
-NoneType = type(None)
-Numeric = Union[int, float, np.int64, np.float64, SupportsInt, SupportsFloat]
+from macrosynergy.pnl.contract_signals import contract_signals
+from macrosynergy.pnl.notional_positions import notional_positions
+from macrosynergy.pnl.proxy_pnl import proxy_pnl
 
 
-__all__ = ['Numeric', 'NoneType', 'NaivePnL']
+TYPES = [Numeric, NoneType]
+TYPES = [f.__name__ for f in TYPES]
+
+CLASSES = [NaivePnL]
+CLASSES = [f.__name__ for f in CLASSES]
+
+FUNCTIONS = [contract_signals, notional_positions, proxy_pnl]
+FUNCTIONS = [f.__name__ for f in FUNCTIONS]
+
+__all__ = TYPES + CLASSES + FUNCTIONS
