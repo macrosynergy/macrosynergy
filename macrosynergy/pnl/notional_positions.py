d908f9f73f4494b85970746f12bf75daae2f1f4a
Palash Tyagi <ptyagi@macrosynergy.com>
2023-09-19 17:33:07 +0100

    added skeleton code

--- macrosynergy/pnl/notional_positions.py
+++ macrosynergy/pnl/notional_positions.py
@@ -14,7 +14,10 @@ from typing import List, Union, Tuple
 
 from macrosynergy.management.simulate_quantamental_data import make_qdf
 from macrosynergy.management.shape_dfs import reduce_df
-from macrosynergy.pnl import Numeric
+from macrosynergy.management.utils import standardise_dataframe, is_valid_iso_date
+from macrosynergy.panel import historic_vol
+from macrosynergy.pnl import Numeric, _short_xcat
+
 
 def notional_positions(
     df: pd.DataFrame,
@@ -24,16 +27,16 @@ def notional_positions(
     dollar_per_signal: Numeric = 1,
     leverage: Numeric = None,
     vol_target: Numeric = None,
-    rebal_freq: str = 'm', 
+    rebal_freq: str = "m",
     slip: int = 1,
-    lback_periods: int = 21, 
-    lback_meth: str = 'ma', 
+    lback_periods: int = 21,
+    lback_meth: str = "ma",
     half_life=11,
-    rstring: str = 'XR',
+    rstring: str = "XR",
     start: str = None,
     end: str = None,
     blacklist: dict = None,
-    pname: str = 'POS',   
+    pname: str = "POS",
 ):
     """
     Calculates contract positions based on contract signals, AUM and other specs.
@@ -45,7 +48,7 @@ def notional_positions(
     :param <str> sname: the name of the strategy. It must correspond to contract
         signals in the dataframe, which have the format "<cid>_<ctype>_<sname>_CSIG", and
         which are typically calculated by the function contract_signals().
-    :param <list[str]> contids: list of contract identifiers in the format 
+    :param <list[str]> contids: list of contract identifiers in the format
         "<cid>_<ctype>". It must correspond to contract signals in the dataframe.
     :param <float> aum: the assets under management in USD million (for consistency).
         This is basis for all position sizes. Default is 100.
@@ -53,15 +56,15 @@ def notional_positions(
         contract signal value. Default is 1. The default scale has no specific meaning
         and is merely a basis for tryouts.
     :param <float> leverage: the ratio of the sum of notional positions to AUM.
-        This is the main basis for leveraged-based positioning. Since different 
+        This is the main basis for leveraged-based positioning. Since different
         contracts have diferent eexpected volatility and correlations this method
         does not control expected volatility. Default is None, i.e. the method is not
         applied.
     :param <float> vol_target: the target volatility of the portfolio in % of AUM.
         This is the main parameter for volatility-targeted positioning. That method
-        estimates the annualized standard deviation of the signal-based portfolio 
+        estimates the annualized standard deviation of the signal-based portfolio
         for a 1 USD per signal portfolio based on past variances and covariances of
-        the contract returns. The estimation is managed by the function 
+        the contract returns. The estimation is managed by the function
         `historic_portfolio_vol()`.
         Default is None, i.e. the volatility-targeting is not applied.
     :param <str> rebal_freq: the rebalancing frequency. Default is 'm' for monthly.
@@ -75,7 +78,7 @@ def notional_positions(
         of the volatility-targeting method. Default is 21. This passed through to
         the function `historic_portfolio_vol()`.
     :param <str> lback_meth: the method to use for the lookback period of the
-        volatility-targeting method. Default is 'ma' for moving average. Alternative is 
+        volatility-targeting method. Default is 'ma' for moving average. Alternative is
         "xma", for exponential moving average. Again this is passed through to
         the function `historic_portfolio_vol()`.
     :param <int> half_life: the half-life of the exponential moving average for the
@@ -94,24 +97,23 @@ def notional_positions(
     :param <str> pname: the name of the position. Default is 'POS'.
 
     :return: <pd.DataFrame> with the positions for all traded contracts and the
-        specified strategy in USD million. It has the standard JPMaQS DataFrame. 
+        specified strategy in USD million. It has the standard JPMaQS DataFrame.
         The contract signals have the following format "<cid>_<ctype>_<sname>_<pname>".
-    
+
     """
 
     pass
-
 
 
 def historic_portfolio_vol(
     df: pd.DataFrame,
     sname: str,
     contids: List[str],
-    est_freq: str = 'm',
-    lback_periods: int = 21, 
-    lback_meth: str = 'ma', 
+    est_freq: str = "m",
+    lback_periods: int = 21,
+    lback_meth: str = "ma",
     half_life=11,
-    rstring: str = 'XR',
+    rstring: str = "XR",
     start: str = None,
     end: str = None,
     blacklist: dict = None,
@@ -125,7 +127,7 @@ def historic_portfolio_vol(
     :param <str> sname: the name of the strategy. It must correspond to contract
         signals in the dataframe, which have the format "<cid>_<ctype>_<sname>_CSIG", and
         which are typically calculated by the function contract_signals().
-    :param <list[str]> contids: list of contract identifiers in the format 
+    :param <list[str]> contids: list of contract identifiers in the format
         "<cid>_<ctype>". It must correspond to contract signals in the dataframe.
     :param <str> est_freq: the frequency of the volatility estimation. Default is 'm'
         for monthly. Alternatives are 'w' for business weekly, 'd' for daily, and 'q'
@@ -137,7 +139,7 @@ def historic_portfolio_vol(
         of the volatility-targeting method. Default is 21. This passed through to
         the function `historic_portfolio_vol()`.
     :param <str> lback_meth: the method to use for the lookback period of the
-        volatility-targeting method. Default is 'ma' for moving average. Alternative is 
+        volatility-targeting method. Default is 'ma' for moving average. Alternative is
         "xma", for exponential moving average. Again this is passed through to
         the function `historic_portfolio_vol()`.
     :param <str> rstring: a general string of the return category. This identifies
@@ -150,20 +152,56 @@ def historic_portfolio_vol(
         the end date is taken from the dataframe.
     :param <dict> blacklist: a dictionary of contract identifiers to exclude from
         the calculation. Default is None, which means that no contracts are excluded.
-    
+
 
     :return: <pd.DataFrame> with the annualized standard deviations of the portfolios.
         The values are in % annualized. Values between estimation points are forward
-        filled. 
-    
+        filled.
+
     N.B.: If returns in the lookback window are not available the function will replace
     them with the average of the available returns of the same contract type. If no
-    returns are available for a contract type the function will reduce the lookback window 
-    up to a minimum of 11 days. If no returns are available for a contract type for 
-    at least 11 days the function returns an NaN for that date and sends a warning of all 
+    returns are available for a contract type the function will reduce the lookback window
+    up to a minimum of 11 days. If no returns are available for a contract type for
+    at least 11 days the function returns an NaN for that date and sends a warning of all
     the dates for which this happened.
 
-    
+
     """
-    
-    pass
+    ## Check inputs
+    for varx, namex, typex in [
+        (df, "df", pd.DataFrame),
+        (sname, "sname", str),
+        (contids, "contids", list),
+        (est_freq, "est_freq", str),
+        (lback_periods, "lback_periods", int),
+        (lback_meth, "lback_meth", str),
+        (half_life, "half_life", int),
+        (rstring, "rstring", str),
+        (start, "start", str),
+        (end, "end", str),
+        (blacklist, "blacklist", dict),
+    ]:
+        if not isinstance(varx, typex):
+            raise ValueError(f"`{namex}` must be {typex}.")
+        if typex in [str, list, dict] and len(varx) == 0:
+            raise ValueError(f"`{namex}` must not be an empty {str(typex)}.")
+
+    ## Standardize and copy DF
+    df: pd.DataFrame = standardise_dataframe(df.copy())
+
+    ## Check the dates
+    if start is None:
+        start: str = pd.Timestamp(df["real_date"].min()).strftime("%Y-%m-%d")
+    if end is None:
+        end: str = pd.Timestamp(df["real_date"].max()).strftime("%Y-%m-%d")
+
+    for dx, nx in [(start, "start"), (end, "end")]:
+        if not is_valid_iso_date(dx):
+            raise ValueError(f"`{nx}` must be a valid ISO-8601 date string")
+
+    ## Reduce the dataframe
+    df: pd.DataFrame = reduce_df(df=df, start=start, end=end, blacklist=blacklist)
+
+    df["ticker"] = df["cid"] + "_" + df["xcat"]
+
+    df["scat"] = df.apply(lambda x: _short_xcat(ticker=x["ticker"]), axis=1)
