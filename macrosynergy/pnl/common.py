861cd56dae8ebfa24e14fe6f829adbad721ed769
Palash Tyagi <ptyagi@macrosynergy.com>
2023-10-05 14:08:38 +0100

    removed short_xcat from common

--- macrosynergy/pnl/common.py
+++ macrosynergy/pnl/common.py
@@ -49,36 +49,3 @@ class NoneType(type):
 
     def __instancecheck__(cls, instance):
         return instance is None
-
-
-def _short_xcat(
-    ticker: Optional[Union[str, List[str]]] = None,
-    xcat: Optional[Union[str, List[str]]] = None,
-):
-    """
-    Get the short version of the cross-section category.
-
-    :param <str> ticker: the ticker of the contract.
-    :param <str> xcat: a extended category of the contract.
-
-    :return <str>: the category from the ticker/xcat.
-    """
-    if ticker is not None and xcat is not None:
-        raise ValueError("Either `ticker` or `xcat` must be specified, not both")
-
-    if isinstance(ticker, list):
-        return [_short_xcat(ticker=t) for t in ticker]
-    if isinstance(xcat, list):
-        return [_short_xcat(xcat=x) for x in xcat]
-
-    for vx, nx in [(ticker, "ticker"), (xcat, "xcat")]:
-        if vx is not None and not isinstance(vx, str):
-            raise TypeError(f"`{nx}` must be a string, not {type(vx)}")
-
-    if ticker is not None:
-        _, xcat = ticker.split("_", 1)
-        return _short_xcat(xcat=xcat)
-    elif xcat is not None:
-        return xcat.split("_")[0]
-    else:
-        raise ValueError("Either `ticker` or `xcat` must be specified")
