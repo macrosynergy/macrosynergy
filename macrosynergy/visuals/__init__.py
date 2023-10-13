7b78ff4005f227ef7635cd246c3a234dbf22c5f6
Palash Tyagi <ptyagi@macrosynergy.com>
2023-10-13 16:18:29 +0100

    removed type imports from msv.__init__

--- macrosynergy/visuals/__init__.py
+++ macrosynergy/visuals/__init__.py
@@ -1,12 +1,10 @@
 from .facetplot import FacetPlot
 from .lineplot import LinePlot
 from .plotter import Plotter
-from .common import NoneType, Numeric
 from . import view
 
 
-TYPES = ["NoneType", "Numeric"]
 CLASSES = ["FacetPlot", "LinePlot", "Plotter"]
 MODULES = ["view"]
 
-__all__ = TYPES + CLASSES + MODULES
+__all__ = CLASSES + MODULES
