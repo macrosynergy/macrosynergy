77618c6eee620ad36e634401ba6f9c73f3b05422
Palash Tyagi <ptyagi@macrosynergy.com>
2023-10-13 16:18:10 +0100

    removed type defs from msv.common

--- macrosynergy/visuals/common.py
+++ macrosynergy/visuals/common.py
@@ -19,40 +19,10 @@ from typing import (
     get_origin,
 )
 
+from macrosynergy.management.types import NoneType
+
 import numpy as np
 import pandas as pd
-
-
-class NumericType(type):
-    """
-    MetaClass to support type checks across `int`, `float`, `np.int64`, `np.float64`,
-    `SupportsInt`, and `SupportsFloat`.
-    """
-
-    # A tuple of the desired types
-    _numeric_types = (int, float, np.int64, np.float64, SupportsInt, SupportsFloat)
-
-    def __instancecheck__(cls, instance):
-        return isinstance(instance, cls._numeric_types)
-
-
-class Numeric(metaclass=NumericType):
-    """
-    Custom class definition for a numeric type that supports type checks across `int`,
-    `float`, `np.int64`, `np.float64`, `SupportsInt`, and `SupportsFloat`.
-    """
-
-    # Alternatively, use `numbers.Number` directly
-    pass
-
-
-class NoneType(type):
-    """
-    MetaClass to support type checks for `None`.
-    """
-
-    def __instancecheck__(cls, instance):
-        return instance is None
 
 
 def is_matching_subscripted_type(value: Any, type_hint: Type[Any]) -> bool:
