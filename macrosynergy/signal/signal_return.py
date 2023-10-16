60ba431f3b474578cab1e79eafdfd127166fc70f
Samuel Andresen <sandresen@macrosynergy.com>
2023-10-16 17:16:28 +0100

    Added comments to describe what each function does

--- macrosynergy/signal/signal_return.py
+++ macrosynergy/signal/signal_return.py
@@ -31,6 +31,41 @@ class SignalBase:
         fwin: int = 1,
         slip: int = 0,
     ):
+        """
+        Signal base is used as a parent class for both SignalReturns and SignalReturnRelations to inherit variables and methods from.
+
+        :param <pd.Dataframe> df: standardized DataFrame with the following necessary
+            columns: 'cid', 'xcat', 'real_date' and 'value.
+        :param <str> ret: return category.
+        :param <str> sig: primary signal category for which detailed relational statistics
+            can be calculated.
+        :param <bool> cosp: If True the comparative statistics are calculated only for the
+            "communal sample periods", i.e. periods and cross-sections that have values
+            for all compared signals. Default is False.
+        :param <str> start: earliest date in ISO format. Default is None in which case the
+            earliest date available will be used.
+        :param <str> end: latest date in ISO format. Default is None in which case the
+            latest date in the df will be used.
+        :param <dict> blacklist: cross-sections with date ranges that should be excluded from
+            the data frame. If one cross-section has several blacklist periods append numbers
+            to the cross-section code.
+        :param <str> freq: letter denoting frequency at which the series are to be sampled.
+            This must be one of 'D', 'W', 'M', 'Q', 'A'. Default is 'M'.
+            The return series will always be summed over the sample period.
+            The signal series will be aggregated according to the value of agg_sig.
+        :param <str> agg_sig: aggregation method applied to the signal values in down-
+            sampling. The default is "last".
+            If defined, the additional signals will also use the same aggregation method for
+            any down-sampling.
+        :param <int> fwin: forward window of return category in base periods. Default is 1.
+            This conceptually corresponds to the holding period of a position in
+            accordance with the signal.
+        :param <int> slip: implied slippage of feature availability for relationship with
+            the target category. This mimics the relationship between trading signals and
+            returns, which is often characterized by a delay due to the setup of of positions.
+            Technically, this is a negative lag (early arrival) of the target category
+            in working days prior to any frequency conversion. Default is 0.
+        """
         if not isinstance(df, pd.DataFrame):
             raise TypeError(f"DataFrame expected and not {type(df)}.")
 
@@ -110,7 +145,7 @@ class SignalBase:
             df_cs = df
 
         return df_cs
-    
+
     @staticmethod
     def apply_slip(
         df: pd.DataFrame,
@@ -119,17 +154,21 @@ class SignalBase:
         xcats: List[str],
         metrics: List[str],
     ) -> pd.DataFrame:
+        """
+        Function used to call the apply slip method that is defined in management/utils.py
+        """
         return apply_slip_util(
             df=df, slip=slip, cids=cids, xcats=xcats, metrics=metrics, raise_error=False
         )
 
     @staticmethod
     def is_list_of_strings(variable):
-            return isinstance(variable, list) and all(
-                isinstance(item, str) for item in variable
-            )    
-
-    # NOTE THAT THE ORIGINAL __table__stats does not have a ret argument, so this needs to be rectified during inheritance
+        """
+        Function used to test whether a variable is a list of strings, to avoid the compiler saying a string is a list of characters
+        """
+        return isinstance(variable, list) and all(
+            isinstance(item, str) for item in variable
+        )
 
     def __table_stats__(
         self,
@@ -332,7 +371,7 @@ class SignalsReturns(SignalBase):
 
     def single_relation_table(self, ret=None, xcat=None, freq=None, agg_sigs=None):
         """
-        Computes the statistics for a specific panel specified by the arguments
+        Computes all the statistics for a specific panel specified by the arguments:
 
         :param <str> ret: target return category
         :param <str> xcat: signal category to be considered
@@ -412,7 +451,9 @@ class SignalsReturns(SignalBase):
         self, rets=None, xcats=None, freqs=None, agg_sigs=None
     ):
         """
-        Calculates statistics for each return and signal category specified with each frequency and aggregation method
+        Calculates all the statistics for each return and signal category specified with each frequency and aggregation 
+        method, note that if none are defined it does this for all categories, frequencies and aggregation methods that 
+        were stored in the class.
 
         :param <str, List[str]> rets: target return category
         :param <str, List[str]> xcats: signal categories to be considered
@@ -467,9 +508,21 @@ class SignalsReturns(SignalBase):
         columns: List[str] = ["ret", "freq"],
     ):
         """
-        Creates a table which shows the specified statistic for each row and column specified
+        Creates a table which shows the specified statistic for each row and column specified as arguments:
 
-        
+        :param stat: type of statistic to be displayed. (this can be any of the column names of summary_table)
+        :param type: type of the statistic displayed. This can be based on the overall panel ("panel", default), an 
+        average of annual panels (mean_years), an average of cross-sectional relations ("mean_cids"), 
+        the positive ratio across years("pr_years"), positive ratio across sections ("pr_cids").
+        :param <List[str]> rows: row indices, which can be return categories, feature categories, frequencies and/or 
+        aggregations. The choice is made through a list of one or more of "xcat", "ret", "freq" and "agg_sigs". The 
+        default is ["xcat", "agg_sigs"] resulting in index strings (<agg_signs>) or if only one aggregation is 
+        available.
+        :param <List[str]> columns: column indices, which can be return categories, feature categories, 
+        frequencies and/or aggregations. The choice is made through a list of one or more of "xcat", "ret", "freq" and 
+        "agg_sigs". The default is ["ret", "freq] resulting in index strings () or if only one frequency is available.
+
+
         """
         self.df = self.original_df
 
@@ -543,7 +596,7 @@ class SignalsReturns(SignalBase):
 
                 if not isinstance(self.signs, list):
                     self.signs = [self.signs]
-                
+
                 self.signals = [sig]
 
                 if -1 in self.signs:
@@ -555,10 +608,7 @@ class SignalsReturns(SignalBase):
                     self.df.rename(
                         columns=dict(zip(s_copy, self.signals)), inplace=True
                     )
-
-                #################################################################################################
-                #################################################################################################
-                #################################################################################################
+                    
                 if type == "mean_years" or type == "pr_years":
                     cs_type = "years"
                 else:
