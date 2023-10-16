14ae2c0e8cdc1776b9e630378a8b96136c38915d
Palash Tyagi <ptyagi@macrosynergy.com>
2023-10-16 13:03:56 +0100

    updating subpackage init

--- macrosynergy/visuals/__init__.py
+++ macrosynergy/visuals/__init__.py
@@ -2,11 +2,9 @@ from .facetplot import FacetPlot
 from .lineplot import LinePlot
 from .plotter import Plotter
 from .heatmap import Heatmap
-from .common import NoneType, Numeric
 from . import view
 
 
-TYPES = ["NoneType", "Numeric"]
 CLASSES = ["FacetPlot", "LinePlot", "Plotter", "Heatmap"]
 MODULES = []
 
