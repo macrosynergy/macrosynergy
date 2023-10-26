73792ab4196073251887078361f400986f6fbe2b
Palash Tyagi <23239946+Magnus167@users.noreply.github.com>
2023-10-26 23:44:38 +0100

    modifications to test

--- tests/unit/panel/test_granger_causality.py
+++ tests/unit/panel/test_granger_causality.py
@@ -11,6 +11,7 @@ from macrosynergy.panel.granger_causality_tests import (
     _get_tickers,
     _granger_causality_backend,
     granger_causality_test,
+    _type_checks,
 )
 
 
@@ -28,20 +29,7 @@ class TestGrangerCausality(unittest.TestCase):
             end="2020-01-01",
         )
 
-    def test_type_checks(self):
-        # df: pd.DataFrame
-        # tickers: Optional[List[str]]
-        # cids: Optional[List[str]]
-        # xcats: Optional[List[str]]
-        # max_lag: Union[int, List[int]]
-        # add_constant: bool
-        # start: Optional[str]
-        # end: Optional[str]
-        # freq: str
-        # agg: str
-        # metric: str
-
-        good_args: Dict[str, Union[pd.DataFrame, List[str], List[int], bool, str]] = {
+        good_args: Dict[str, Any] = {
             "df": self.df,
             "cids": self.cids[0],
             "xcats": self.xcats[:2],
@@ -54,9 +42,28 @@ class TestGrangerCausality(unittest.TestCase):
             "agg": "mean",
             "metric": "value",
         }
+        self.good_args: Dict[str, Any] = good_args
 
+    def test_type_checks(self):
+        # df: pd.DataFrame
+        # tickers: Optional[List[str]]
+        # cids: Optional[List[str]]
+        # xcats: Optional[List[str]]
+        # max_lag: Union[int, List[int]]
+        # add_constant: bool
+        # start: Optional[str]
+        # end: Optional[str]
+        # freq: str
+        # agg: str
+        # metric: str
+        good_args: Dict[str, Any] = self.good_args.copy()
         # Test that good args work
-        self.assertIsInstance(granger_causality_test(**good_args), dict)
+        self.assertTrue(_type_checks(**good_args))
+
+        good_args2 = good_args.copy()
+        good_args2["cids"] = self.cids[:2]
+        good_args2["xcats"] = self.xcats[0]
+        self.assertTrue(_type_checks(**good_args2))
 
         # Test bad args: all except "max_lag" should raise TypeError on integer input
 
@@ -70,7 +77,7 @@ class TestGrangerCausality(unittest.TestCase):
                 continue
 
             with self.assertRaises(TypeError, msg=f"Key: {key}={bad_args[key]}"):
-                granger_causality_test(**bad_args)
+                _type_checks(**bad_args)
 
         # Test specifying cids, xcats and tickers
         bad_args: Dict[str, Any] = good_args.copy()
@@ -78,19 +85,19 @@ class TestGrangerCausality(unittest.TestCase):
         bad_args["tickers"] = _tickers
 
         with self.assertRaises(ValueError):
-            granger_causality_test(**bad_args)
+            _type_checks(**bad_args)
 
         # Test specifying more than 2 cids
         bad_args = good_args.copy()
         bad_args["cids"] = self.cids
         with self.assertRaises(ValueError):
-            granger_causality_test(**bad_args)
+            _type_checks(**bad_args)
 
         # Test specifying more than 2 xcats
         bad_args = good_args.copy()
         bad_args["xcats"] = self.xcats
         with self.assertRaises(ValueError):
-            granger_causality_test(**bad_args)
+            _type_checks(**bad_args)
 
         # Test specifying more than 2 tickers
         bad_args = good_args.copy()
@@ -98,39 +105,50 @@ class TestGrangerCausality(unittest.TestCase):
             f"{cid}_{xcat}" for cid in self.cids for xcat in self.xcats
         ]
 
-        bad_args.pop("cids")
-        bad_args.pop("xcats")
+        bad_args["cids"] = None
+        bad_args["xcats"] = None
         with self.assertRaises(ValueError):
-            granger_causality_test(**bad_args)
+            _type_checks(**bad_args)
 
         # pass a list of ints for tickers
         bad_args["tickers"] = [1, 2]
         with self.assertRaises(TypeError):
-            granger_causality_test(**bad_args)
+            _type_checks(**bad_args)
 
         # Pass random xcats to test ValueError
         bad_args = good_args.copy()
         bad_args["xcats"] = [u[::-1] for u in self.xcats[:2]]
         with self.assertRaises(ValueError):
-            granger_causality_test(**bad_args)
+            _type_checks(**bad_args)
 
         # Value error when metric="return"
         bad_args = good_args.copy()
         bad_args["metric"] = "return"
         with self.assertRaises(ValueError):
-            granger_causality_test(**bad_args)
+            _type_checks(**bad_args)
 
         # test with max_lag as a list of string
         bad_args = good_args.copy()
         bad_args["max_lag"] = ["apple", "orange"]
         with self.assertRaises(TypeError):
-            granger_causality_test(**bad_args)
+            _type_checks(**bad_args)
 
         # test random strings for dates
         bad_args = good_args.copy()
         bad_args["start"] = "apple"
         with self.assertRaises(ValueError):
-            granger_causality_test(**bad_args)
+            _type_checks(**bad_args)
+
+        bad_args = good_args.copy()
+        bad_args["df"] = bad_args["df"][
+            ~((bad_args["df"]["cid"] == "USD") & (bad_args["df"]["xcat"] == "EQ"))
+        ]
+
+        with self.assertRaises(ValueError):
+            _type_checks(**bad_args)
+
+        bad_args = good_args.copy()
+        bad_args["cids"] = None
 
     def test_get_tickers(self):
         ticks: List[str] = [f"{cid}_{xcat}" for cid in self.cids for xcat in self.xcats]
@@ -193,3 +211,8 @@ class TestGrangerCausality(unittest.TestCase):
         bad_args["add_constant"] = 1
         with self.assertRaises(AssertionError):
             _granger_causality_backend(**bad_args)
+
+    def test_wrapper(self):
+        # all failure cases are already tested in the backend tests or utils tests
+        # run with good args to see if it works
+        granger_causality_test(**self.good_args)
