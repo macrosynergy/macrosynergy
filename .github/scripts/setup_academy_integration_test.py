4b8ca63ab506f18bbf0c8004ed6d1059124e5372
Palash Tyagi <23239946+Magnus167@users.noreply.github.com>
2023-10-28 02:17:24 +0100

    testing with different patterns

--- .github/scripts/setup_academy_integration_test.py
+++ .github/scripts/setup_academy_integration_test.py
@@ -15,6 +15,7 @@ logger = logging.getLogger(__name__)
 NOTEBOOK_URL: str = "https://academy.macrosynergy.com/academy/Introductions/Introduction%20to%20Macrosynergy%20package/_build/html/_sources/Introduction%20to%20Macrosynergy%20package.ipynb"
 TEST_DIR: str = "tests/integration/notebooks"
 
+GH_USER: str = os.environ.get("GH_USERNAME", None)
 GH_TOKEN: str = os.environ.get("GH_TOKEN", None)
 UTILS_REPO: str = "github.com/macrosynergy/msy-utils"
 
@@ -46,35 +47,30 @@ def clone_notebook_runner(
     repo: str,
     branch: str,
     path: str,
-    max_retries: int = 3,
+    user: Optional[str] = GH_USER,
     token: Optional[str] = GH_TOKEN,
 ) -> None:
     """
     Clone the notebook runner repository.
     """
-    if not os.path.exists(path):
-        logger.info("Cloning notebook runner repository.")
-        while max_retries > 0:
-            try:
-                git.Repo.clone_from(f"https://{UTILS_REPO}", path, branch=branch)
-
-                return
-            except Exception as e:
-                logger.warn(
-                    f"Failed to clone notebook runner. Retrying in 5 seconds. Error: {e}"
-                )
-                max_retries -= 1
-                time.sleep(5)
-
-        raise Exception("Failed to clone notebook runner repository.")
-    else:
-        logger.info(
-            "Notebook runner repository already cloned; pulling latest changes."
+    try:
+        git.Repo.clone_from(
+            f"https://{user}:{token}@{repo}",
+            path,
+            branch=branch,
+            depth=1,
         )
-        repo: git.Repo = git.Repo(path)
-        for remote in repo.remotes:
-            remote.fetch()
-            remote.pull()
+
+        logger.info("Cloned notebook runner repository.")
+        return
+    except Exception as excA:
+        try:
+            git.Repo.clone_from(f"https://{repo}", path, branch=branch, depth=1)
+            logger.info("Cloned notebook runner repository.")
+            return
+        except Exception as excB:
+            logger.error(f"Failed to clone notebook runner repository. Error: {excB}")
+            raise excB from excA
 
 
 def setup_test(test_dir: str = TEST_DIR) -> None:
