603e2e1a6637f083275c597784428babb98f19e6
rushilg99 <rushil1.gholkar@gmail.com>
2023-11-02 21:09:04 +0000

    added class documentation

--- macrosynergy/learning/transformers.py
+++ macrosynergy/learning/transformers.py
@@ -13,35 +13,68 @@ from sklearn.base import BaseEstimator, TransformerMixin
 from statsmodels.tools.tools import add_constant
 from statsmodels.regression.mixed_linear_model import MixedLM
 
+from typing import Union
+
 class LassoSelectorTransformer(BaseEstimator, TransformerMixin):
-    def __init__(self, alpha):
+    def __init__(self, alpha: float):
         """
         Transformer class to use the Lasso as a feature selection algorithm.
         Given a hyper-parameter, alpha, the Lasso model is fit and 
         the non-zero coefficients are used to extract features from an input dataframe.
+
+        :param <float> alpha: the regularisation imposed by the Lasso.
         """
         self.alpha = alpha
 
-    def fit(self, X, y):
+    def fit(self, X: pd.DataFrame, y: pd.Series):
         """
         Fit method to fit a Lasso regression and obtain the selected features.
+
+        :param <pd.DataFrame> X: Pandas dataframe of input features.
+        :param <pd.Series> y: Pandas series of targets associated with each
+            sample in X.
         """
         self.p = X.shape[-1]
         self.lasso = Lasso(alpha=self.alpha).fit(X, y)
         self.selected_ftr_idxs = [i for i in range(self.p) if self.lasso.coef_[i] != 0]
 
-    def transform(self, X):
+    def transform(self, X: pd.DataFrame):
         """
         Transform method to return only the selected features of the dataframe.
+
+        :param <pd.DataFrame> X: Pandas dataframe of input features.
+        
+        :return <pd.DataFrame>: Pandas dataframe of input features selected
+            based on the Lasso's feature selection capabilities.
         """
         return X.iloc[:,self.selected_ftr_idxs]
     
 class MapSelectorTransformer(BaseEstimator, TransformerMixin):
-    def __init__(self, threshold):
+    def __init__(self, threshold: float):
+        """
+        Transformer class to select features from a training set
+        based on the Macrosynergy panel test. This test involves creating
+        a linear mixed effects model with period-specific random effects to 
+        account for cross-sectional correlations. The p-value for the slope
+        parameter is used to perform the significance test.
+
+        :param <float> threshold: Significance threshold. This should be in
+            the interval (0,1).
+        """
+        if (threshold <= 0) or (threshold >= 1):
+            raise ValueError("The threshold must be in between 0 and 1.")
+        
         self.threshold = threshold
 
-    def fit(self, X, y):
-        # determine the ftrs that are significant
+    def fit(self, X: pd.DataFrame, y: pd.Series):
+        """
+        Fit method to assess significance of each feature using
+        the Macrosynergy panel test.
+
+        :param <pd.DataFrame> X: Pandas dataframe of input features.
+        :param <pd.Series> y: Pandas series of targets associated
+            with each sample in X. 
+        """
         self.ftrs = []
         cols = X.columns
 
@@ -55,7 +88,15 @@ class MapSelectorTransformer(BaseEstimator, TransformerMixin):
                 self.ftrs.append(col)
 
 
-    def transform(self, X):
+    def transform(self, X: pd.DataFrame):
+        """
+        Transform method to return the significant training features.
+
+        :param <pd.DataFrame> X: Pandas dataframe of input features.
+
+        :return <pd.DataFrame>: Pandas dataframe of input features selected
+            based on the Macrosynergy panel test.
+        """
         return X[self.ftrs]
     
 if __name__ == "__main__":
