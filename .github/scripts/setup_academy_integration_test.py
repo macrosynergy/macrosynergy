d344a7228165f7db98dba05eb7db596f21c14f7e
Samuel Andresen <sandresen@macrosynergy.com>
2023-11-06 13:07:41 +0000

    Running notebook

--- .github/scripts/setup_academy_integration_test.py
+++ .github/scripts/setup_academy_integration_test.py
@@ -19,6 +19,14 @@ GH_USER: str = os.environ.get("GH_USERNAME", None)
 GH_TOKEN: str = os.environ.get("GH_TOKEN", None)
 UTILS_REPO: str = "github.com/macrosynergy/msy-utils"
 
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+import sys
+import os
+from glob import glob
+import logging
+from typing import List
+
 
 def download_notebook(url: str, filename: str, max_retries: int = 3) -> None:
     """
@@ -57,7 +65,7 @@ def clone_notebook_runner(
         print("user is", user)
         print("token is", token)
         git.Repo.clone_from(
-            f"https://{token}@{repo}",
+            f"https://{user}:{token}@{repo}",
             path,
             branch=branch,
             depth=1,
@@ -85,9 +93,40 @@ def setup_test(test_dir: str = TEST_DIR) -> None:
     download_notebook(NOTEBOOK_URL, notebook_filename)
     print("Downloaded notebook.")
 
-    clone_notebook_runner(UTILS_REPO, "feature/notebook_script", "notebook-runner")
+    #clone_notebook_runner(UTILS_REPO, "feature/notebook_script", "notebook-runner")
 
     logger.info("Integration test setup complete.")
+
+def execute_notebook(nb):
+    try:
+        ep = ExecutePreprocessor(timeout=600, kernel_name="python3")
+        ep.preprocess(nb, {"metadata": {"path": "./"}})
+        return None  # No errors
+    except Exception as e:
+        return str(e)
+
+
+def run_notebooks(root_dir: str = "./"):
+    notebooks: List[str] = glob(os.path.join(root_dir, "**/*.ipynb"), recursive=True)
+
+    logger.info(f"Found {len(notebooks)} notebooks")
+    error_notebooks = []
+    for notebook in notebooks:
+        if "r_code" in notebook:
+            logger.info(f"Skipping R notebook: {notebook}")
+            continue
+
+        logger.info(f"Executing notebook: {notebook}")
+        with open(notebook, "r", encoding="utf-8") as f:
+            nb = nbformat.read(f, as_version=nbformat.NO_CONVERT)
+        errors = execute_notebook(nb)
+        if errors is not None:
+            logger.info(f"{notebook} raised {errors}")
+            error_notebooks.append((notebook, errors))
+        else:
+            logger.info(f"{notebook} ran successfully")
+            with open(notebook, "w", encoding="utf-8") as f:
+                nbformat.write(nb, f)
 
 
 def run_test(test_dir: str = TEST_DIR) -> None:
@@ -95,7 +134,8 @@ def run_test(test_dir: str = TEST_DIR) -> None:
     Run the integration test.
     """
     logger.info("Running integration test.")
-    os.system(f"python notebook-runner/notebook_scripts/run_notebooks.py {test_dir}")
+    run_notebooks(test_dir)
+    #os.system(f"python notebook-runner/notebook_scripts/run_notebooks.py {test_dir}")
     logger.info("Integration test complete.")
 
 
