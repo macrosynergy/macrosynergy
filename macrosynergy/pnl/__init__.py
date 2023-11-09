564d7f5e361ff84da83fd5736276b9cb9469b8da
Palash Tyagi <ptyagi@macrosynergy.com>
2023-11-09 11:12:59 +0000

    updated ms.pnl.init

--- macrosynergy/pnl/__init__.py
+++ macrosynergy/pnl/__init__.py
@@ -1,10 +1,8 @@
 from macrosynergy.pnl.naive_pnl import NaivePnL
 
 from macrosynergy.pnl.contract_signals import contract_signals
-from macrosynergy.pnl.notional_positions import (
-    notional_positions,
-    historic_portfolio_vol,
-)
+from macrosynergy.pnl.notional_positions import notional_positions
+from macrosynergy.pnl.historic_portfolio_volatility import historic_portfolio_vol
 from macrosynergy.pnl.proxy_pnl import proxy_pnl
 
 
