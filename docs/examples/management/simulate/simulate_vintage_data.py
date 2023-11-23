db9b3e53bd56d78557b09b86eb93afe20c2b202b
Palash Tyagi <ptyagi@macrosynergy.com>
2023-11-23 18:15:38 +0000

    Refactor code for generating vintage data

--- docs/examples/management/simulate/simulate_vintage_data.py
+++ docs/examples/management/simulate/simulate_vintage_data.py
@@ -19,12 +19,9 @@ vins_m = VintageData(
 
 dfm1 = vins_m.make_grade1()
 
-
 dfm1.groupby("release_date").agg(["mean", "count"])
 
-
 dfm2 = vins_m.make_grade2()
-
 
 dfmg = vins_m.make_graded(grading=[3, 2.1, 1], upgrades=[12, 24])
 
@@ -42,12 +39,9 @@ vins_q = VintageData(
 
 dfq1 = vins_q.make_grade1()
 
-
 dfq1.groupby("release_date").agg(["mean", "count"])
 
-
 dfq2 = vins_q.make_grade2()
-
 
 vins_w = VintageData(
     "USD_INDX_SA",
@@ -63,9 +57,7 @@ vins_w = VintageData(
 
 dfw1 = vins_w.make_grade1()
 
-
 dfw1.groupby("release_date").agg(["mean", "count"])
-
 
 dfw2 = vins_w.make_grade2()
 
