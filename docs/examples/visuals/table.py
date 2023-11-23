5a6b7934e51eb6e23fb1ba3b31276573fee9aaaa
Palash Tyagi <ptyagi@macrosynergy.com>
2023-11-23 16:53:26 +0000

    Add import statement and view_table function call

--- docs/examples/visuals/table.py
+++ docs/examples/visuals/table.py
@@ -1,4 +1,6 @@
 """example/macrosynergy/visuals/table.py"""
+import pandas as pd
+from macrosynergy.visuals import view_table
 
 data = {
     "Col1": [1, 2, 3, 4],
@@ -6,8 +8,12 @@ data = {
     "Col3": [9, 10, 11, 12],
     "Col4": [13, 14, 15, 16],
 }
+
+
 row_labels = ["Row1", "Row2", "Row3", "Row4"]
 
+
 df = pd.DataFrame(data, index=row_labels)
+
 
 view_table(df, title="Table")
