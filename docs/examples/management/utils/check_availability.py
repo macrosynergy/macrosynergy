355614092b82737d7d6a37b3ad6ab15ef28e31c2
Palash Tyagi <ptyagi@macrosynergy.com>
2023-11-23 18:16:42 +0000

    Refactor data initialization in check_availability.py

--- docs/examples/management/utils/check_availability.py
+++ docs/examples/management/utils/check_availability.py
@@ -14,35 +14,21 @@ import pandas as pd
 
 
 cids = ["AUD", "CAD", "GBP"]
-
-
 xcats = ["XR", "CRY"]
 
 
 cols_1 = ["earliest", "latest", "mean_add", "sd_mult"]
 
-
 df_cids = pd.DataFrame(index=cids, columns=cols_1)
-
-
 df_cids.loc["AUD",] = ["2010-01-01", "2020-12-31", 0.5, 2]
-
-
 df_cids.loc["CAD",] = ["2010-01-01", "2020-11-30", 0, 1]
-
-
 df_cids.loc["GBP",] = ["2012-01-01", "2020-11-30", -0.2, 0.5]
 
 
 cols_2 = cols_1 + ["ar_coef", "back_coef"]
 
-
 df_xcats = pd.DataFrame(index=xcats, columns=cols_2)
-
-
 df_xcats.loc["XR",] = ["2010-01-01", "2020-12-31", 0, 1, 0, 0.3]
-
-
 df_xcats.loc["CRY",] = ["2011-01-01", "2020-10-30", 1, 2, 0.9, 0.5]
 
 
@@ -56,7 +42,6 @@ dfd.loc[filt_na, "value"] = pd.NA
 
 
 xxcats = xcats + ["TREND"]
-
 
 xxcids = cids + ["USD"]
 
