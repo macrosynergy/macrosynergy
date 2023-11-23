c58120f372199ffd0811a65c153a5886763a64b6
Palash Tyagi <ptyagi@macrosynergy.com>
2023-11-23 18:21:03 +0000

    Refactor category_relations.py

--- docs/examples/panel/category_relations.py
+++ docs/examples/panel/category_relations.py
@@ -2,60 +2,34 @@
 
 
 from macrosynergy.management.simulate import make_qdf
-
-
 from macrosynergy.panel.category_relations import CategoryRelations
-
-
 import pandas as pd
 
 
 cids = ["AUD", "CAD", "GBP", "NZD", "USD"]
-
-
 xcats = ["XR", "CRY", "GROWTH", "INFL"]
 
+cols = ["earliest", "latest", "mean_add", "sd_mult"]
 
-df_cids = pd.DataFrame(
-    index=cids, columns=["earliest", "latest", "mean_add", "sd_mult"]
-)
+df_cids = pd.DataFrame(index=cids, columns=cols)
 
 
 df_cids.loc["AUD"] = ["2000-01-01", "2020-12-31", 0.1, 1]
-
-
 df_cids.loc["CAD"] = ["2001-01-01", "2020-11-30", 0, 1]
-
-
 df_cids.loc["BRL"] = ["2001-01-01", "2020-11-30", -0.1, 2]
-
-
 df_cids.loc["GBP"] = ["2002-01-01", "2020-11-30", 0, 2]
-
-
 df_cids.loc["NZD"] = ["2002-01-01", "2020-09-30", -0.1, 2]
-
-
 df_cids.loc["USD"] = ["2003-01-01", "2020-12-31", -0.1, 2]
 
-
-cols = ["earliest", "latest", "mean_add", "sd_mult", "ar_coef", "back_coef"]
-
+cols += ["ar_coef", "back_coef"]
 
 df_xcats = pd.DataFrame(index=xcats, columns=cols)
 
 
 df_xcats.loc["XR"] = ["2000-01-01", "2020-12-31", 0.1, 1, 0, 0.3]
-
-
 df_xcats.loc["CRY"] = ["2000-01-01", "2020-10-30", 1, 2, 0.95, 1]
-
-
 df_xcats.loc["GROWTH"] = ["2001-01-01", "2020-10-30", 1, 2, 0.9, 1]
-
-
 df_xcats.loc["INFL"] = ["2001-01-01", "2020-10-30", 1, 2, 0.8, 0.5]
-
 
 dfd = make_qdf(df_cids, df_xcats, back_ar=0.75)
 
@@ -70,8 +44,6 @@ black = {"AUD": ["2000-01-01", "2003-12-31"], "GBP": ["2018-01-01", "2100-01-01"
 
 
 filt1 = (dfd["xcat"] == "GROWTH") & (dfd["cid"] == "AUD")
-
-
 filt2 = (dfd["xcat"] == "INFL") & (dfd["cid"] == "NZD")
 
 
