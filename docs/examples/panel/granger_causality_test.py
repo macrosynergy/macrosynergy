2e4ea3de8d2214f51b67cfc4582967f80c8a6aff
Palash Tyagi <ptyagi@macrosynergy.com>
2023-11-23 15:50:37 +0000

    Refactor granger_causality_test.py and update test data

--- docs/examples/panel/granger_causality_test.py
+++ docs/examples/panel/granger_causality_test.py
@@ -1,28 +1,33 @@
 """example/macrosynergy/panel/granger_causality_test.py"""
 
-cids: List[str] = ["AUD"]
-xcats: List[str] = ["FX", "EQ"]
+import pandas as pd
 
-df: pd.DataFrame = make_test_df(
+from macrosynergy.management.simulate import make_test_df
+from macrosynergy.panel.granger_causality_test import granger_causality_test
+
+cids = ["AUD"]
+xcats = ["FX", "EQ"]
+
+df = make_test_df(
     cids=cids,
     xcats=xcats,
 )
 
-gct: Dict[Any, Any] = granger_causality_test(
+gct = granger_causality_test(
     df=df,
     cids=cids,
     xcats=xcats,
 )
 
-cids: List[str] = ["AUD", "CAD"]
-xcats: str = "FX"
-# tickers =  AUD_FX, CAD_FX
-df: pd.DataFrame = make_test_df(
+cids = ["AUD", "CAD"]
+xcats = "FX"
+
+df = make_test_df(
     cids=cids,
     xcats=xcats,
 )
 
-gct: Dict[Any, Any] = granger_causality_test(
+gct = granger_causality_test(
     df=df,
     tickers=["AUD_FX", "CAD_FX"],
 )
