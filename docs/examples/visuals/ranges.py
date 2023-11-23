ee3779e27c78a40ebe6e95a331b3cb816f548560
Palash Tyagi <ptyagi@macrosynergy.com>
2023-11-23 16:53:37 +0000

    Add imports and update dataframes in ranges.py

--- docs/examples/visuals/ranges.py
+++ docs/examples/visuals/ranges.py
@@ -1,23 +1,45 @@
 """example/macrosynergy/visuals/ranges.py"""
+import pandas as pd
+from macrosynergy.management.simulate import make_qdf
+from macrosynergy.visuals import view_ranges
 
 cids = ["AUD", "CAD", "GBP", "USD"]
+
+
 xcats = ["XR", "CRY"]
+
+
 df_cids = pd.DataFrame(
     index=cids, columns=["earliest", "latest", "mean_add", "sd_mult"]
 )
+
+
 df_cids.loc["AUD",] = ["2010-01-01", "2020-12-31", 0.5, 0.2]
+
+
 df_cids.loc["CAD",] = ["2011-01-01", "2020-11-30", 0, 1]
+
+
 df_cids.loc["GBP",] = ["2012-01-01", "2020-11-30", 0, 2]
+
+
 df_cids.loc["USD",] = ["2012-01-01", "2020-11-30", 1, 2]
+
 
 df_xcats = pd.DataFrame(
     index=xcats,
     columns=["earliest", "latest", "mean_add", "sd_mult", "ar_coef", "back_coef"],
 )
+
+
 df_xcats.loc["XR",] = ["2010-01-01", "2020-12-31", 0, 1, 0, 0.3]
+
+
 df_xcats.loc["CRY",] = ["2011-01-01", "2020-10-30", 1, 2, 0.9, 0.5]
 
+
 dfd = make_qdf(df_cids, df_xcats, back_ar=0.75)
+
 
 view_ranges(
     dfd,
@@ -28,8 +50,12 @@ view_ranges(
     sort_cids_by="std",
 )
 
+
 filter_1 = (dfd["xcat"] == "XR") & (dfd["cid"] == "AUD")
+
+
 dfd = dfd[~filter_1]
+
 
 view_ranges(
     dfd,
