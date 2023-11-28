f3153a05846aeec380e250a562ebb0b6396a37d4
Palash Tyagi <ptyagi@macrosynergy.com>
2023-11-28 16:59:45 +0000

    smol changes

--- docs/scripts/build_examples.py
+++ docs/scripts/build_examples.py
@@ -9,6 +9,7 @@ import shutil
 import subprocess
 import glob
 from typing import List, Dict, Any, Optional
+from concurrent.futures import ProcessPoolExecutor, as_completed
 
 INPUT_DIR = "./docs/examples"
 OUTPUT_DIR = "./docs/examples.build"
@@ -35,15 +36,21 @@ def convert_to_notebook(
     output_dir: str,
     input_dir: str,
     output_extension: str = ".ipynb",
-) -> None:
+) -> str:
     """Convert a file to a notebook.
     :param <str> file_path: The file path.
     :param <str> output_dir: The output directory.
     :param <str> output_extension: The output extension.
+
+    :return <str>: The output path.
     """
     # set the output_path such that the relative path is preserved
-    output_path = os.path.join(output_dir, os.path.relpath(file_path, input_dir))
-    output_path = os.path.splitext(output_path)[0] + output_extension
+    output_path = (
+        os.path.splitext(
+            os.path.join(output_dir, os.path.relpath(file_path, input_dir))
+        )[0]
+        + output_extension
+    )
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
 
     subprocess.run(
@@ -57,6 +64,15 @@ def convert_to_notebook(
         ],
         check=True,
     )
+    return output_path
+
+
+def apply_black(output_dir: str) -> None:
+    """Apply black to all notebooks in a directory.
+    :param <str> output_dir: The output directory.
+    """
+    # apply black to the whole directory
+    subprocess.run(["black", output_dir], check=True)
 
 
 def convert_all_files(
@@ -70,15 +86,32 @@ def convert_all_files(
     :param <str> output_format: The output format.
     :param <str> output_extension: The output extension.
     """
+    if os.path.exists(output_dir):
+        shutil.rmtree(output_dir)
     os.makedirs(output_dir, exist_ok=True)
 
-    for file_path in get_files(input_dir):
-        convert_to_notebook(
-            file_path=file_path,
-            output_dir=output_dir,
-            input_dir=input_dir,
-            output_extension=output_extension,
-        )
+    # for file_path in get_files(input_dir):
+    #     convert_to_notebook(
+    #         file_path=file_path,
+    #         output_dir=output_dir,
+    #         input_dir=input_dir,
+    #         output_extension=output_extension,
+    #     )
+    with ProcessPoolExecutor() as executor:
+        futures = [
+            executor.submit(
+                convert_to_notebook,
+                file_path=file_path,
+                output_dir=output_dir,
+                input_dir=input_dir,
+                output_extension=output_extension,
+            )
+            for file_path in get_files(input_dir)
+        ]
+        for future in as_completed(futures):
+            future.result()
+
+    apply_black(output_dir)
 
 
 if __name__ == "__main__":
