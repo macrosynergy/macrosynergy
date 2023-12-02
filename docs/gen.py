48d437bd60824109e3b94dca76205c6d832888c6
Palash Tyagi <ptyagi@macrosynergy.com>
2023-12-02 05:39:29 +0000

    updating gen.py

--- docs/gen.py
+++ docs/gen.py
@@ -1,16 +1,17 @@
-# sphinx-apidoc -o docs/source/rsts -fMeT macrosynergy
-# cd docs
-# make.bat html
-
 import os
 
+OUTPUT_DIR = "./docs/source/rsts"
 
-makecom = "make" + ".bat" if os.name == "nt" else ""
+os.makedirs(OUTPUT_DIR, exist_ok=True)
 
-rst_gen = "sphinx-apidoc -o docs/source/rsts -fMeT macrosynergy"
+# make command for windows or linux
+makecommand = "make" + (".bat" if os.name == "nt" else "") + " html"
 
-makecommand = f"{makecom} html"
+# command to generate rst files
+rst_gen = f"sphinx-apidoc -o {OUTPUT_DIR} -fMeT macrosynergy"
 
-os.system(rst_gen)
-os.chdir("docs")
-os.system(makecommand)
+os.system(rst_gen)  # generate rst files
+current_dir = os.getcwd()  # get current directory
+os.chdir("docs")  # change directory to docs
+os.system(makecommand)  # make html
+os.chdir(current_dir)  # change directory back to original
