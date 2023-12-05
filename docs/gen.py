96063f139cc76ea2f643914cfa11e0153cb02172
Palash Tyagi <ptyagi@macrosynergy.com>
2023-12-05 10:56:08 +0000

    Refactor documentation generation process

--- docs/gen.py
+++ docs/gen.py
@@ -10,44 +10,11 @@ parser.add_argument(
     default=False,
 )
 
-parser.add_argument(
-    "--clean",
-    action="store_true",
-    help="Clean documentation before generation.",
-    default=False,
-)
-
-parser.add_argument(
-    "--rebuild",
-    action="store_true",
-    help="Rebuild documentation before generation.",
-    default=True,
-)
-
-# remove rst opt
-parser.add_argument(
-    "--remove-rst",
-    action="store_true",
-    help="Remove rst files after generation.",
-    default=False,
-)
-
-parser.add_argument(
-    "--no-make",
-    action="store_true",
-    help="Do not run make html after generation.",
-    default=False,
-)
-
 OUTPUT_DIR = "./docs/source/gen_rsts"
 README = "./README.md"
 args = parser.parse_args()
 
-CLEAN_PREVIOUS = args.clean
-REMOVE_RST = args.remove_rst
 SHOW = args.show
-REBUILD = args.rebuild
-NO_MAKE = args.no_make
 
 #####################
 
@@ -62,11 +29,7 @@ makeclean = makescript + " clean"
 # command to generate rst files
 rst_gen = f"sphinx-apidoc -o {OUTPUT_DIR} -fMeT macrosynergy"
 
-# generate rst files
-# if there are any rsts in the output dir, then we assume that the rst files are
-# already generated and we don't need to do it again
-if len(os.listdir(OUTPUT_DIR)) == 0 or REBUILD:
-    os.system(rst_gen)
+os.system(rst_gen)
 
 # copy readme to rst
 shutil.copy(README, OUTPUT_DIR)
@@ -75,24 +38,15 @@ shutil.copy(README, OUTPUT_DIR)
 current_dir = os.getcwd()
 
 # change directory to docs
-os.chdir("docs")
+os.chdir("./docs")
 
 # clean docs
-if CLEAN_PREVIOUS:
-    os.system(makeclean)
-
+os.system(makeclean)
 # make html
-if not NO_MAKE:
-    os.system(makehtml)
+os.system(makehtml)
 
 # change directory back to original
 os.chdir(current_dir)
-
-# remove rst files
-if REMOVE_RST:
-    for file in os.listdir(OUTPUT_DIR):
-        if file.endswith(".rst"):
-            os.remove(os.path.join(OUTPUT_DIR, file))
 
 # print success message with path to index.html
 
