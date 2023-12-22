43ce958ac34c5e3441f8c19caab04df6e3e2b1b2
Palash Tyagi <ptyagi@macrosynergy.com>
2023-12-22 13:42:49 +0000

    updating build.bat

--- docs.old/scripts/build.bat
+++ docs.old/scripts/build.bat
@@ -1,7 +1,7 @@
-python.exe .\docs\scripts\build_md.py
-python.exe .\docs\scripts\build_jpb.py
-jupyter-book build .\docs\build\
-python.exe .\docs\scripts\post_build.py
+python.exe .\docs.old\scripts\build_md.py
+python.exe .\docs.old\scripts\build_jpb.py
+jupyter-book build .\docs.old\build\
+python.exe .\docs.old\scripts\post_build.py
 
 @REM jupyter-book config sphinx macrosynergy
 @REM .\scripts\build.bat
