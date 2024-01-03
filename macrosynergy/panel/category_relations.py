8634f463d7dde4c346f61f5792f4f785399905b9
Samuel Andresen <sandresen@macrosynergy.com>
2024-01-03 13:55:36 +0000

    Change main back to same as before

--- macrosynergy/panel/category_relations.py
+++ macrosynergy/panel/category_relations.py
@@ -729,151 +729,107 @@ class CategoryRelations(object):
 
 
 if __name__ == "__main__":
-    # cids = ["AUD", "CAD", "GBP", "NZD", "USD"]
-    # xcats = ["XR", "CRY", "GROWTH", "INFL"]
-    # df_cids = pd.DataFrame(
-    #     index=cids, columns=["earliest", "latest", "mean_add", "sd_mult"]
-    # )
-    # df_cids.loc["AUD"] = ["2000-01-01", "2020-12-31", 0.1, 1]
-    # df_cids.loc["CAD"] = ["2001-01-01", "2020-11-30", 0, 1]
-    # df_cids.loc["BRL"] = ["2001-01-01", "2020-11-30", -0.1, 2]
-    # df_cids.loc["GBP"] = ["2002-01-01", "2020-11-30", 0, 2]
-    # df_cids.loc["NZD"] = ["2002-01-01", "2020-09-30", -0.1, 2]
-    # df_cids.loc["USD"] = ["2003-01-01", "2020-12-31", -0.1, 2]
+    cids = ["AUD", "CAD", "GBP", "NZD", "USD"]
+    xcats = ["XR", "CRY", "GROWTH", "INFL"]
+    df_cids = pd.DataFrame(
+        index=cids, columns=["earliest", "latest", "mean_add", "sd_mult"]
+    )
+    df_cids.loc["AUD"] = ["2000-01-01", "2020-12-31", 0.1, 1]
+    df_cids.loc["CAD"] = ["2001-01-01", "2020-11-30", 0, 1]
+    df_cids.loc["BRL"] = ["2001-01-01", "2020-11-30", -0.1, 2]
+    df_cids.loc["GBP"] = ["2002-01-01", "2020-11-30", 0, 2]
+    df_cids.loc["NZD"] = ["2002-01-01", "2020-09-30", -0.1, 2]
+    df_cids.loc["USD"] = ["2003-01-01", "2020-12-31", -0.1, 2]
 
-    # cols = ["earliest", "latest", "mean_add", "sd_mult", "ar_coef", "back_coef"]
-    # df_xcats = pd.DataFrame(index=xcats, columns=cols)
-    # df_xcats.loc["XR"] = ["2000-01-01", "2020-12-31", 0.1, 1, 0, 0.3]
-    # df_xcats.loc["CRY"] = ["2000-01-01", "2020-10-30", 1, 2, 0.95, 1]
-    # df_xcats.loc["GROWTH"] = ["2001-01-01", "2020-10-30", 1, 2, 0.9, 1]
-    # df_xcats.loc["INFL"] = ["2001-01-01", "2020-10-30", 1, 2, 0.8, 0.5]
+    cols = ["earliest", "latest", "mean_add", "sd_mult", "ar_coef", "back_coef"]
+    df_xcats = pd.DataFrame(index=xcats, columns=cols)
+    df_xcats.loc["XR"] = ["2000-01-01", "2020-12-31", 0.1, 1, 0, 0.3]
+    df_xcats.loc["CRY"] = ["2000-01-01", "2020-10-30", 1, 2, 0.95, 1]
+    df_xcats.loc["GROWTH"] = ["2001-01-01", "2020-10-30", 1, 2, 0.9, 1]
+    df_xcats.loc["INFL"] = ["2001-01-01", "2020-10-30", 1, 2, 0.8, 0.5]
 
-    # dfd = make_qdf(df_cids, df_xcats, back_ar=0.75)
-    # dfd["grading"] = np.ones(dfd.shape[0])
-    # black = {"AUD": ["2000-01-01", "2003-12-31"], "GBP": ["2018-01-01", "2100-01-01"]}
+    dfd = make_qdf(df_cids, df_xcats, back_ar=0.75)
+    dfd["grading"] = np.ones(dfd.shape[0])
+    black = {"AUD": ["2000-01-01", "2003-12-31"], "GBP": ["2018-01-01", "2100-01-01"]}
 
-    # # All AUD GROWTH locations.
-    # filt1 = (dfd["xcat"] == "GROWTH") & (dfd["cid"] == "AUD")
-    # filt2 = (dfd["xcat"] == "INFL") & (dfd["cid"] == "NZD")
+    # All AUD GROWTH locations.
+    filt1 = (dfd["xcat"] == "GROWTH") & (dfd["cid"] == "AUD")
+    filt2 = (dfd["xcat"] == "INFL") & (dfd["cid"] == "NZD")
 
-    # # Reduced DataFrame.
-    # dfdx = dfd[~(filt1 | filt2)].copy()
-    # dfdx["ERA"]: str = "before 2007"
-    # dfdx.loc[dfdx["real_date"].dt.year > 2007, "ERA"] = "from 2010"
+    # Reduced DataFrame.
+    dfdx = dfd[~(filt1 | filt2)].copy()
+    dfdx["ERA"]: str = "before 2007"
+    dfdx.loc[dfdx["real_date"].dt.year > 2007, "ERA"] = "from 2010"
 
-    # cidx = ["AUD", "CAD", "GBP", "USD"]
-
-    # cr = CategoryRelations(
-    #     dfdx,
-    #     xcats=["CRY", "XR"],
-    #     freq="M",
-    #     lag=1,
-    #     cids=cidx,
-    #     xcat_aggs=["mean", "sum"],
-    #     start="2001-01-01",
-    #     blacklist=black,
-    #     years=None,
-    # )
-
-    # cr.reg_scatter(
-    #     labels=False,
-    #     separator=None,
-    #     title="Carry and Return",
-    #     xlab="Carry",
-    #     ylab="Return",
-    #     coef_box="lower left",
-    #     prob_est="map",
-    # )
-
-    # # years parameter
-
-    # cr = CategoryRelations(
-    #     dfdx,
-    #     xcats=["CRY", "XR"],
-    #     freq="M",
-    #     years=5,
-    #     lag=0,
-    #     cids=cidx,
-    #     xcat_aggs=["mean", "sum"],
-    #     start="2001-01-01",
-    #     blacklist=black,
-    # )
-
-    # cr.reg_scatter(
-    #     labels=False,
-    #     separator=None,
-    #     title="Carry and Return, 5-year periods",
-    #     xlab="Carry",
-    #     ylab="Return",
-    #     coef_box="lower left",
-    #     prob_est="map",
-    # )
-
-    # cr = CategoryRelations(
-    #     dfdx,
-    #     xcats=["CRY", "XR"],
-    #     xcat1_chg="diff",
-    #     freq="M",
-    #     lag=1,
-    #     cids=cidx,
-    #     xcat_aggs=["mean", "sum"],
-    #     start="2001-01-01",
-    #     blacklist=black,
-    #     years=None,
-    # )
-
-    # cr.reg_scatter(
-    #     labels=False,
-    #     separator=2016,
-    #     title="Carry and Return",
-    #     xlab="Carry",
-    #     ylab="Return",
-    #     coef_box="lower left",
-    # )
-
-    # cr.ols_table(type="pool")
-    # cr.ols_table(type="re")
-
-    feats = [
-        "XGHIPC",
-        "XGHI",
-        "INTRGDPv5Y_NSA_P1M1ML12_3MMA",
-        "CPIH_SA_PALLvIET",
-        "CPIC_SA_PALLvIET",
-        "PCREDITBN_SJA_P1M1ML12vLTB",
-    ]
-
-    main_feat = feats[0]
-    target = "DU02YXR_VT10"
-    cids_sel = ["EUR", "USD"]
-
-    df = pd.read_csv("./macrosynergy/panel/test.csv")
-    fxblack = {'CHF': (Timestamp('2011-10-03 00:00:00'), Timestamp('2015-01-30 00:00:00')),
-        'CZK': (Timestamp('2014-01-01 00:00:00'), Timestamp('2017-07-31 00:00:00')),
-        'ILS': (Timestamp('2000-01-03 00:00:00'), Timestamp('2005-12-30 00:00:00')),
-        'INR': (Timestamp('2000-01-03 00:00:00'), Timestamp('2004-12-31 00:00:00')),
-        'THB': (Timestamp('2007-01-01 00:00:00'), Timestamp('2008-11-28 00:00:00')),
-        'TRY_1': (Timestamp('2000-01-03 00:00:00'), Timestamp('2003-09-30 00:00:00')),
-        'TRY_2': (Timestamp('2020-01-01 00:00:00'), Timestamp('2022-08-01 00:00:00'))}
+    cidx = ["AUD", "CAD", "GBP", "USD"]
 
     cr = CategoryRelations(
-        df,
-        xcats=[main_feat, target],
-        cids=cids_sel,
+        dfdx,
+        xcats=["CRY", "XR"],
         freq="M",
         lag=1,
-        xcat_aggs=["last", "sum"],
-        blacklist=fxblack,
-        start="2000-01-01",
-        xcat_trims=[None, None],
+        cids=cidx,
+        xcat_aggs=["mean", "sum"],
+        start="2001-01-01",
+        blacklist=black,
+        years=None,
     )
-
 
     cr.reg_scatter(
         labels=False,
-        coef_box="upper left",
-        title=None,
-        xlab=None,
-        ylab=None,
-        separator=2010,
+        separator=None,
+        title="Carry and Return",
+        xlab="Carry",
+        ylab="Return",
+        coef_box="lower left",
+        prob_est="map",
     )
+
+    # years parameter
+
+    cr = CategoryRelations(
+        dfdx,
+        xcats=["CRY", "XR"],
+        freq="M",
+        years=5,
+        lag=0,
+        cids=cidx,
+        xcat_aggs=["mean", "sum"],
+        start="2001-01-01",
+        blacklist=black,
+    )
+
+    cr.reg_scatter(
+        labels=False,
+        separator=None,
+        title="Carry and Return, 5-year periods",
+        xlab="Carry",
+        ylab="Return",
+        coef_box="lower left",
+        prob_est="map",
+    )
+
+    cr = CategoryRelations(
+        dfdx,
+        xcats=["CRY", "XR"],
+        xcat1_chg="diff",
+        freq="M",
+        lag=1,
+        cids=cidx,
+        xcat_aggs=["mean", "sum"],
+        start="2001-01-01",
+        blacklist=black,
+        years=None,
+    )
+
+    cr.reg_scatter(
+        labels=False,
+        separator=2016,
+        title="Carry and Return",
+        xlab="Carry",
+        ylab="Return",
+        coef_box="lower left",
+    )
+
+    cr.ols_table(type="pool")
+    cr.ols_table(type="re")
