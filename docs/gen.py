931187a887afdef8d25eac9443bec2ab62de1dd3
Palash Tyagi <23239946+Magnus167@users.noreply.github.com>
2024-01-18 17:01:53 +0530

    working as expected

--- docs/gen.py
+++ docs/gen.py
@@ -11,8 +11,8 @@ ORGANIZATION: str = "macrosynergy"
 REPO_NAME: str = "macrosynergy"
 REPO_URL: str = f"github.com/{REPO_OWNER}/{REPO_NAME}"
 RELEASES_API_URL = f"https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}/releases"
-OUTPUT_DIR = "./docs/source/gen_rsts"
-STATIC_RSTS_DIR = "./docs/source/static_rsts"
+OUTPUT_DIR = "./docs/source"
+STATIC_RSTS_DIR = "./docs/source"
 PATH_TO_DOCS: str = os.path.join(OUTPUT_DIR, "release_notes.md")
 LINE_SEPARATOR: str = "\n\n____________________\n\n"
 
@@ -171,22 +171,15 @@ def fetch_release_notes(
         file.write(release_md)
 
 
-def remove_file_spec(
-    gen_dir: str = OUTPUT_DIR,
-    static_dir: str = "./docs/source/static_rsts",
-):
+def remove_file_spec(gen_dir: str = OUTPUT_DIR, perm_files: List[str] = []):
     """
     Removes '... module'/'... package' from the first line of each rst file.
-    Also removes any rst files that have manually been added to the static_rsts folder.
+    Specifically ignores the files in `perm_files`.
     """
-    static_rsts_basenames = list(
-        map(os.path.basename, glob.glob(os.path.join(static_dir, "*.rst")))
-    )
     for fname in glob.glob(os.path.join(gen_dir, "*.rst")):
-        if os.path.basename(fname) in static_rsts_basenames:
-            os.remove(fname)
+        if os.path.abspath(fname) in perm_files:
+            continue
 
-    for fname in glob.glob(os.path.join(gen_dir, "*.rst")):
         # open the file
         with open(fname, "r", encoding="utf8") as file:
             data = file.readlines()
@@ -196,9 +189,11 @@ def remove_file_spec(
             file.writelines(data)
 
 
-def copy_subpackage_readmes(gen_dir: str = OUTPUT_DIR, package: str = "macrosynergy"):
+def copy_subpackage_readmes(
+    output_dir: str = OUTPUT_DIR, package: str = "macrosynergy"
+):
     """
-    Copies the README.md files from each subpackage to the docs/source/gen_rsts folder.
+    Copies the README.md files from each subpackage to the docs/source/ folder.
     Renames them to <subpackage_name>.README.md to avoid conflicts.
     Also removes the header from each README.md file.
     """
@@ -212,7 +207,7 @@ def copy_subpackage_readmes(gen_dir: str = OUTPUT_DIR, package: str = "macrosyne
     new_names: List[str] = [
         ".".join(px.split(os.sep)[-2:]) for px in subpackage_readmes
     ]
-    new_names = [os.path.join(gen_dir, px) for px in new_names]
+    new_names = [os.path.join(output_dir, px) for px in new_names]
 
     for old, new in zip(subpackage_readmes, new_names):
         # if the file already exists, remove it
@@ -229,17 +224,40 @@ def copy_subpackage_readmes(gen_dir: str = OUTPUT_DIR, package: str = "macrosyne
             file.writelines(data)
 
 
-def generate_rsts(output_dir: str, package: str = "macrosynergy"):
+def generate_rsts(output_dir: str, package: str = "macrosynergy",
+                  perm_files: List[str] = []
+                  ):
     """
     Calls `sphinx-apidoc` to generate rst files for all modules in `package`.
     """
+    # make a temporary directory to store the generated rst files
+    # move all files in output_dir to a temporary directory
+    temp_folder = os.path.join(output_dir, "_temp")
+    os.makedirs(temp_folder, exist_ok=True)
+    for fname in glob.glob(os.path.join(output_dir, "*")):
+        if os.path.isfile(fname):
+            shutil.move(fname, temp_folder)
+
     rst_gen = f"sphinx-apidoc -o {output_dir} -fMeT {package}"
     os.system(rst_gen)
-    remove_file_spec(gen_dir=output_dir, static_dir=STATIC_RSTS_DIR)
-    copy_subpackage_readmes(gen_dir=output_dir, package=package)
+
+    # move all files from the temporary directory to output_dir, overwriting the generated rst files
+    for fname in glob.glob(os.path.join(temp_folder, "*")):
+        if os.path.isfile(fname):
+            if os.path.exists(os.path.join(output_dir, os.path.basename(fname))):
+                os.remove(os.path.join(output_dir, os.path.basename(fname)))
+            shutil.move(fname, output_dir)
+
+    # remove the temporary directory
+    shutil.rmtree(temp_folder)
+
+    remove_file_spec(gen_dir=output_dir, perm_files=perm_files)
 
 
-def make_docs(docs_dir: str = "./docs", show: bool = False):
+def make_docs(
+    docs_dir: str = "./docs",
+    show: bool = False,
+):
     """
     Calls `make html` in `docs_dir` (makefile from sphinx-quickstart).
     """
@@ -251,8 +269,6 @@ def make_docs(docs_dir: str = "./docs", show: bool = False):
     os.system(makeclean)
     os.system(makehtml)
     os.chdir(current_dir)
-    # remove the generated rst files
-    shutil.rmtree(OUTPUT_DIR)
     print(f"Documentation generated successfully.")
     print(
         "Paste the following path into your browser to view:\n\t\t "
@@ -261,6 +277,49 @@ def make_docs(docs_dir: str = "./docs", show: bool = False):
 
     if show:
         os.system(f"start docs/build/html/index.html")
+
+
+def driver(
+    build: bool = False,
+    show: bool = False,
+    prod: bool = False,
+    output_dir: str = OUTPUT_DIR,
+    docs_dir: str = "./docs",
+):
+    """
+    Driver function.
+    :param <bool> build: Whether to build the documentation.
+    :param <bool> show: Whether to show the documentation in browser after generation.
+    :param <bool> prod: Whether to build the documentation for production.
+    """
+    # get a list of all files in the output directory
+    perm_files: List[str] = [
+        os.path.abspath(filex)
+        for filex in glob.glob(f"{docs_dir}/**/*", recursive=True)
+    ]
+    if not build:
+        return
+    os.makedirs(output_dir, exist_ok=True)
+
+    # copy all rsts
+
+    generate_rsts(output_dir=output_dir, perm_files=perm_files)
+    fetch_release_notes()
+    copy_subpackage_readmes(output_dir=output_dir)
+    make_docs(docs_dir=docs_dir, show=show)
+
+    if prod:
+        return
+
+    # get a list of all files in docs/build/
+    build_files: List[str] = [
+        os.path.abspath(filex)
+        for filex in glob.glob(f"{docs_dir}/build/**/*", recursive=True)
+    ]
+    keep_files = perm_files + build_files
+    for filex in glob.glob(f"{docs_dir}/**/*", recursive=True):
+        if os.path.abspath(filex) not in keep_files and os.path.isfile(filex):
+            os.remove(filex)
 
 
 def main():
@@ -279,6 +338,13 @@ def main():
         "--build",
         action="store_true",
         help="Build documentation.",
+        default=True,
+    )
+
+    parser.add_argument(
+        "--prod",
+        action="store_true",
+        help="Build documentation for production.",
         default=False,
     )
 
@@ -286,17 +352,10 @@ def main():
 
     SHOW = args.show
     BUILD = args.build
-    README = "./README.md"
+    PROD = args.prod
+    DOCS_DIR = "./docs"
 
-    os.makedirs(OUTPUT_DIR, exist_ok=True)
-
-    if BUILD:
-        generate_rsts(output_dir=OUTPUT_DIR)
-
-    fetch_release_notes()
-
-    if BUILD:
-        make_docs(show=SHOW)
+    driver(build=BUILD, show=SHOW, prod=PROD, docs_dir=DOCS_DIR)
 
 
 if __name__ == "__main__":
