7953448687c6f06d1b9b318ddb1b4e4da9625d4c
Palash Tyagi <23239946+Magnus167@users.noreply.github.com>
2024-01-23 12:31:48 +0000

    fixing unittest

--- tests/unit/download/test_dataquery.py
+++ tests/unit/download/test_dataquery.py
@@ -682,7 +682,7 @@ class TestDataQueryInterface(unittest.TestCase):
             with self.assertRaises(ValueError):
                 validate_download_args(**bad_args)
 
-        for delay_param in [0.0, 0.1, 1.0]:
+        for delay_param in [0.0, 0.1, 0.15]:
             bad_args: Dict[str, Any] = good_args.copy()
             bad_args["delay_param"] = delay_param
             with self.assertWarns(RuntimeWarning):
