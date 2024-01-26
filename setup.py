1e9ff9002c4974e9370829f7c9c2a8de2c957b0e
Samuel Andresen <sandresen@macrosynergy.com>
2024-01-26 12:15:16 +0000

    Chore: Bump version to 0.1.5

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ if sys.version_info[:2] < (3, 8):
 
 MAJOR = 0
 MINOR = 1
-MICRO = 4
-ISRELEASED = True
+MICRO = 5
+ISRELEASED = False
 VERSION = "%d.%d.%d" % (MAJOR, MINOR, MICRO)
 
 if sys.version_info >= (3, 12):
