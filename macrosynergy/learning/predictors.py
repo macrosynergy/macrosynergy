6243d1d65a75f11274a700b974d63870ed53a7b3
rushilg99 <rushil1.gholkar@gmail.com>
2024-02-04 16:27:33 +0000

    lad regressor class

--- macrosynergy/learning/predictors.py
+++ macrosynergy/learning/predictors.py
@@ -1,6 +1,10 @@
 import numpy as np
 import pandas as pd
 
+from scipy.optimize import minimize
+
+from functools import partial
+
 import datetime
 
 from sklearn.base import BaseEstimator, RegressorMixin
@@ -280,6 +284,191 @@ class TimeWeightedLinearRegression(TimeWeightedRegressor):
         )
         super().__init__(half_life=half_life, model=model)
 
+
+class LADRegressor(BaseEstimator, RegressorMixin):
+    def __init__(self, fit_intercept=True, positive=False):
+        """
+        Custom class to create a linear regression model with model fit determined
+        by minimising L1 (absolute) loss.   
+
+        :param <bool> fit_intercept: Whether to calculate the intercept for this model. If set to False, no intercept will be used in calculations.
+        :param <bool> positive: If True, the optimisation is restricted so that model weights (other than the intercept) are greater or equal to zero. 
+            This is a way of incorporating prior knowledge about the model features into the model fit.
+
+        NOTE: Standard OLS linear regression models are fit by minimising the residual sum of squares. Our
+            implemented (L)east (A)bsolute (D)eviation regression model fits a linear model by minimising the residual absolute deviations. 
+            Consequently, the model fit is more robust to outliers. 
+        """
+        self.fit_intercept = fit_intercept
+        self.positive = positive
+
+    def __l1_loss(self, X: pd.DataFrame, y: Union[pd.DataFrame, pd.Series], weights: np.ndarray):
+        """
+        Private helper method to determine the mean training L1 loss induced by 'weights'.
+
+        :param <pd.DataFrame> X: Pandas dataframe of input features.
+        :param <Union[pd.DataFrame, pd.Series]> y: Pandas series or dataframe of targets
+            associated with each sample in X.
+        :param <np.ndarray> weights: LADRegressor model coefficients to be optimised. 
+
+        :return <float>: Training loss induced by 'weights'.
+        """
+        # Checks
+        if type(X) != pd.DataFrame:
+            raise TypeError(
+                "Input feature matrix for the LADRegressor must be a pandas dataframe. "
+                "If used as part of an sklearn pipeline, ensure that previous steps "
+                "return a pandas dataframe."
+            )
+        if (type(y) != pd.Series) and (type(y) != pd.DataFrame):
+            raise TypeError(
+                "Target vector for the LADRegressor must be a pandas series or dataframe. "
+                "If used as part of an sklearn pipeline, ensure that previous steps "
+                "return a pandas series or dataframe."
+            )
+        if type(y) == pd.DataFrame:
+            if y.shape[1] != 1:
+                raise ValueError(
+                    "The target dataframe must have only one column. If used as part of "
+                    "an sklearn pipeline, ensure that previous steps return a pandas "
+                    "series or dataframe."
+                )
+
+        if not isinstance(X.index, pd.MultiIndex):
+            raise ValueError("X must be multi-indexed.")
+        if not isinstance(y.index, pd.MultiIndex):
+            raise ValueError("y must be multi-indexed.")
+        if not isinstance(X.index.get_level_values(1)[0], datetime.date):
+            raise TypeError("The inner index of X must be datetime.date.")
+        if not isinstance(y.index.get_level_values(1)[0], datetime.date):
+            raise TypeError("The inner index of y must be datetime.date.")
+        if not X.index.equals(y.index):
+            raise ValueError(
+                "The indices of the input dataframe X and the output dataframe y don't "
+                "match."
+            )
+        if not isinstance(weights, np.ndarray):
+            raise TypeError("The weights must be contained within a numpy array.")
+        
+        raw_resids = y - X.dot(weights)
+        abs_resids = np.abs(raw_resids)
+
+        return np.mean(abs_resids)
+
+    def fit(self, X: pd.DataFrame, y: Union[pd.DataFrame, pd.Series]):
+        """
+        Fit method to fit a LAD linear regression model.
+
+        :param <pd.DataFrame> X: Pandas dataframe of input features.
+        :param <Union[pd.DataFrame, pd.Series]> y: Pandas series or dataframe of targets
+            associated with each sample in X.
+
+        :return <LADRegressor>
+        """
+        # Checks
+        if type(X) != pd.DataFrame:
+            raise TypeError(
+                "Input feature matrix for the LADRegressor must be a pandas dataframe. "
+                "If used as part of an sklearn pipeline, ensure that previous steps "
+                "return a pandas dataframe."
+            )
+        if (type(y) != pd.Series) and (type(y) != pd.DataFrame):
+            raise TypeError(
+                "Target vector for the LADRegressor must be a pandas series or dataframe. "
+                "If used as part of an sklearn pipeline, ensure that previous steps "
+                "return a pandas series or dataframe."
+            )
+        if type(y) == pd.DataFrame:
+            if y.shape[1] != 1:
+                raise ValueError(
+                    "The target dataframe must have only one column. If used as part of "
+                    "an sklearn pipeline, ensure that previous steps return a pandas "
+                    "series or dataframe."
+                )
+
+        if not isinstance(X.index, pd.MultiIndex):
+            raise ValueError("X must be multi-indexed.")
+        if not isinstance(y.index, pd.MultiIndex):
+            raise ValueError("y must be multi-indexed.")
+        if not isinstance(X.index.get_level_values(1)[0], datetime.date):
+            raise TypeError("The inner index of X must be datetime.date.")
+        if not isinstance(y.index.get_level_values(1)[0], datetime.date):
+            raise TypeError("The inner index of y must be datetime.date.")
+        if not X.index.equals(y.index):
+            raise ValueError(
+                "The indices of the input dataframe X and the output dataframe y don't "
+                "match."
+            )
+
+        # Fit
+        if self.fit_intercept:
+            self.X = X.copy()
+            self.X.insert(0,"intercept", 1)
+
+        p = self.X.shape[1]
+
+        if self.positive:
+            if self.fit_intercept:
+                bounds = [(None, None)] + [(0, None) for _ in range(p-1)]
+            else:
+                bounds = [(0, None) for _ in range(p)]
+        else:
+            bounds = [(None, None) for _ in range(p)]
+
+        # optimisation
+        init_weights = np.zeros(self.X.shape[1])
+        optim_results = minimize(
+            fun = partial(
+                self.__l1_loss,
+                X = self.X,
+                y = self.y,
+            ),
+            x0 = init_weights,
+            method='SLSQP',
+            bounds=bounds,
+        )
+
+        if not optim_results.success:
+            raise ValueError("Optimization Failed")
+        
+        if self.fit_intercept:
+            # Then store the intercept and feature weights
+            self.intercept_ = optim_results.x[0]
+            self.coef_ = optim_results.x[1:]
+        else:
+            self.intercept_ = None
+            self.coef_ = optim_results.x
+
+        return self 
+    
+    def predict(self, X: pd.DataFrame):
+        """
+        Predict method to make model predictions on the input feature matrix X based on
+        the previously fit model.
+
+        :param <pd.DataFrame> X: Pandas dataframe of input features.
+
+        :return <np.ndarray>: Numpy array of predictions.
+        """
+        # Checks
+        if type(X) != pd.DataFrame:
+            raise TypeError(
+                "Input feature matrix for the LADRegressor must be a pandas dataframe. "
+                "If used as part of an sklearn pipeline, ensure that previous steps "
+                "return a pandas dataframe."
+            )
+        if not isinstance(X.index, pd.MultiIndex):
+            raise ValueError("X must be multi-indexed.")
+        if not isinstance(X.index.get_level_values(1)[0], datetime.date):
+            raise TypeError("The inner index of X must be datetime.date.")
+
+        # Predict
+        if self.fit_intercept:
+            X = X.copy()
+            X.insert(0,"intercept",1)
+            return X.dot(self.coef_)
+        else:
+            return X.dot(self.coef_) + self.intercept_
 
 if __name__ == "__main__":
     from macrosynergy.management import make_qdf
