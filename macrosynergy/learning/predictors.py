c27032173d28bf2e3f64f32a404372ad54c14484
Rushil Gholkar <67958144+rushilg99@users.noreply.github.com>
2024-02-05 13:42:08 +0000

    Added sample weights option to ladregressor fit method

--- macrosynergy/learning/predictors.py
+++ macrosynergy/learning/predictors.py
@@ -4,6 +4,7 @@ import pandas as pd
 from scipy.optimize import minimize
 
 from functools import partial
+import warnings
 
 import datetime
 
@@ -285,35 +286,58 @@ class TimeWeightedLinearRegression(TimeWeightedRegressor):
         super().__init__(half_life=half_life, model=model)
 
 
+
 class LADRegressor(BaseEstimator, RegressorMixin):
     def __init__(self, fit_intercept=True, positive=False):
         """
         Custom class to create a linear regression model with model fit determined
-        by minimising L1 (absolute) loss.   
+        by minimising L1 (absolute) loss.
 
-        :param <bool> fit_intercept: Whether to calculate the intercept for this model. If set to False, no intercept will be used in calculations.
-        :param <bool> positive: If True, the optimisation is restricted so that model weights (other than the intercept) are greater or equal to zero. 
-            This is a way of incorporating prior knowledge about the model features into the model fit.
+        :param <bool> fit_intercept: Whether to calculate the intercept for this model.
+            If set to False, no intercept will be used in calculations.
+        :param <bool> positive: If True, the optimisation is restricted so that model
+            weights (other than the intercept) are greater or equal to zero.
+            This is a way of incorporating prior knowledge about the relationship between 
+            model features and corresponding targets into the model fit.
 
-        NOTE: Standard OLS linear regression models are fit by minimising the residual sum of squares. Our
-            implemented (L)east (A)bsolute (D)eviation regression model fits a linear model by minimising the residual absolute deviations. 
-            Consequently, the model fit is more robust to outliers than an OLS fit.  
+        NOTE: Standard OLS linear regression models are fit by minimising the residual
+            sum of squares. Our implemented (L)east (A)bsolute (D)eviation regression
+            model fits a linear model by minimising the residual absolute deviations.
+            Consequently, the model fit is more robust to outliers than an OLS fit.
         """
+        # Checks
+        if not isinstance(fit_intercept, bool):
+            raise TypeError("'fit_intercept' must be a boolean.")
+        if not isinstance(positive, bool):
+            raise TypeError("'positive' must be a boolean.")
+
+        # Initialise
         self.fit_intercept = fit_intercept
         self.positive = positive
 
-    def __l1_loss(self, weights: np.ndarray, X: pd.DataFrame, y: Union[pd.DataFrame, pd.Series]):
+    def __l1_loss(
+        self,
+        weights: np.ndarray,
+        X: pd.DataFrame,
+        y: Union[pd.DataFrame, pd.Series],
+        sample_weights: np.ndarray = None,
+    ):
         """
         Private helper method to determine the mean training L1 loss induced by 'weights'.
 
+        :param <np.ndarray> weights: LADRegressor model coefficients to be optimised.
         :param <pd.DataFrame> X: Pandas dataframe of input features.
         :param <Union[pd.DataFrame, pd.Series]> y: Pandas series or dataframe of targets
             associated with each sample in X.
-        :param <np.ndarray> weights: LADRegressor model coefficients to be optimised. 
+        :param <np.ndarray> sample_weights: Numpy array of sample weights to create a
+            weighted LAD regression model. Default is None for equal sample weights.
 
         :return <float>: Training loss induced by 'weights'.
         """
         # Checks
+        if not isinstance(weights, np.ndarray):
+            raise TypeError("The weights must be contained within a numpy array.")
+
         if type(X) != pd.DataFrame:
             raise TypeError(
                 "Input feature matrix for the LADRegressor must be a pandas dataframe. "
@@ -347,24 +371,44 @@ class LADRegressor(BaseEstimator, RegressorMixin):
                 "The indices of the input dataframe X and the output dataframe y don't "
                 "match."
             )
-        if not isinstance(weights, np.ndarray):
-            raise TypeError("The weights must be contained within a numpy array.")
-        
+
+        if sample_weights is None:
+            sample_weights = np.ones(X.shape[0])
+
+        if not isinstance(sample_weights, np.ndarray):
+            raise TypeError(
+                "The sample weights must be contained within a numpy array."
+            )
+        if sample_weights.ndim != 1:
+            raise ValueError("The sample weights must be a 1D numpy array.")
+        if len(sample_weights) != X.shape[0]:
+            raise ValueError(
+                "The number of sample weights must match the number of samples in the input feature matrix."
+            )
+
         if isinstance(y, pd.DataFrame):
-            raw_resids = y.iloc[:,0] - X.dot(weights)
-        else: # y is a series
+            raw_resids = y.iloc[:, 0] - X.dot(weights)
+        else:  # y is a series
             raw_resids = y - X.dot(weights)
         abs_resids = np.abs(raw_resids)
+        weighted_abs_resids = abs_resids * sample_weights
 
-        return np.mean(abs_resids)
+        return np.mean(weighted_abs_resids)
 
-    def fit(self, X: pd.DataFrame, y: Union[pd.DataFrame, pd.Series]):
+    def fit(
+        self,
+        X: pd.DataFrame,
+        y: Union[pd.DataFrame, pd.Series],
+        sample_weights: np.ndarray = None,
+    ):
         """
         Fit method to fit a LAD linear regression model.
 
         :param <pd.DataFrame> X: Pandas dataframe of input features.
         :param <Union[pd.DataFrame, pd.Series]> y: Pandas series or dataframe of targets
             associated with each sample in X.
+        :param <np.ndarray> sample_weights: Numpy array of sample weights to create a
+            weighted LAD regression model. Default is None for equal sample weights.
 
         :return <LADRegressor>
         """
@@ -403,18 +447,30 @@ class LADRegressor(BaseEstimator, RegressorMixin):
                 "match."
             )
 
+        if sample_weights is not None:
+            if not isinstance(sample_weights, np.ndarray):
+                raise TypeError(
+                    "The sample weights must be contained within a numpy array."
+                )
+            if sample_weights.ndim != 1:
+                raise ValueError("The sample weights must be a 1D numpy array.")
+            if len(sample_weights) != X.shape[0]:
+                raise ValueError(
+                    "The number of sample weights must match the number of samples in the input feature matrix."
+                )
+
         # Fit
         X2 = X.copy()
         y2 = y.copy()
 
         if self.fit_intercept:
-            X2.insert(0,"intercept", 1)
+            X2.insert(0, "intercept", 1)
 
         p = X2.shape[1]
 
         if self.positive:
             if self.fit_intercept:
-                bounds = [(None, None)] + [(0, None) for _ in range(p-1)]
+                bounds = [(None, None)] + [(0, None) for _ in range(p - 1)]
             else:
                 bounds = [(0, None) for _ in range(p)]
         else:
@@ -423,19 +479,27 @@ class LADRegressor(BaseEstimator, RegressorMixin):
         # optimisation
         init_weights = np.zeros(p)
         optim_results = minimize(
-            fun = partial(
+            fun=partial(
                 self.__l1_loss,
-                X = X2,
-                y = y2,
+                X=X2,
+                y=y2,
+                sample_weights=sample_weights,
             ),
-            x0 = init_weights,
-            method='SLSQP',
+            x0=init_weights,
+            method="SLSQP",
             bounds=bounds,
+            tol=1e-9,
         )
 
         if not optim_results.success:
-            raise ValueError("Optimization Failed")
-        
+            self.intercept_ = None
+            self.coef_ = None
+            warnings.warn(
+                "LADRegressor optimisation failed. Setting intercept and coefficients to None.",
+                RuntimeWarning,
+            )
+            return self
+
         if self.fit_intercept:
             # Then store the intercept and feature weights
             self.intercept_ = optim_results.x[0]
@@ -444,8 +508,8 @@ class LADRegressor(BaseEstimator, RegressorMixin):
             self.intercept_ = None
             self.coef_ = optim_results.x
 
-        return self 
-    
+        return self
+
     def predict(self, X: pd.DataFrame):
         """
         Predict method to make model predictions on the input feature matrix X based on
@@ -468,11 +532,18 @@ class LADRegressor(BaseEstimator, RegressorMixin):
             raise TypeError("The inner index of X must be datetime.date.")
 
         # Predict
+        if self.intercept_ is None and self.coef_ is None:
+            warnings.warn(
+                "LADRegressor model fit failed. Predicting a zero signal.", RuntimeWarning
+            )
+            return pd.Series(0, index=X.index)
+
         X = X.copy()
         if self.fit_intercept:
             return X.dot(self.coef_) + self.intercept_
         else:
             return X.dot(self.coef_)
+
 
 if __name__ == "__main__":
     from macrosynergy.management import make_qdf
