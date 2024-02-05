fc89cee92a629b454194e3d2ec3496a5d79d053b
Palash Tyagi <ptyagi@macrosynergy.com>
2024-02-05 23:38:46 +0000

    Add redirects for static pages

---
+++ docs/redirects.py
@@ -0,0 +1,16 @@
+import glob
+import os
+
+STATIC_PAGES = [
+    ("05_dev_guide.html", "contribution_guide.html"),
+    ("06_definitions.html", "common_definitions.html"),
+    ("02_installation.html", "getting_started.html"),
+    ("01_context.html", "index.html"),
+    ("03_dataquery.html", "macrosynergy.download.html"),
+    # ("04_academy.html", "academy.html"),
+]
+
+
+
+
+
