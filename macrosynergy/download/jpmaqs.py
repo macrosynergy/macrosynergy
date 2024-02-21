bb9daddd4740633bf0d146e16953b2b0292e9f79
Palash Tyagi <ptyagi@macrosynergy.com>
2024-02-21 17:03:51 +0000

    reverting unneccessary change

--- macrosynergy/download/jpmaqs.py
+++ macrosynergy/download/jpmaqs.py
@@ -682,9 +682,7 @@ class JPMaQSDownload(DataQueryInterface):
         if len(download_outputs) == 0:
             return []
 
-        if isinstance(download_outputs[0], pd.DataFrame) and not isinstance(
-            download_outputs[0], QuantamentalDataFrame
-        ):
+        if isinstance(download_outputs[0], pd.DataFrame):
             return concat_column_dfs(df_list=download_outputs)
 
         if isinstance(download_outputs[0][0], (dict, QuantamentalDataFrame)):
