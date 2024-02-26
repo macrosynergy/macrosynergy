659fc4a2ec9338ace43afa157af8b945a144e4e8
Lasse de la Porte Simonsen <lsimonsen@macrosynergy.com>
2024-02-26 14:30:53 +0000

    Fix merge conflict left-over

--- macrosynergy/pnl/__init__.py
+++ macrosynergy/pnl/__init__.py
@@ -5,8 +5,6 @@ from macrosynergy.pnl.notional_positions import notional_positions
 from macrosynergy.pnl.historic_portfolio_volatility import historic_portfolio_vol
 from macrosynergy.pnl.proxy_pnl import proxy_pnl
 
-<<<<<<< HEAD
-
 __all__ = [
     'create_results_dataframe',
     "NaivePnL",
