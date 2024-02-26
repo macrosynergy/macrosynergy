65431056ec10b987340b684a914b371e0e122fc7
Palash Tyagi <ptyagi@macrosynergy.com>
2024-02-26 14:26:10 +0000

    added code to download transaction costs

---
+++ macrosynergy/download/transaction_costs.py
@@ -0,0 +1,60 @@
+import datetime
+import pandas as pd
+import warnings
+import requests
+import numpy as np
+from macrosynergy.management.types import QuantamentalDataFrame
+from io import StringIO
+
+MAX_RETRIES = 3
+CSV_URL = "https://macrosynergy-trading-costs.s3.eu-west-2.amazonaws.com/transaction-costs.csv"
+
+
+def _request_wrapper(url: str, verbose: bool = True, **kwargs) -> requests.Response:
+    """
+    Wrapper around requests.get to handle errors and retries.
+    """
+    if verbose:
+        print(f"Requesting data from {url}")
+
+    for _ in range(MAX_RETRIES):
+        try:
+            r = requests.get(url, **kwargs)
+            r.raise_for_status()
+            return r
+        except requests.exceptions.RequestException as e:
+            if verbose:
+                print(f"Error downloading {url}: {e}")
+            continue
+    raise requests.exceptions.RequestException(f"Failed to download {url}")
+
+
+def download_transaction_costs(
+    csv_url: str = CSV_URL,
+    verbose: bool = False,
+    **kwargs,
+) -> QuantamentalDataFrame:
+    """
+    Download trading costs data from the S3 bucket.
+    """
+    if verbose:
+        print(f"Timestamp (UTC): {datetime.datetime.now(datetime.timezone.utc)}")
+        print(f"Downloading trading costs data from {csv_url}")
+    dfd: pd.DataFrame = pd.read_csv(
+        StringIO(_request_wrapper(csv_url, **kwargs).text),
+        parse_dates=["real_date"],
+        dtype={"ticker": str, "real_date": str, "value": np.float64},
+        usecols=["ticker", "real_date", "value"],
+        header=0,
+        engine="c",
+    )
+    if not isinstance(dfd, pd.DataFrame):
+        raise ValueError("Failed to download trading costs data")
+
+    if not isinstance(dfd, QuantamentalDataFrame):
+        warnings.warn("Downloaded dataframe is not a QuantamentalDataFrame.")
+
+    return dfd
+
+
+download_transaction_costs()
