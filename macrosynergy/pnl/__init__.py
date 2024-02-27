aedfe75ed2c15d796a6db48af5920f7e0ff43521
Palash Tyagi <ptyagi@macrosynergy.com>
2024-02-27 15:42:43 +0000

    renamve proxy_pnl.py to proxy_pnl_calc.py

--- macrosynergy/pnl/__init__.py
+++ macrosynergy/pnl/__init__.py
@@ -3,7 +3,7 @@ from macrosynergy.pnl.naive_pnl import NaivePnL, create_results_dataframe
 from macrosynergy.pnl.contract_signals import contract_signals
 from macrosynergy.pnl.notional_positions import notional_positions
 from macrosynergy.pnl.historic_portfolio_volatility import historic_portfolio_vol
-from macrosynergy.pnl.proxy_pnl import proxy_pnl
+from macrosynergy.pnl.proxy_pnl_calc import proxy_pnl
 
 __all__ = [
     "NaivePnL",
