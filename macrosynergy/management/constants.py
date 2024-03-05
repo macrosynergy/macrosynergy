eaf42929f2ba7535a8ddd9499679af5bde9e2aa3
Palash Tyagi <ptyagi@macrosynergy.com>
2024-03-05 02:44:16 +0000

    Update frequency mappings and cross-sections of interest

--- macrosynergy/management/constants.py
+++ macrosynergy/management/constants.py
@@ -1,7 +1,34 @@
 """
+
 Constants used throughout the package.
 
-- FREQUENCY_MAP:  used to map the frequencies from D, W, M, Q, A to Business Day Frequencies.
+Frequency Mappings
+------------------
+
+* FREQUENCY_MAP:  used to map the frequencies from D, W, M, Q, A to Business Day Frequencies.
+
+* FFILL_LIMITS: used to map the frequencies from D, W, M, Q, A to the maximum number of ffill periods.
+
+
+Cross-sections of interest
+--------------------------
+
+* cids_dmca: DM currency areas
+
+* cids_dmec: DM euro area countries
+
+* cids_latm: Latam countries
+
+* cids_emea: EMEA countries
+
+* cids_emas: EM Asia countries
+
+* cids_dm: DM currency areas and DM euro area countries
+
+* cids_em: Latam, EMEA and EM Asia countries
+
+* all_cids: All countries
+
 """
 
 FREQUENCY_MAP = {
@@ -12,18 +39,32 @@ FREQUENCY_MAP = {
     "A": "BA",
 }
 
+FFILL_LIMITS = {
+    "D": 1,
+    "W": 5,
+    "M": 24,
+    "Q": 64,
+    "A": 252,
+    "B": 1,
+    "W-FRI": 5,
+    "BM": 24,
+    "BQ": 64,
+    "BA": 252,
+}
+
 # Cross-sections of interest
-cids_dmca = [
-    "AUD", "CAD", "CHF", "EUR", "GBP", "JPY","NOK", "NZD", "SEK", "USD",
-]  # DM currency areas
-cids_dmec = ["DEM", "ESP", "FRF", "ITL"]  # DM euro area countries
-cids_latm = ["BRL", "COP", "CLP", "MXN", "PEN"]  # Latam countries
-cids_emea = ["CZK", "HUF", "ILS", "PLN", "RON", "RUB", "TRY", "ZAR"]  # EMEA countries
-cids_emas = [
-    "CNY", "IDR", "INR", "KRW", "MYR", "PHP", "SGD", "THB", "TWD",
-]  # EM Asia countries
+## DM currency areas
+cids_dmca = ["AUD", "CAD", "CHF", "EUR", "GBP", "JPY", "NOK", "NZD", "SEK", "USD"]
+## DM euro area countries
+cids_dmec = ["DEM", "ESP", "FRF", "ITL"]
+## Latam countries
+cids_latm = ["BRL", "COP", "CLP", "MXN", "PEN"]
+## EMEA countries
+cids_emea = ["CZK", "HUF", "ILS", "PLN", "RON", "RUB", "TRY", "ZAR"]
+## EM Asia countries
+cids_emas = ["CNY", "IDR", "INR", "KRW", "MYR", "PHP", "SGD", "THB", "TWD"]
 
 cids_dm = cids_dmca + cids_dmec
 cids_em = cids_latm + cids_emea + cids_emas
 
-cids = sorted(cids_dm + cids_em)
+all_cids = sorted(cids_dm + cids_em)
