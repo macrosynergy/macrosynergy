73e62a979deabec7280871757b3702a2e201602e
Samuel Andresen <sandresen@macrosynergy.com>
2024-03-18 10:18:55 +0000

    Add class docstring

---
+++ macrosynergy/panel/intertemporal_correlation.py
@@ -0,0 +1,11 @@
+"""
+Implementation of correlation functions for quantamental data.
+The functions calculates and visualize for two categories or cross-sections the following
+  - Probabilities of Non-Stationarity
+  - Autocorrelation function (ACF, PACF)
+  - Cross-Correlation function (CCF)
+  - Granger Causality
+"""
+
+class IntertemporalCorrelation(object):
+  
