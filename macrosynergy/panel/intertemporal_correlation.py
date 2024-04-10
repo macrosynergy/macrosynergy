851ee572dd1da95abf07019989f14d10a59f5ec3
Samuel Andresen <sandresen@macrosynergy.com>
2024-04-10 15:06:53 +0100

    Add acf and pacf implementations

--- macrosynergy/panel/intertemporal_correlation.py
+++ macrosynergy/panel/intertemporal_correlation.py
@@ -3,8 +3,8 @@ import numpy as np
 from typing import List
 from macrosynergy.management.simulate.simulate_quantamental_data import make_qdf
 from macrosynergy.management.utils import categories_df
-from statsmodels.tsa.stattools import adfuller, kpss, acf, pacf
-from arch.unitroot import PhillipsPerron, ADF, DFGLS
+from arch.unitroot import PhillipsPerron, ADF, DFGLS, KPSS, ZivotAndrews
+from statsmodels.tsa.stattools import pacf
 
 """
 Implementation of correlation functions for quantamental data.
@@ -14,6 +14,7 @@ The functions calculates and visualize for two categories or cross-sections the 
     - Phillips-Perron (PP) test
     - KPSS test
     - ADF-GLS test
+    - Zivot-Andrews test
     - Plot time trends, random walks and seasonalities
   - Autocorrelation function (ACF, PACF)
   - Cross-Correlation function (CCF)   
@@ -59,79 +60,127 @@ class IntertemporalCorrelation(object):
         self.cids = cids
 
     def calc_prob_of_non_stationarity(
-        self, lags: int = None, trend="c"
+        self,
+        statistical_tests: List[str] = None,
+        lags: int = None,
+        trend="c",
+        max_lags=None,
     ) -> pd.DataFrame:
         """
         Calculate the probabilities of non-stationarity
         """
+        stat_test_dict = {
+            "ADF": self._calc_adf,
+            "PP": self._calc_pp,
+            "DFGLS": self._calc_dfgls,
+            "KPSS": self._calc_kpss,
+            "ZA": self._calc_za,
+        }
+
+        if statistical_tests is None:
+            statistical_tests = ["ADF", "PP", "DFGLS", "KPSS", "ZA"]
+        else:
+            if not set(statistical_tests).issubset(
+                {"ADF", "PP", "DFGLS", "KPSS", "ZA"}
+            ):
+                raise ValueError(
+                    "statistical_tests must be a subset of {'ADF', 'PP', 'DFGLS', 'KPSS', 'ZA'}"
+                )
 
         index = pd.MultiIndex.from_product(
             [self.cids, self.xcats], names=["cid", "xcat"]
         )
-
-        df_result = pd.DataFrame(
-            index=index, columns=["ADF P Value", "Phillips-Perron P Value"]
-        )
+        df_result = pd.DataFrame(index=index)
 
         for idx in index:
             cid = idx[0]
             xcat = idx[1]
-            if cid in self.df[xcat]:
-                adf_result = ADF(
-                    self.df[xcat][cid].dropna().values,
-                    lags=lags,
-                    trend=trend,
+            for test in statistical_tests:
+                df_result.loc[idx, f"{test} P Value"] = stat_test_dict[test](
+                    xcat, cid, lags, trend, max_lags
                 )
-                df_result.loc[(cid, xcat), "ADF P Value"] = adf_result[1]
-
-                PP_result = PhillipsPerron(
-                    self.df[xcat][cid].dropna().values,
-                    lags=lags,
-                    trend=trend,
-                )
-                df_result.loc[(cid, xcat), "Phillips-Perron P Value"] = (
-                    PP_result.pvalue
-                )
-
-                dfgls_result = DFGLS(
-                    self.df[xcat][cid].dropna().values,
-                    lags=lags,
-                    trend=trend
-                )
-                df_result.loc[(cid, xcat), "DFGLS P Value"] = (
-                    dfgls_result.pvalue
-                )
-                
-
-        # KPSS test HERE
-
-        # ADF-GLS test HERE
 
         return df_result
 
-    def plot_time_series_components(self):
-        """
-        Plot the time trends, random walks and seasonalities
-        """
-        pass
+    def _calc_adf(self, xcat, cid, lags, trend, max_lags, *args, **kwargs):
+        adf_result = ADF(
+            self.df[xcat][cid].dropna().values,
+            lags=lags,
+            trend=trend,
+            max_lags=max_lags,
+            method="aic",
+        )
+        return "{:.6f}".format(adf_result.pvalue)
 
-    def calculate_acf(self, lags: int):
+    def _calc_pp(self, xcat, cid, lags, trend, *args, **kwargs):
+        PP_result = PhillipsPerron(
+            self.df[xcat][cid].dropna().values, lags=lags, trend=trend
+        )
+        return "{:.6f}".format(PP_result.pvalue)
+
+    def _calc_dfgls(self, xcat, cid, lags, trend, max_lags, *args, **kwargs):
+        dfgls_result = DFGLS(
+            self.df[xcat][cid].dropna().values,
+            lags=lags,
+            max_lags=max_lags,
+            trend=trend,
+            method="aic",
+        )
+        return "{:.6f}".format(dfgls_result.pvalue)
+
+    def _calc_kpss(self, xcat, cid, lags, trend, *args, **kwargs):
+        kpss_result = KPSS(self.df[xcat][cid].dropna().values, lags=lags, trend=trend)
+        return "{:.6f}".format(kpss_result.pvalue)
+
+    def _calc_za(self, xcat, cid, lags, trend, max_lags, *args, **kwargs):
+        za_result = ZivotAndrews(
+            self.df[xcat][cid].dropna().values,
+            lags=lags,
+            trend=trend,
+            max_lags=max_lags,
+            method="aic",
+        )
+        return "{:.6f}".format(za_result.pvalue)
+
+    # def plot_time_series_components(self):
+    #     """
+    #     Plot the time trends, random walks and seasonalities
+    #     """
+    #     pass
+
+    def calculate_acf(self, xcat: str, cid: str, max_lag: int, detrend: bool = True):
         """
         Calculate the ACF and for statistically significant lags
         """
-        pass
+        time_series = self.df[xcat][cid].dropna()
+        
+        if detrend:
+            time_series = time_series - time_series.mean()
+        
+        autocorr_values = []
+        
+        for lag in range(max_lag + 1):
+            if lag == 0:
+                autocorr_values.append(1.0)
+            else:
+                lag_correlation = np.corrcoef(time_series[:-lag], time_series[lag:])[0, 1]
+                autocorr_values.append(lag_correlation)
+                
+        return pd.Series(autocorr_values, index=np.arange(max_lag + 1))
 
-    def calculate_pacf(self, lags: int):
+    def calculate_pacf(self, xcat: str, cid: str, max_lag: int):
         """
         Calculate the PACF and for statistically significant lags
         """
-        pass
+        pacf_values = pacf(self.df[xcat][cid].dropna(), nlags=max_lag)
+    
+        return pacf_values
 
     def plot_acf(self):
         """
         Plot the ACF and PACF for statistically significant lags
         """
-        pass
+        pacf_values = self.calculate_pacf("FXXR_NSA", "AUD", )
 
     def calculate_ccf(self, lags: int):
         """
@@ -159,39 +208,67 @@ class IntertemporalCorrelation(object):
 
 
 if __name__ == "__main__":
-    cids = ["AUD", "CAD", "GBP", "NZD", "USD"]
-    xcats = ["XR", "XRH", "CRY", "GROWTH", "INFL"]
-    df_cids = pd.DataFrame(
-        index=cids, columns=["earliest", "latest", "mean_add", "sd_mult"]
-    )
-    df_cids.loc["AUD"] = ["2000-01-01", "2020-12-31", 0.1, 1]
-    df_cids.loc["CAD"] = ["2001-01-01", "2020-11-30", 0, 1]
-    df_cids.loc["BRL"] = ["2001-01-01", "2020-11-30", -0.1, 2]
-    df_cids.loc["GBP"] = ["2002-01-01", "2020-11-30", 0, 2]
-    df_cids.loc["NZD"] = ["2002-01-01", "2020-09-30", -0.1, 2]
-    df_cids.loc["USD"] = ["2003-01-01", "2020-12-31", -0.1, 2]
+    from macrosynergy.download import JPMaQSDownload
+    import os
 
-    cols = ["earliest", "latest", "mean_add", "sd_mult", "ar_coef", "back_coef"]
-    df_xcats = pd.DataFrame(index=xcats, columns=cols)
-    df_xcats.loc["XR"] = ["2000-01-01", "2020-12-31", 0.1, 1, 0, 0.3]
-    df_xcats.loc["XRH"] = ["2000-01-01", "2020-12-31", 0.2, 1, 0, 0.25]
-    df_xcats.loc["CRY"] = ["2000-01-01", "2020-10-30", 1, 2, 0.95, 1]
-    df_xcats.loc["GROWTH"] = ["2001-01-01", "2020-10-30", 1, 2, 0.9, 1]
-    df_xcats.loc["INFL"] = ["2001-01-01", "2020-10-30", 1, 2, 0.8, 0.5]
+    # ids_dmlc = ["EUR", "JPY", "MXN", "TRY", "INR"]  # DM large currency areas
+    cids_dmsc = [
+        "AUD",
+        "CAD",
+        "CHF",
+        "GBP",
+        "NOK",
+        "NZD",
+        "SEK",
+    ]  # DM small currency areas
 
-    dfd = make_qdf(df_cids, df_xcats, back_ar=0.75)
-    dfd["grading"] = np.ones(dfd.shape[0])
-    # black = {"AUD": ["2000-01-01", "2003-12-31"], "GBP": ["2018-01-01", "2100-01-01"]}
+    cids = cids_dmsc
+    rets = [
+        "FXXR_NSA",
+        "FXXR_VT10",
+        # "FXXRHvGDRB_NSA",
+        # "MTBGDPRATIO_SA_3MMAv120MMA"
+    ]
+
+    xcats = rets
+
+    # Resultant tickers
+
+    tickers = [cid + "_" + xcat for cid in cids for xcat in xcats]
+    print(f"Maximum number of tickers is {len(tickers)}")
+
+    start_date = "2000-01-01"
+    end_date = "2023-05-01"
+
+    # Retrieve credentials
+
+    client_id: str = os.getenv("DQ_CLIENT_ID")
+    client_secret: str = os.getenv("DQ_CLIENT_SECRET")
+
+    with JPMaQSDownload(client_id=client_id, client_secret=client_secret) as dq:
+        df = dq.download(
+            tickers=tickers,
+            start_date=start_date,
+            end_date=end_date,
+            suppress_warning=True,
+            metrics=["value"],
+            report_time_taken=True,
+            show_progress=True,
+        )
 
     itc = IntertemporalCorrelation(
-        dfd,
+        df,
         xcats=xcats,
         cids=cids,
         start="2000-01-01",
-        end="2020-12-31",
+        end="2023-01-01",
         # blacklist=black,
         freq="M",
     )
 
     print(itc.df.head())
-    print(itc.calc_prob_of_non_stationarity(lags=12, regression_trend="ct"))
+    print(itc.calc_prob_of_non_stationarity(trend="c"))
+    print(itc.calc_prob_of_non_stationarity(trend="ct"))
+
+    print(itc.calculate_acf("FXXR_NSA", "AUD", 10))  # ACF for AUD
+    print(itc.calculate_pacf("FXXR_NSA", "AUD", 10))  # PACF for AUD
