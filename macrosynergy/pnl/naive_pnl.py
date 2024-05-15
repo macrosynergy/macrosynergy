3da8991fc1de1b3c26196d8b74617bdf05874cc5
Samuel Andresen <sandresen@macrosynergy.com>
2024-05-15 14:41:33 +0100

    Bugfix: Change label in evaluate_pnl

--- macrosynergy/pnl/naive_pnl.py
+++ macrosynergy/pnl/naive_pnl.py
@@ -945,8 +945,8 @@ class NaivePnL:
             "Sortino Ratio",
             "Max 21-Day Draw",
             "Max 6-Month Draw",
-            "Top 5% Monthly PnL Share %",
-            "Peak to Trough Draw",
+            "Top 5% Monthly PnL Share",
+            "Peak to Trough Draw %",
             "Traded Months",
         ]
 
@@ -980,7 +980,7 @@ class NaivePnL:
 
         top_months = monthly_pnl.nlargest(top_5_percent_cutoff, columns=monthly_pnl.columns)
 
-        df.iloc[6, :] = (top_months.sum() / total_pnl) * 100
+        df.iloc[6, :] = top_months.sum() / total_pnl
 
         cum_pnl = dfw.cumsum()
         high_watermark = cum_pnl.cummax()
