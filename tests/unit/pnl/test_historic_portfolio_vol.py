419d96cadb568a86ff45861d7c12f12c67517531
Palash Tyagi <ptyagi@macrosynergy.com>
2024-06-04 14:31:24 +0100

    refactoring tests

--- tests/unit/pnl/test_historic_portfolio_vol.py
+++ tests/unit/pnl/test_historic_portfolio_vol.py
@@ -16,53 +16,47 @@ from macrosynergy.pnl.historic_portfolio_volatility import (
 from macrosynergy.management.types import QuantamentalDataFrame
 
 
-class TestHistoricPortfolioVol(unittest.TestCase):
-    def setUp(self): ...
-    def tearDown(self): ...
-
-    def test_weighted_covariance(
-        self,
-    ):
-        x_arr = np.arange(100) / 100
-        y_arr = np.arange(100) / 100
-        good_args: Dict[str, Any] = {
+class TestWeightedCovariance(unittest.TestCase):
+    # testing `weighted_covariance` function
+    def setUp(self):
+        self.good_args: Dict[str, Any] = {
             "half_life": 10,
             "lback_periods": 100,
-            "x": x_arr,
-            "y": y_arr,
+            "x": np.arange(100) / 100,
+            "y": np.arange(100) / 100,
             "weights_func": expo_weights_arr,
         }
 
+    def tearDown(self): ...
+
+    def test_weighted_covariance(self):
         # Test good args
-        res = _weighted_covariance(**good_args)
+        res = _weighted_covariance(**self.good_args)
         self.assertTrue(isinstance(res, float))
         self.assertTrue(np.isclose(res, 0.019827, atol=1e-6))
 
-        ## Test bad args
-
+    def test_x_y_length(self):
         ## X and Y must be same length
         for argn in ["x", "y"]:
-            bad_args = good_args.copy()
+            bad_args = self.good_args.copy()
             bad_args[argn] = np.arange(99) / 100
             with self.assertRaises(AssertionError):
                 _weighted_covariance(**bad_args)
 
         ## X and Y must be 1D
         for argn in ["x", "y"]:
-            bad_args = good_args.copy()
+            bad_args = self.good_args.copy()
             bad_args[argn] = np.arange(100).reshape((10, 10))
             with self.assertRaises(AssertionError):
                 _weighted_covariance(**bad_args)
 
+    def test_nan_handling(self):
         ## For either being all nan, the result should be nan
         for argn in ["x", "y"]:
-            bad_args = good_args.copy()
+            bad_args = self.good_args.copy()
             bad_args[argn] = np.full(100, np.nan)
             res = _weighted_covariance(**bad_args)
             self.assertTrue(np.isnan(res))
-
-        # @mock.patch("macrosynergy.pnl.historic_portfolio_volatility.expo_weights_arr")
-        # def _test
 
 
 if __name__ == "__main__":
