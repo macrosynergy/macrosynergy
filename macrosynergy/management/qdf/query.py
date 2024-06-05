3de0b5f9624eca2eff9144a7bb5649b3279fdb13
Palash Tyagi <ptyagi@macrosynergy.com>
2024-06-05 16:51:48 +0100

    adding query methods

---
+++ macrosynergy/management/qdf/query.py
@@ -0,0 +1,164 @@
+from typing import Dict, Iterable, List, Optional, Union
+from collections.abc import Mapping, Iterable
+from .classes import QDFManagerBase, DateLike
+from .methods import get_ticker_dict_from_df_dict
+import macrosynergy.management.constants as msy_constants
+import pandas as pd
+import fnmatch
+import itertools
+
+
+def _substring_matcher(
+    substrings: Iterable[str],
+    all_available_tickers: Iterable[str],
+) -> List[str]:
+    """
+    Get the tickers from the query parameters.
+    """
+    assert isinstance(substrings, Iterable)
+    assert isinstance(all_available_tickers, Iterable)
+    return list(
+        itertools.chain.from_iterable(
+            fnmatch.filter(all_available_tickers, substring) for substring in substrings
+        )
+    )
+
+
+def _string_query(
+    qdf_manager: QDFManagerBase,
+    queries: List[str],
+) -> List[str]:
+    """
+    Get the tickers from the query parameters.
+    """
+    all_available_tickers = qdf_manager.tickers
+    queries = [queries] if isinstance(queries, str) else queries
+    _contains_wildcard = lambda x: not ("*" in x or "?" in x)
+    results = []
+    _queries = [f"{q}*" for q in queries if not _contains_wildcard(q)]
+    
+
+def _run_ticker_query(
+    qdf_manager: QDFManagerBase,
+    cids: Optional[Iterable[str]] = None,
+    xcats: Optional[Iterable[str]] = None,
+    tickers: Optional[Iterable[str]] = None,
+    metrics: Optional[Union[str, Iterable[str]]] = ["all"],
+    cid: Optional[str] = None,
+    xcat: Optional[str] = None,
+    ticker: Optional[str] = None,
+) -> List[str]:
+    """
+    Get the tickers from the query parameters.
+    """
+    all_available_tickers = qdf_manager.tickers
+    cids = [cids] if isinstance(cids, str) else cids
+    xcats = [xcats] if isinstance(xcats, str) else xcats
+    if isinstance(cid, str):
+        cids = [cid] + (cids or [])
+    if isinstance(xcat, str):
+        xcats = [xcat] + (xcats or [])
+    if isinstance(ticker, str):
+        tickers = [ticker] + (tickers or [])
+    if tickers is None:
+        if cids is None or cids == []:
+            cids = qdf_manager.cids
+        if xcats is None or xcats == []:
+            xcats = qdf_manager.xcats
+        tickers = [f"{cid}_{xcat}" for cid in cids for xcat in xcats]
+
+    _contains_wildcard = lambda x: "*" in x or "?" in x
+    wtickers = list(filter(_contains_wildcard, tickers))
+    if wtickers:
+        tickers = list(set(tickers) - set(wtickers))
+        tickers += _substring_matcher(wtickers, all_available_tickers)
+
+    metrics = list(set(metrics) & set(qdf_manager.metrics))
+    if cids is None or xcats is None:
+        cids: List[str] = []
+        xcats: List[str] = []
+
+    if tickers is None:
+        tickers: List[str] = []
+
+    tickers += [f"{cid}_{xcat}" for cid in cids for xcat in xcats]
+    tickers = set(tickers)
+
+    return {
+        m: sorted(set(qdf_manager.df_dict[m].columns).intersection(tickers))
+        for m in metrics
+    }
+
+
+def query_df_dict(
+    qdf_manager: QDFManagerBase,
+    cid: Optional[str] = None,
+    cids: Optional[List[str]] = None,
+    xcat: Optional[str] = None,
+    xcats: Optional[List[str]] = None,
+    ticker: Optional[str] = None,
+    tickers: Optional[List[str]] = None,
+    start: Optional[DateLike] = None,
+    end: Optional[DateLike] = None,
+    date_range: Optional[pd.DatetimeIndex] = None,
+    # substring: Optional[str] = None,
+    # substrings: Optional[List[str]] = None,
+    metric: Optional[str] = None,
+    metrics: Optional[List[str]] = None,
+    cross_section_groups: Optional[List[str]] = None,
+) -> dict[str, pd.DataFrame]:
+    """
+    Query a dictionary of `pd.DataFrame`s.
+    """
+    metrics = [metric] if isinstance(metric, str) else []
+    metrics = [metrics] if isinstance(metrics, str) else []
+    metrics = metrics + (metrics or [])
+
+    if metrics == ["all"] or not metrics:
+        metrics = qdf_manager.metrics
+    start: DateLike = qdf_manager.start_date if start is None else start
+    end: DateLike = qdf_manager.end_date if end is None else end
+    if date_range is not None:
+        date_range: pd.DatetimeIndex = pd.DatetimeIndex(map(pd.Timestamp, date_range))
+        start = date_range.min()
+        end = date_range.max()
+
+    if not isinstance(start, pd.Timestamp):
+        start = pd.Timestamp(start)
+    if not isinstance(end, pd.Timestamp):
+        end = pd.Timestamp(end)
+
+    if start < qdf_manager.start_date:
+        start = qdf_manager.start_date
+    if end > qdf_manager.end_date:
+        end = qdf_manager.end_date
+
+    if start > end:
+        start, end = end, start
+
+    if isinstance(cross_section_groups, str):
+        cross_section_groups = [cross_section_groups]
+    cross_section_groups = cross_section_groups or []
+    if cids is None:
+        cids = []
+    for cxg in cross_section_groups or []:
+        _cx = cxg.lower()
+        if _cx in msy_constants.cross_section_groups:
+            cids = list(set(cids + msy_constants.cross_section_groups[_cx]))
+
+    ticker_query: Dict[str, List[str]] = _run_ticker_query(
+        qdf_manager,
+        cid=cid,
+        cids=cids,
+        xcat=xcat,
+        xcats=xcats,
+        ticker=ticker,
+        tickers=tickers,
+        metrics=metrics,
+    )
+
+    df_dict: dict[str, pd.DataFrame] = {
+        metric: qdf_manager.df_dict[metric].loc[start:end, tickers]
+        for metric, tickers in ticker_query.items()
+    }
+    return df_dict
