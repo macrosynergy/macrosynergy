26f46e493cb82e5a31abfc55282a848740a974d7
Palash Tyagi <23239946+Magnus167@users.noreply.github.com>
2024-06-12 19:20:31 +0100

    adding tests for misc. functions

--- tests/unit/pnl/test_historic_portfolio_vol.py
+++ tests/unit/pnl/test_historic_portfolio_vol.py
@@ -12,9 +12,13 @@ from macrosynergy.pnl.historic_portfolio_volatility import (
     expo_weights_arr,
     _weighted_covariance,
     estimate_variance_covariance,
+    _check_est_args,
+    _check_missing_data,
+    _check_frequency,
+    _check_input_arguments,
 )
 from macrosynergy.management.utils import qdf_to_ticker_df, ticker_df_to_qdf
-from macrosynergy.management.types import QuantamentalDataFrame
+from macrosynergy.management.types import QuantamentalDataFrame, NoneType
 from macrosynergy.management.simulate import make_test_df
 
 
@@ -96,6 +100,142 @@ class TestEstimateVarianceCovariance(unittest.TestCase):
         self.assertEqual(res.shape[0], self.good_args["piv_ret"].shape[1])
         self.assertEqual(res.shape[0], res.shape[1])
         self.assertEqual(set(res.columns), set(self.good_args["piv_ret"].columns))
+
+
+class TestMisc(unittest.TestCase):
+    def setUp(self): ...
+
+    def tearDown(self): ...
+
+    def test_flat_weights_arr(self):
+        # Test good args
+        res = flat_weights_arr(10)
+        self.assertTrue(isinstance(res, np.ndarray))
+        self.assertEqual(res.shape[0], 10)
+        self.assertTrue(np.allclose(res, np.full(10, 1 / 10)))
+
+    def test_expo_weights_arr(self):
+        # Test good args
+        res = expo_weights_arr(10, 10)
+        self.assertTrue(isinstance(res, np.ndarray))
+        self.assertEqual(res.shape[0], 10)
+
+    def test_check_frequency(self):
+        # Test good args
+        for freq in ["D", "W", "M", "Q", "A"]:
+            _check_frequency(freq, "freq-type")
+        for freq in ["X", "Y", "Z"]:
+            with self.assertRaises(ValueError):
+                _check_frequency(freq, "freq-type")
+
+    def test_check_missing_data(self):
+        # Test good args
+        sname = "SNAME"
+        rstring = "RSTRING"
+        cids = ["USD", "EUR", "GBP"]
+        fids = [f"{c}_FX" for c in cids]
+        xcats = [f"FX{rstring}_CSIG_{sname}", f"FX{rstring}"]
+        good_df = make_test_df(
+            cids=cids,
+            xcats=xcats,
+            start="2020-01-01",
+            end="2021-01-01",
+        )
+        good_df["ticker"] = good_df["cid"] + "_" + good_df["xcat"]
+        _check_missing_data(df=good_df, fids=fids, rstring=rstring, sname=sname)
+
+        # Test bad args
+        bad_df = good_df.copy()
+        bad_df["xcat"] = bad_df["xcat"].str.replace("CSIG", "BAR")
+        bad_df["ticker"] = bad_df["cid"] + "_" + bad_df["xcat"]
+        with self.assertRaises(ValueError):
+            _check_missing_data(df=bad_df, fids=fids, rstring=rstring, sname=sname)
+
+        # test dropping cid=USD, xcat.endswith("_CSIG_SNAME")
+        bad_df = good_df.copy()
+        bad_df = bad_df[
+            ~(
+                (bad_df["cid"] == "USD")
+                & (bad_df["xcat"].str.endswith(f"_CSIG_{sname}"))
+            )
+        ].reset_index(drop=True)
+        with self.assertRaises(ValueError):
+            _check_missing_data(df=bad_df, fids=fids, rstring=rstring, sname=sname)
+
+        # drop all rows with cid=USD, xcat=="FXRSTRING"
+        bad_df = good_df.copy()
+        bad_df = bad_df[
+            ~((bad_df["cid"] == "USD") & (bad_df["xcat"] == f"FX{rstring}"))
+        ].reset_index(drop=True)
+        with self.assertRaises(ValueError):
+            _check_missing_data(df=bad_df, fids=fids, rstring=rstring, sname=sname)
+
+    def test_check_input_arguments(self):
+        arguments = [
+            ("df", pd.DataFrame),
+            ("sname", str),
+            ("fids", list),
+            ("rstring", str),
+            ("rebal_freq", str),
+            ("lback_meth", str),
+            ("lback_periods", list),
+            ("half_life", list),
+            ("est_freqs", list),
+            ("est_weights", list),
+            ("start", (str, NoneType)),
+            ("end", (str, NoneType)),
+            ("blacklist", (dict, NoneType)),
+            ("nan_tolerance", float),
+            ("remove_zeros", bool),
+            ("return_variance_covariance", bool),
+        ]
+        good_args = {
+            "df": make_test_df(),
+            "sname": "SNAME",
+            "fids": ["FID1", "FID2"],
+            "rstring": "RSTRING",
+            "rebal_freq": "M",
+            "lback_meth": "ma",
+            "lback_periods": [10, 20],
+            "half_life": [5, 10],
+            "est_freqs": ["D", "W"],
+            "est_weights": [0.5, 0.5],
+            "start": "2020-01-01",
+            "end": "2021-01-01",
+            "blacklist": {"A": ["B", "C"]},
+            "nan_tolerance": 0.1,
+            "remove_zeros": True,
+            "return_variance_covariance": False,
+        }
+
+        # Test good args
+        _check_input_arguments(
+            [(good_args[argn], argn, argt) for argn, argt in arguments]
+        )
+
+        # Test bad args
+        # pass an int for all arguments
+        for argn, argt in arguments:
+            # pass an int instead of the expected type
+            bad_args = good_args.copy()
+            bad_args[argn] = -1
+            with self.assertRaises(TypeError):
+                _check_input_arguments(
+                    [(bad_args[argn], argn, argt) for argn, argt in arguments]
+                )
+
+            if isinstance(argt, (list, dict, str)):
+                bad_args = good_args.copy()
+                if isinstance(argt, list):
+                    bad_args[argn] = []
+                elif isinstance(argt, dict):
+                    bad_args[argn] = {}
+                elif isinstance(argt, str):
+                    bad_args[argn] = ""
+                with self.assertRaises(ValueError):
+                    _check_input_arguments(
+                        [(bad_args[argn], argn, argt) for argn, argt in arguments]
+                    )
 
 
 if __name__ == "__main__":
