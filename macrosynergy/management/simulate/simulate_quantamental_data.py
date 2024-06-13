35f63d0bb74ef624d8f36488ae5cc26af045a911
Palash Tyagi <ptyagi@macrosynergy.com>
2024-06-13 12:05:02 +0100

    applied formatting & isort

--- macrosynergy/management/simulate/simulate_quantamental_data.py
+++ macrosynergy/management/simulate/simulate_quantamental_data.py
@@ -11,8 +11,12 @@ from collections import defaultdict
 import datetime
 import warnings
 from macrosynergy.management.types import QuantamentalDataFrame
-from macrosynergy.management.utils import ticker_df_to_qdf
-from macrosynergy.management.utils import is_valid_iso_date, get_cid, get_xcat
+from macrosynergy.management.utils import (
+    ticker_df_to_qdf,
+    is_valid_iso_date,
+    get_cid,
+    get_xcat,
+)
 
 
 def simulate_ar(nobs: int, mean: float = 0, sd_mult: float = 1, ar_coef: float = 0.75):
@@ -444,7 +448,6 @@ def simulate_returns_and_signals(
     periods = 252 * years
     assert (periods > 0) and (n_cids > 0)
 
-
     def simulate_volatility(
         periods: int = 252 * 20, sigma_eta: float = 0.01, sigma_0: float = 0.1
     ):
@@ -455,8 +458,9 @@ def simulate_returns_and_signals(
             sigma[ii + 1] = np.exp(np.log(sigma[ii]) + ee)
         return sigma[1:]
 
-
-    dates = pd.bdate_range(end=pd.Timestamp.today() + pd.offsets.BDay(n=0), periods=periods)
+    dates = pd.bdate_range(
+        end=pd.Timestamp.today() + pd.offsets.BDay(n=0), periods=periods
+    )
     # Generate volatility
     print("Generate volatility (shared???)")
     volatility = np.empty(shape=(periods, n_cids))
@@ -468,12 +472,16 @@ def simulate_returns_and_signals(
     # Generate signals: persistent?
     rho_signal = 0.9
     mean_signal = 0
-    signals = np.empty(shape=(periods+1, n_cids))
-    signals[0,:] = mean_signal
+    signals = np.empty(shape=(periods + 1, n_cids))
+    signals[0, :] = mean_signal
     for tt in range(periods):
-        signals[tt+1,:] = (1 - rho_signal) * mean_signal + rho_signal * signals[tt,:] + np.random.normal(0, 0.01, n_cids)
+        signals[tt + 1, :] = (
+            (1 - rho_signal) * mean_signal
+            + rho_signal * signals[tt, :]
+            + np.random.normal(0, 0.01, n_cids)
+        )
     # signals = np.random.randn(periods, n_cids)  # Unit variance, zero mean
-    signals = signals[1:,:]
+    signals = signals[1:, :]
 
     # Generate alpha
     # TODO alpha needs to be a function of lagged signal and not necessarily continous?
