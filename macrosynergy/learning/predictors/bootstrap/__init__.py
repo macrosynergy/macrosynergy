ad4ab0c79a6f85004253ffb990e34e7ffefcee29
Rushil Gholkar <67958144+rushilg99@users.noreply.github.com>
2024-07-17 11:29:20 +0100

    bootstrap folder constructor

---
+++ macrosynergy/learning/predictors/bootstrap/__init__.py
@@ -0,0 +1,13 @@
+from .bootstrap import (
+    BasePanelBootstrap,
+)
+
+from .base_modified_regressor import (
+    BaseModifiedRegressor,
+)
+
+__all__ = [
+    "BasePanelBootstrap",
+    "BaseModifiedRegressor",
+    #"BaseVolAdjRegressor",
+]
