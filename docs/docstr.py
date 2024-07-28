c39c1d2ff8450c11621229a47f9c084f61276a9b
Palash Tyagi <ptyagi@macrosynergy.com>
2024-07-29 00:17:39 +0100

    wip

--- docs/docstr.py
+++ docs/docstr.py
@@ -1,93 +1,66 @@
-import argparse
+import ast
+from typing import List, Dict, Any
 import glob
-import re
-import os
-from typing import List, Tuple, Dict, Any
-
-sphinx_directives = {
-    "param": "Parameters",
-    "return": "Returns",
-    "raises": "Raises",
-    "rtype": "Returns",
-}
+import json
 
 
-def is_directive(line: str) -> bool:
-    return any(
-        line.startswith(f":{directive}") for directive in sphinx_directives.keys()
-    )
+def read_python_file(file_path: str) -> str:
+    with open(file_path, "r") as file:
+        return file.read()
 
 
-def format_param_docstr(docstr: str) -> str:
-    # the docstring is in the format ":param <TYPE> name: description"
-    type_start = docstr.find("<") + 1
-    type_end = docstr.find(">", type_start)
-    type_str = docstr[type_start:type_end]
+class DocstringVisitor(ast.NodeVisitor):
+    def __init__(self) -> None:
+        self.docstrings_info: List[Dict[str, Any]] = []
 
-    name_start = type_end + 1
-    name_end = docstr.find(":", name_start)
-    name_str = docstr[name_start:name_end]
+    def visit_FunctionDef(self, node: ast.FunctionDef) -> None:
+        self._check_docstring(node)
+        self.generic_visit(node)
 
+    def visit_ClassDef(self, node: ast.ClassDef) -> None:
+        self._check_docstring(node)
+        self.generic_visit(node)
 
-def format_return_docstr(docstr: str) -> str: ...
+    def visit_Module(self, node: ast.Module) -> None:
+        self._check_docstring(node)
+        self.generic_visit(node)
 
-
-def format_raises_docstr(docstr: str) -> str: ...
-
-
-def format_rtype_docstr(docstr: str) -> str: ...
-
-
-def format_directive(docstr: str) -> str:
-    patterns = ["param", "return", "raises", "rtype"]
-    fdict = {f: eval(f"format_{f}_docstr") for f in patterns}
-    assert is_directive(docstr), "The given docstring is not a directive."
-
-    for pattern in patterns:
-        if docstr.startswith(f":{pattern}"):
-            return fdict[pattern](docstr)
-
-
-def format_docstring(data_lines: List[str]) -> str:
-    directive_lines: List[bool] = list(map(is_directive, data_lines))
-
-    formatted_groups: List[str] = []
-    curr_group = []
-    for i, line in enumerate(data_lines):
-        if (directive_lines[i] is True) and bool(curr_group):
-            formatted_groups.append("\n".join(curr_group))
-            curr_group = []
-        curr_group.append(line)
-    if bool(curr_group):
-        formatted_groups.append("\n".join(curr_group))
-
-    for group in formatted_groups:
-        if is_directive(group):
-            format_directive(group)
-
-    formatted_groups
-
-    return "\n".join(formatted_groups)
+    def _check_docstring(self, node: ast.AST) -> None:
+        docstring = ast.get_docstring(node)
+        if docstring:
+            # Retrieve the starting line number
+            start_line = node.body[0].lineno
+            # Calculate the ending line number
+            end_line = start_line + len(docstring.split("\n")) - 1
+            self.docstrings_info.append(
+                {
+                    "type": type(node).__name__,
+                    "name": getattr(node, "name", "Module"),
+                    "start_line": start_line,
+                    "end_line": end_line,
+                    "docstring": docstring,
+                }
+            )
 
 
-# def format_docstring(docstr: str):
-def find_docstrings(file_lines: List[str]) -> List[Tuple[int, int]]:
-    """
-    Finds the start and end indices of all docstrings in the given file.
-    :param <str> filestr: The content of the file.
-    :return <List[Tuple[int, int]>: A list of tuples containing the start and end indices of all docstrings.
-    """
-    results: List[Tuple[int, int]] = []
+def get_docstring_info(file_path: str) -> List[Dict[str, Any]]:
+    with open(file_path, "r") as file:
+        source = file.read()
 
-    curr_start = None
-    for i, line in enumerate(file_lines):
-        if (line.strip() in ['"""', "'''"]) and curr_start is None:
-            curr_start = i
-        elif curr_start is not None and line.strip() in ['"""', "'''"]:
-            results.append((curr_start, i))
-            curr_start = None
+    tree = ast.parse(source)
+    visitor = DocstringVisitor()
+    visitor.visit(tree)
+    return visitor.docstrings_info
 
-    return results
+
+class DSParser:
+    def __init__(self, file_path: str) -> None:
+        self.file_path = file_path
+        self.source = None
+        self.docstrings_info: List[Dict[str, Any]] = []
+
+    def read_python_file(self) -> None:
+        self.source = read_python_file(self.file_path)
 
 
 def format_python_file(file_path: str):
@@ -100,13 +73,13 @@ def format_python_file(file_path: str):
         data_lines = file.readlines()
 
     # find all docstrings
-    docstrings_coords = find_docstrings(data_lines)
-    for start, end in docstrings_coords:
-        data_lines[start : end + 1] = format_docstring(data_lines[start : end + 1])
+    docstrings_info = get_docstring_info(file_path)
+    for docstring_info in docstrings_info:
+        print(json.dumps(docstring_info, indent=4))
 
     # find the last occurance of os.sep in the file path
     last_sep = file_path.rfind(".")
-    file_path = file_path[:last_sep] + "_fmt" + file_path[last_sep:]
+    # file_path = file_path[:last_sep] + "_fmt" + file_path[last_sep:]
     with open(file_path, "w", encoding="utf8") as file:
         file.writelines(data_lines)
 
@@ -120,16 +93,20 @@ def format_python_files(root_dir: str):
         format_python_file(file)
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Format docstrings in python files.")
-    parser.add_argument(
-        "-d",
-        "--dir",
-        type=str,
-        help="The root directory to search for python files.",
-        # required=True,
-        default="macrosynergy",
-    )
-    args = parser.parse_args()
+# if __name__ == "__main__":
+#     parser = argparse.ArgumentParser(description="Format docstrings in python files.")
+#     parser.add_argument(
+#         "-d",
+#         "--dir",
+#         type=str,
+#         help="The root directory to search for python files.",
+#         # required=True,
+#         default="macrosynergy",
+#     )
+#     args = parser.parse_args()
 
-    format_python_files(args.dir)
+#     format_python_files(args.dir)
+
+if __name__ == "__main__":
+    file_path = "macrosynergy/download/dataquery.py"
+    format_python_file(file_path)
