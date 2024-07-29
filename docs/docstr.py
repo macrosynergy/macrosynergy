fcd01b17f94367f6f381e948bb2b7d1b690c6fa2
Palash Tyagi <ptyagi@macrosynergy.com>
2024-07-30 00:29:51 +0100

    wip, almost there

--- docs/docstr.py
+++ docs/docstr.py
@@ -1,5 +1,5 @@
 import ast
-from typing import List, Dict, Any
+from typing import List, Dict, Any, Tuple, Optional
 import glob
 import json
 
@@ -7,6 +7,9 @@ import json
 def read_python_file(file_path: str) -> str:
     with open(file_path, "r") as file:
         return file.read()
+
+
+MAX_LINE_LENGTH = 88
 
 
 class DocstringVisitor(ast.NodeVisitor):
@@ -31,7 +34,9 @@ class DocstringVisitor(ast.NodeVisitor):
             # Retrieve the starting line number
             start_line = node.body[0].lineno
             # Calculate the ending line number
-            end_line = start_line + len(docstring.split("\n")) - 1
+            end_line = start_line + len(docstring.split("\n")) + 1
+            # NOTE: this may break if the docstring is at the very end of the file with no newline
+            # however, this case does not seem to exist in the current codebase
             self.docstrings_info.append(
                 {
                     "type": type(node).__name__,
@@ -53,14 +58,203 @@ def get_docstring_info(file_path: str) -> List[Dict[str, Any]]:
     return visitor.docstrings_info
 
 
+SPHINX_DIRECTIVES: Dict[str, str] = {
+    "param": "Parameters",
+    "raises": "Raises",
+    "return": "Returns",
+    "rtype": "Returns",
+}
+DIRECTIVES = [f":{directive}" for directive in SPHINX_DIRECTIVES]
+
+
+def is_directive_line(line: str) -> bool:
+    return line.strip().startswith(tuple(DIRECTIVES))
+
+
+def get_indentation(line: str) -> int:
+    return len(line) - len(line.lstrip())
+
+
+def split_docstring(docstring: str) -> Dict[str, List[str]]:
+    # First, split the docstring by lines
+    lines = docstring.split("\n")
+    # Apply is_directive_line to each line
+    directive_lines = list(map(is_directive_line, lines))
+
+    # Initialize variables to hold the description and sections
+    sections = []
+    current_section = []
+
+    for i, line in enumerate(lines):
+        if directive_lines[i]:
+            # This line is a directive
+            if current_section:
+                # Append the current section to sections before starting a new section
+                sections.append(" ".join(current_section).strip())
+                current_section = []
+            # Start the new section with the directive line
+            current_section.append(line.strip())
+        else:
+            # This line is not a directive, add to the current section
+            current_section.append(line.strip())
+
+    # Append the last section if it exists
+    if current_section:
+        sections.append(" ".join(current_section).strip())
+
+    # Calculate overall indentation
+    non_empty_lines = [line for line in lines if line.strip()]
+    overall_indentation = (
+        min(get_indentation(line) for line in non_empty_lines) if non_empty_lines else 0
+    )
+    if overall_indentation == 0:
+        overall_indentation = 4
+
+    return dict(
+        description=sections[0],
+        directives=sections[1:],
+        indentation=overall_indentation,
+    )
+
+
+MAX_LINE_LENGTH = 88
+
+
+def roll_description(
+    description: str, indentation: int, max_line_length: int = MAX_LINE_LENGTH
+) -> str:
+    # split the description by words, then join them back with a newline with len(line) + indentation < max_line_length
+    # each line should start with indentation number of spaces
+    words = description.split()
+    lines = []
+    current_line = chr(32) * indentation
+    for word in words:
+        if len(current_line) + len(word) + 1 < max_line_length:
+            current_line += f" {word}"
+        else:
+            lines.append(current_line)
+            current_line = chr(32) * indentation + word
+    lines.append(current_line)
+    return "\n".join(lines)
+
+
+def format_param_directive(directive: str, min_indent: int) -> str:
+    # e.g.: ":param <str> name: The name of the person."
+    try:
+        assert directive.startswith(":param")
+        # split by < and >
+        typex = directive.split("<", 1)[1].split(">", 1)[0].strip()
+        name, description = directive.split(">", 1)[1].split(":", 1)
+        name, description = name.strip(), description.strip()
+        idt = chr(32) * (4 + min_indent)
+        description = roll_description(description, 4 + min_indent)
+        return f"{name} : ({typex})\n{idt}{description}"
+    except Exception as e:
+        print(directive)
+        print(e)
+        return chr(32) * (4 + min_indent) + directive
+
+
+def format_raises_directive(directive: str, min_indent: int) -> str:
+    # e.g.: ":raises <Exception>: description"
+    assert directive.startswith(":raises")
+    # split by < and >
+    typex = directive.split("<", 1)[1].split(">", 1)[0].strip()
+    description = directive.split(">", 1)[1].split(":", 1)[1].strip()
+
+    idt = chr(32) * (4 + min_indent)
+    description = roll_description(description, 4 + min_indent)
+    return f"{typex}\n{idt}{description}"
+
+
+def format_return_directive(directive: str, min_indent: int) -> str:
+    # e.g.: ":return <type>: description"
+    assert directive.startswith(":return") or directive.startswith(":rtype")
+    # split by < and >
+    typex = directive.split("<", 1)[1].split(">", 1)[0].strip()
+    description = directive.split(">", 1)[1].split(":", 1)[1].strip()
+    idt = chr(32) * (4 + min_indent)
+    description = roll_description(description, 4 + min_indent)
+    return f"{typex}\n{idt}{description}"
+
+
+def format_docstring_section(section: Dict[str, Any]) -> Dict[str, Any]:
+    dir_func_map = {
+        "param": format_param_directive,
+        "raises": format_raises_directive,
+        "return": format_return_directive,
+        "rtype": format_return_directive,
+    }
+    # first group the sections by directive
+    directive_sections = {}
+    # if there are any param directives, they should be first
+    for directive in ["param", "raises", "return", "rtype"]:
+        directive_sections[directive] = [
+            dirx
+            for dirx in section["directives"]
+            if str(dirx).startswith(f":{directive}")
+        ]
+
+    # then format each directive section
+    formatted_sections = {}
+    for directive, directives in directive_sections.items():
+        if directives:
+            formatted_sections[SPHINX_DIRECTIVES[directive]] = [
+                dir_func_map[directive](dirx, section["indentation"])
+                for dirx in directives
+            ]
+
+    # Put the description at the top
+    output = ""
+    dscr = roll_description(section["description"], section["indentation"])
+    if dscr:
+        output += dscr + "\n\n"
+
+    for dirtype, dirtextlist in formatted_sections.items():
+        full_name = f"{dirtype}\n{chr(32) * section['indentation']}{'-' * len(dirtype)}"
+        output += f"{full_name}\n\n"
+        output += "\n\n".join(dirtextlist) + "\n\n"
+
+    return output
+
+
 class DSParser:
     def __init__(self, file_path: str) -> None:
         self.file_path = file_path
         self.source = None
         self.docstrings_info: List[Dict[str, Any]] = []
+        self.docstrings_sections: List[Dict[str, Any]] = []
+        self.read_python_file()
 
     def read_python_file(self) -> None:
         self.source = read_python_file(self.file_path)
+        self.docstrings_info = get_docstring_info(self.file_path)
+        for ds_info in self.docstrings_info:
+            docstring = ds_info["docstring"]
+            ds_info["docstrings_sections"] = split_docstring(docstring)
+            ds_info["formatted_sections"] = format_docstring_section(
+                ds_info["docstrings_sections"]
+            )
+        self.docstrings_info
+
+    def write_formatted_file(self, file_path: Optional[str] = None) -> None:
+        source_lines = self.source.split("\n")
+        for ds_info in self.docstrings_info:
+            start_line = ds_info["start_line"]
+            end_line = ds_info["end_line"]
+            # start_line should be """ and end_line should be """
+            # get the indentation from source
+            source_lines[start_line - 1] = (
+                chr(32) * ds_info["indentation"]
+                + '"""\n'
+                + ds_info["formatted_sections"]
+            )
+            source_lines[end_line - 1] = '"""'
+
+        if file_path is None:
+            file_path = self.file_path
+        with open(file_path, "w") as file:
+            file.write("\n".join(source_lines))
 
 
 def format_python_file(file_path: str):
@@ -68,20 +262,9 @@ def format_python_file(file_path: str):
     Formats the docstrings in the given python file.
     :param <str> file_path: The path to the python file.
     """
-    # open the file
-    with open(file_path, "r", encoding="utf8") as file:
-        data_lines = file.readlines()
-
-    # find all docstrings
-    docstrings_info = get_docstring_info(file_path)
-    for docstring_info in docstrings_info:
-        print(json.dumps(docstring_info, indent=4))
-
-    # find the last occurance of os.sep in the file path
-    last_sep = file_path.rfind(".")
-    # file_path = file_path[:last_sep] + "_fmt" + file_path[last_sep:]
-    with open(file_path, "w", encoding="utf8") as file:
-        file.writelines(data_lines)
+    last_dot = file_path.rfind(".")
+    out_path = file_path[:last_dot] + "_fmt" + file_path[last_dot:]
+    DSParser(file_path).write_formatted_file(file_path=out_path)
 
 
 def format_python_files(root_dir: str):
