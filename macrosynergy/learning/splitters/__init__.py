9246ed0b745c7469c6e476685a48493d1134e507
Rushil Gholkar <67958144+rushilg99@users.noreply.github.com>
2024-07-31 16:25:57 +0100

    added frequency splitter to the subsubpackage constructor

--- macrosynergy/learning/splitters/__init__.py
+++ macrosynergy/learning/splitters/__init__.py
@@ -1,8 +1,12 @@
 from .base_panel_split import BasePanelSplit
 
-from .expanding_increment_splitters import ExpandingIncrementPanelSplit
+from .expanding_increment_splitters import (
+    ExpandingIncrementPanelSplit,
+    ExpandingFrequencyPanelSplit,
+)
 
 __all__ = [
     "BasePanelSplit",
     "ExpandingIncrementPanelSplit",
+    "ExpandingFrequencyPanelSplit",
 ]
