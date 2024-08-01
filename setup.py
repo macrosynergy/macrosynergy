9c5eae22c627bfd3e5ea63634173cb38197928c0
sandresen1 <146712777+sandresen1@users.noreply.github.com>
2024-08-01 12:17:24 +0100

    Change is released flag to false

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ if sys.version_info[:2] < (3, 8):
 
 MAJOR = 0
 MINOR = 1
-MICRO = 26
-ISRELEASED = True
+MICRO = 27
+ISRELEASED = False
 VERSION = "%d.%d.%d" % (MAJOR, MINOR, MICRO)
 
 if sys.version_info >= (3, 12):
