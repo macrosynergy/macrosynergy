f2c3fdd466e67839255dee23611d4e8961a060b5
Rushil Gholkar <67958144+rushilg99@users.noreply.github.com>
2024-08-03 14:47:21 +0100

    completed base panel split

--- macrosynergy/learning/splitters/base_panel_split.py
+++ macrosynergy/learning/splitters/base_panel_split.py
@@ -45,14 +45,16 @@ class BasePanelSplit(BaseCrossValidator, ABC):
         n_splits : int
             Number of splits in the cross-validator.
         """
-
-        return self.n_splits
+        if hasattr(self, "n_splits"):
+            return self.n_splits
+        else:
+            return np.nan
 
     def _calculate_xranges(
         self,
-        cs_dates: pd.DatetimeIndex,
-        real_dates: pd.DatetimeIndex,
-        freq_offset: pd.DateOffset,
+        cs_dates,
+        real_dates,
+        freq_offset,
     ):
         """
         Returns date ranges of contiguous blocks in each training and test set. 
@@ -60,7 +62,7 @@ class BasePanelSplit(BaseCrossValidator, ABC):
         Parameters
         ----------
         cs_dates : pd.DatetimeIndex
-            DatetimeIndex of dates in a set for a given cross-section.
+            DatetimeIndex of dates in a (training or test) set for a given cross-section.
         real_dates : pd.DatetimeIndex
             DatetimeIndex of all dates in the panel.
         freq_offset : pd.DateOffset
@@ -73,8 +75,10 @@ class BasePanelSplit(BaseCrossValidator, ABC):
         """
         xranges = []
         if len(cs_dates) == 0:
+            # No dates in the training or test set - return empty list. 
             return xranges
 
+        # Filter all dates in the panel spanning the dates in the considered set
         filtered_real_dates = real_dates[
             (real_dates >= cs_dates.min()) & (real_dates <= cs_dates.max())
         ]
@@ -86,7 +90,7 @@ class BasePanelSplit(BaseCrossValidator, ABC):
                 (cs_dates.min(), cs_dates.max() + freq_offset - cs_dates.min())
             )
         else:
-            # Handle multiple contiguous ranges of dates.
+            # Multiple contiguous blocks present. Iterate over them.
             while len(difference) > 0:
                 xranges.append((cs_dates.min(), difference.min() - cs_dates.min()))
                 cs_dates = cs_dates[(cs_dates >= difference.min())]
@@ -118,12 +122,12 @@ class BasePanelSplit(BaseCrossValidator, ABC):
             The dates must be in datetime format.
         figsize : Tuple[int, int]
             Tuple of integers specifying the splitter visualisation figure size.
-
-        TODO: adjust this logic due to new min_xcats parameter
         """
         sns.set_theme(style="whitegrid", palette="colorblind")
-        Xy: pd.DataFrame = pd.concat([X, y], axis=1).dropna()
-        cross_sections: np.ndarray[str] = np.array(
+
+        # Obtain relevant data
+        Xy: pd.DataFrame = self._combine_Xy(X, y)
+        cross_sections = np.array(
             sorted(Xy.index.get_level_values(0).unique())
         )
         real_dates = Xy.index.get_level_values(1).unique().sort_values()
@@ -132,7 +136,7 @@ class BasePanelSplit(BaseCrossValidator, ABC):
         freq_est = pd.infer_freq(real_dates)
         if freq_est is None:
             freq_est = real_dates.to_series().diff().min()
-        freq_offset = pd.tseries.frequencies.to_offset(freq_est)
+        freq_offset = pd.tseries.frequencies.to_offset(freq_est) # Good approximation
 
         splits = list(self.split(X, y))
 
@@ -153,14 +157,14 @@ class BasePanelSplit(BaseCrossValidator, ABC):
             if self.n_splits > 5
             else [f"Split {i+1}" for i in range(self.n_splits)]
         )
-
         fig, ax = plt.subplots(
-            nrows=len(Xy.index.get_level_values(0).unique()),
+            nrows=len(cross_sections),
             ncols=min(self.n_splits, 5),
             figsize=figsize,
         )
 
-        
+        # Determine ranges of contiguous dates in each training and test set, within each
+        # cross-section and split.
         operations = []
         for cs_idx, cs in enumerate(cross_sections):
             for idx, split_idx in enumerate(split_idxs):
@@ -183,13 +187,12 @@ class BasePanelSplit(BaseCrossValidator, ABC):
                 operations.append((cs_idx, idx, xranges_test, "lightcoral", "Test"))
 
         # Calculate the difference between final two dates.
-        # This will be added to the x-axis limits to ensure that the final split is
-        # visible.
-        last_date = real_dates[-1]  # Get the last date
-        second_to_last_date = real_dates[-2]  # Get the second-to-last date
+        # This will be added to the x-axis limits to ensure that the final split is visible.
+        last_date = real_dates[-1]
+        second_to_last_date = real_dates[-2]
         difference = last_date - second_to_last_date
 
-        # Finally, apply all your operations on the ax object.
+        # Add all the broken bar plots to the figure.
         for cs_idx, idx, xranges, color, label in operations:
             if len(cross_sections) == 1:
                 ax[idx].broken_barh(
@@ -225,3 +228,27 @@ class BasePanelSplit(BaseCrossValidator, ABC):
         plt.legend(frameon=True)
         plt.tight_layout()
         plt.show()
+
+    @abstractmethod
+    def _combine_Xy(self, X, y):
+        """
+        Combine the features and target variable into a single dataframe. This is 
+        dependent on the specific splitter implementation and, consequently, the 
+        implementation of the constructor.
+
+        Parameters
+        ----------
+        X : pd.DataFrame
+            Pandas dataframe of features/quantamental indicators, multi-indexed by
+            (cross-section, date). The dates must be in datetime format. The
+            dataframe must be in wide format: each feature is a column.
+        y : pd.DataFrame
+            Pandas dataframe of target variable, multi-indexed by (cross-section, date).
+            The dates must be in datetime format.
+
+        Returns
+        -------
+        Xy : pd.DataFrame
+            Combined dataframe of the features and the target variable.
+        """
+        pass
