e63b6614b804dd529e009458557bec379fe5fea5
Rushil Gholkar <67958144+rushilg99@users.noreply.github.com>
2024-08-03 19:23:47 +0100

    fixed broken expandingincrement splitters

--- tests/unit/learning/splitters/test_walk_forward_splitters.py
+++ tests/unit/learning/splitters/test_walk_forward_splitters.py
@@ -12,10 +12,7 @@ from parameterized import parameterized
 
 from macrosynergy.learning import (
     ExpandingIncrementPanelSplit,
-    ExpandingKFoldPanelSplit,
-    RollingKFoldPanelSplit,
 )
-
 
 class TestAll(unittest.TestCase):
     @classmethod
@@ -63,63 +60,13 @@ class TestAll(unittest.TestCase):
         plt.close("all")
         matplotlib.use(self.mpl_backend)
 
-    # def test_crossval_application(self):
-    #     # Given a generated panel with a true linear relationship between features and target,
-    #     # test that the cross validation procedure correctly identifies that a linear regression
-    #     # is more suitable than a 1-nearest neighbor model.
-    #     # self.setUp()
-
-    #     # models
-    #     lr = LinearRegression()
-    #     knn = KNeighborsRegressor(n_neighbors=1)
-    #     splitter = ExpandingIncrementPanelSplit(
-    #         train_intervals=1, min_cids=2, min_periods=21 * 12, test_size=1
-    #     )
-    #     lrsplits = cross_val_score(
-    #         lr,
-    #         self.X,
-    #         self.y,
-    #         scoring="neg_root_mean_squared_error",
-    #         cv=splitter,
-    #         n_jobs=-1,
-    #     )
-    #     knnsplits = cross_val_score(
-    #         knn,
-    #         self.X,
-    #         self.y,
-    #         scoring="neg_root_mean_squared_error",
-    #         cv=splitter,
-    #         n_jobs=-1,
-    #     )
-
-    #     self.assertLess(np.mean(-lrsplits), np.mean(-knnsplits))
-
-    @parameterized.expand([2, 4, 8])
-    def test_expanding_kfold(self, n_splits):
-        splitter = ExpandingKFoldPanelSplit(n_splits=n_splits)
-        splits = list(splitter.split(self.X, self.y))
-        self.assertEqual(len(splits), n_splits)
-
-    @parameterized.expand([2, 4, 8])
-    def test_rolling_kfold(self, n_splits):
-        splitter = RollingKFoldPanelSplit(n_splits=n_splits)
-        splits = list(splitter.split(self.X, self.y))
-        self.assertEqual(len(splits), n_splits)
-
-    def test_expanding_kfold_too_small(self):
-        with self.assertRaises(ValueError):
-            ExpandingKFoldPanelSplit(n_splits=1)
-
-    def test_rolling_k_fold_too_small(self):
-        with self.assertRaises(ValueError):
-            RollingKFoldPanelSplit(n_splits=1)
-
-    def test_expanding_kfold_split(self):
+    def test_expanding_increment_split_basic(self):
         periods1 = 6
         periods2 = 6
-        n_splits = 5
         X, y = self.make_simple_df(periods1=periods1, periods2=periods2)
-        splitter = ExpandingKFoldPanelSplit(n_splits=n_splits)
+        splitter = ExpandingIncrementPanelSplit(
+            train_intervals=1, test_size=1, min_periods=1, min_cids=2, max_periods=None
+        )
         for i, split in enumerate(splitter.split(X, y)):
             train_set, test_set = split
 
@@ -131,46 +78,29 @@ class TestAll(unittest.TestCase):
             self.assertEqual(test_set[0], i + 1)
             self.assertEqual(test_set[1], i + periods1 + 1)
 
-    def test_kfold_split(self):
-        periods1 = 5
+    def test_expanding_increment_split_min_cids(self):
+        periods1 = 6
         periods2 = 5
-        n_splits = 5
-        X, y = self.make_simple_df(periods1=periods1, periods2=periods2)
-        indices = np.arange(X.shape[0])
-        splitter = RollingKFoldPanelSplit(n_splits=n_splits)
+        X, y = self.make_simple_df(
+            start1="2020-01-01",
+            start2="2020-01-02",
+            periods1=periods1,
+            periods2=periods2,
+        )
+        splitter = ExpandingIncrementPanelSplit(
+            train_intervals=1, test_size=1, min_periods=1, min_cids=2, max_periods=None
+        )
         for i, split in enumerate(splitter.split(X, y)):
             train_set, test_set = split
 
+            # Training sets for each cid
+            self.assertEqual(train_set[i], i)
+            self.assertEqual(train_set[i * 2 + 2], i + periods1)
+            if i == 0:
+                self.assertEqual(train_set.shape[0], 3)
             # Test sets for each cid
-            self.assertEqual(test_set[0], i)
-            self.assertEqual(test_set[1], i + periods1)
-
-            # assert train set is indices excluding test set
-            self.assertTrue(np.array_equal(np.setdiff1d(indices, test_set), train_set))
-
-    def test_valid_visualise_splits(self):
-        # ExpandingIncrementPanelSplit
-        splitter1 = ExpandingIncrementPanelSplit(
-            train_intervals=21 * 12,
-            test_size=1,
-        )
-        try:
-            splitter1.visualise_splits(self.X, self.y)
-        except Exception as e:
-            self.fail(f"ExpandingIncrementPanelSplit.visualise_splits() raised {e}")
-
-        # RollingKFoldPanelSplit
-        splitter2 = RollingKFoldPanelSplit(n_splits=5)
-        try:
-            splitter2.visualise_splits(self.X, self.y)
-        except Exception as e:
-            self.fail(f"RollingKFoldPanelSplit.visualise_splits() raised {e}")
-        # ExpandingKFoldPanelSplit
-        splitter3 = ExpandingKFoldPanelSplit(n_splits=5)
-        try:
-            splitter3.visualise_splits(self.X, self.y)
-        except Exception as e:
-            self.fail(f"ExpandingKFoldPanelSplit.visualise_splits() raised {e}")
+            self.assertEqual(test_set[0], i + 2)
+            self.assertEqual(test_set[1], i + periods1 + 1)
 
     def make_simple_df(
         self,
@@ -207,14 +137,3 @@ class TestAll(unittest.TestCase):
         y = df["xcat2"]
 
         return X, y
-
-
-if __name__ == "__main__":
-    unittest.main()
-
-    # test = TestAll()
-    # X, y = test.make_simple_df(periods1=5, periods2=5)
-    # splitter = RollingKFoldPanelSplit(n_splits=5)
-    # splits = list(splitter.split(X, y))
-    # print(splits)
-    # splitter.visualise_splits(X, y)
