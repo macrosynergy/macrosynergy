d28fc76e501858c8e7986c7edb000cb989c1b50d
Rushil Gholkar <67958144+rushilg99@users.noreply.github.com>
2024-08-08 21:08:47 +0100

    first draft of learner worker

--- macrosynergy/learning/learner.py
+++ macrosynergy/learning/learner.py
@@ -6,6 +6,8 @@ from macrosynergy.panel import categories_df
 from joblib import Parallel, delayed
 from tqdm.auto import tqdm
 
+import warnings
+
 class BasePanelLearner:
     def __init__(self, df, xcats, cids = None, start = None, end = None, blacklist = None, freq = "M", lag = 1, xcat_aggs = ["last", "sum"]):
         """
@@ -91,6 +93,13 @@ class BasePanelLearner:
         self.X = df_long.iloc[:, :-1]
         self.y = df_long.iloc[:, -1]
 
+        # Store necessary index information
+        self.index = self.X.index
+        self.date_levels = self.index.get_level_values(1)
+        self.xs_levels = self.index.get_level_values(0)
+        self.unique_date_levels = self.date_levels.unique()
+        self.unique_xs_levels = self.xs_levels.unique()
+
     def run(
         self,
         name,
@@ -101,7 +110,7 @@ class BasePanelLearner:
         scorers, # List or string of scorers, not metrics
         search_type = "grid", # If string, allow "grid", "random", "greedy" or "bayes". Else allow a general sklearn class to be passed here. 
         n_iter = 100, # Number of iterations for random or bayes
-        splits_function = None, # Brainstorm this
+        split_dictionary = None, # Dictionary of {real_dates} 
         use_variance_correction = False, 
         n_jobs_outer = -1,
         n_jobs_inner = 1,
@@ -147,7 +156,9 @@ class BasePanelLearner:
         n_iter : int
             Number of iterations for random or bayes search. Default is 100.
 
-        splits_function : TODO
+        splits_function : Optional[dict]
+            Dictionary of changepoints for the number of splits in inner cross-validation 
+            splitters. Keys are real dates and values are integers. Default is None. 
 
         use_variance_correction : bool
             Boolean indicating whether or not to take into account variance across splits
@@ -172,7 +183,7 @@ class BasePanelLearner:
             scorers = scorers,
             search_type = search_type,
             n_iter = n_iter,
-            splits_function = splits_function,
+            split_dictionary = split_dictionary,
             use_variance_correction = use_variance_correction,
             n_jobs_outer = n_jobs_outer,
             n_jobs_inner = n_jobs_inner,
@@ -195,7 +206,7 @@ class BasePanelLearner:
                     use_variance_correction = use_variance_correction,
                     search_type = search_type,
                     n_iter = n_iter,
-                    splits_function = splits_function,
+                    splits_function = split_dictionary,
                     n_jobs_inner = n_jobs_inner,
                 )
                 for idx, (train_idx, test_idx) in enumerate(train_test_splits)
@@ -205,6 +216,124 @@ class BasePanelLearner:
 
         return optim_results
 
+    def _worker(
+        self,
+        name,
+        train_idx,
+        test_idx,
+        inner_splitters,
+        models,
+        hyperparameters,
+        scorers,
+        use_variance_correction,
+        search_type,
+        n_iter,
+        splits_function,
+        n_jobs_inner,
+    ):
+        """
+        Worker function for parallel processing of the learning process.
+        """
+        # Train-test split 
+        X_train, X_test = self.X.values[train_idx, :], self.X.values[test_idx, :]
+        y_train, y_test = self.y.values[train_idx], self.y.values[test_idx]
+
+        # Determine correct timestamps of test set forecasts 
+        # First get test index information
+        test_index = self.index[test_idx]
+        test_xs_levels = self.xs_levels[test_idx]
+        test_date_levels = self.date_levels[test_idx]
+        sorted_date_levels = sorted(test_date_levels.unique())
+
+        # Since the features lag behind the targets, the dates need to be adjusted
+        # by the lag applied. 
+        locs: np.ndarray = (
+            np.searchsorted(self.date_levels, sorted_date_levels, side="left") - self.lag
+        )
+        adj_test_date_levels: pd.DatetimeIndex = pd.DatetimeIndex(
+            [self.date_levels[i] if i >= 0 else pd.NaT for i in locs]
+        )
+
+        # Now formulate correct index
+        date_map = dict(zip(test_date_levels, adj_test_date_levels))
+        mapped_dates = test_date_levels.map(date_map)
+        test_index = pd.MultiIndex.from_arrays(
+            [test_xs_levels, mapped_dates], names=["cid", "real_date"]
+        )
+
+        # Run hyperparameter and model search
+        optim_name = None
+        optim_model = None
+        optim_score = -np.inf
+
+        optim_name, optim_model, optim_score, optim_params, n_splits  = self._hyperparameter_search(
+            X_train = X_train,
+            y_train = y_train,
+            inner_splitters = inner_splitters,
+            models = models,
+            hyperparameters = hyperparameters,
+            scorers = scorers,
+            search_type = search_type,
+            n_iter = n_iter,
+            splits_function = splits_function,
+            use_variance_correction = use_variance_correction,
+            n_jobs_inner = n_jobs_inner,
+        )
+
+        # Handle case where no model was selected
+        if optim_name is None:
+            warnings.warn(
+                f"No model was selected for {name} at time {self.date_levels[train_idx].max()}",
+                " Hence, resulting signals are set to zero.",
+                RuntimeWarning,
+            )
+            preds = np.zeros(y_test.shape)
+            prediction_data = [name, test_index, preds]
+            modelchoice_data = [
+                self.date_levels[train_idx],
+                name, 
+                None, # Model selected
+                None, # Hyperparameters selected
+                int(n_splits),
+            ]
+            other_data = None
+
+        else:
+            # Then a model was selected
+            optim_model.fit(X_train, y_train)
+
+            # If optim_model has a create_signal method, use it otherwise use predict
+            if hasattr(optim_model, "create_signal"):
+                if callable(getattr(optim_model, "create_signal")):
+                    preds: np.ndarray = optim_model.create_signal(X_test)
+                else:
+                    preds: np.ndarray = optim_model.predict(X_test)
+            else:
+                preds: np.ndarray = optim_model.predict(X_test)
+                
+            prediction_data = [name, test_index, preds]
+
+            # Store model choice information
+            modelchoice_data = [
+                self.date_levels[train_idx],
+                name,
+                optim_name,
+                optim_params,
+                int(n_splits),
+            ]
+
+            # Store other information
+            other_data: List[List] = self._extract_model_info(
+                name,
+                self.date_levels[train_idx],
+                optim_model,
+            )
+
+        return (
+            prediction_data,
+            modelchoice_data,
+            other_data,
+        )
 
 
     
