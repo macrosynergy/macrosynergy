f83a378b35046b4592d043d4698dff76ff5794e6
Rushil Gholkar <67958144+rushilg99@users.noreply.github.com>
2024-08-29 19:38:59 +0100

    preprocessing constructor

--- macrosynergy/learning/preprocessing/__init__.py
+++ macrosynergy/learning/preprocessing/__init__.py
@@ -5,9 +5,26 @@ from .selectors import (
     MapSelector,
 )
 
+from .scalers import (
+    PanelMinMaxScaler,
+    PanelStandardScaler,
+)
+
+from .transformers import (
+    FeatureAverager,
+    ZnScoreAverager,
+)
+
 __all__ = [
+    # selectors
     "LarsSelector",
     "LassoSelector",
     "ENetSelector",
     "MapSelector",
+    # scalers
+    "PanelMinMaxScaler",
+    "PanelStandardScaler",
+    # transformers
+    "FeatureAverager",
+    "ZnScoreAverager",
 ]
