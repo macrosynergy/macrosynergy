ad09c7b0603511fbac2b0c2204fbe2c5689c2eaa
Rushil Gholkar <67958144+rushilg99@users.noreply.github.com>
2024-09-26 10:21:05 +0100

    fixed signal optimizer imports

--- macrosynergy/learning/signal_optimizer.py
+++ macrosynergy/learning/signal_optimizer.py
@@ -18,12 +18,12 @@ from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 from sklearn.pipeline import Pipeline
 from tqdm.auto import tqdm
 
-from macrosynergy.learning.panel_time_series_split import (
-    BasePanelSplit,
+from macrosynergy.learning.splitters import (
     ExpandingIncrementPanelSplit,
     RollingKFoldPanelSplit,
     ExpandingKFoldPanelSplit,
 )
+from macrosynergy.learning.splitters.base_panel_split import BasePanelSplit
 
 from macrosynergy.learning.predictors import LADRegressionSystem
 from macrosynergy.management.validation import _validate_Xy_learning
