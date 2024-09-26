0fc3d6d5fa4050aeb9306ede42972c417025d927
Rushil Gholkar <67958144+rushilg99@users.noreply.github.com>
2024-09-26 09:26:12 +0100

    added splitters to constructor

--- macrosynergy/learning/splitters/__init__.py
+++ macrosynergy/learning/splitters/__init__.py
@@ -1,7 +1,16 @@
 from .base_panel_split import BasePanelSplit, KFoldPanelSplit, WalkForwardPanelSplit
+from .kfold_splitters import ExpandingKFoldPanelSplit, RollingKFoldPanelSplit, RecencyKFoldPanelSplit
+from .walk_forward_splitters import ExpandingIncrementPanelSplit, ExpandingFrequencyPanelSplit
 
 __all__ = [
     'BasePanelSplit',
     'KFoldPanelSplit',
     'WalkForwardPanelSplit',
+    # KFold splitters
+    'ExpandingKFoldPanelSplit',
+    'RollingKFoldPanelSplit',
+    'RecencyKFoldPanelSplit',
+    # Walk-forward splitters
+    'ExpandingIncrementPanelSplit',
+    'ExpandingFrequencyPanelSplit',
 ]
