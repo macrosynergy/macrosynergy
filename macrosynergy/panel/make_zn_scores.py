1e6e7906a724f6d80d656095bd491be2b1ba13d6
Samuel Andresen <sandresen@macrosynergy.com>
2024-09-30 15:50:30 +0100

    Feature: Add more helpful error message for when minimum observations are too large

--- macrosynergy/panel/make_zn_scores.py
+++ macrosynergy/panel/make_zn_scores.py
@@ -247,6 +247,12 @@ def make_zn_scores(
     dfw_zns_pan = dfw * 0
     dfw_zns_css = dfw * 0
 
+    if dfw.shape[0] < min_obs and pan_weight < 1 and pan_weight > 0:
+        raise ValueError(
+            f"The DataFrame has less than {min_obs} observations. "
+            "Please adjust the `min_obs` parameter."
+        )
+
     if pan_weight > 0:
         df_neutral = expanding_stat(
             dfw,
@@ -366,7 +372,7 @@ if __name__ == "__main__":
         iis=True,
         neutral="mean",
         pan_weight=0.75,
-        min_obs=261,
+        min_obs=10000,
         est_freq="m",
     )
     print(dfzm)
