d33a6b101a575fcb35ae45a3d53876473a315361
Rushil Gholkar <67958144+rushilg99@users.noreply.github.com>
2024-10-01 18:57:27 +0100

    started weighted lad regressor

--- macrosynergy/learning/forecasting/__init__.py
+++ macrosynergy/learning/forecasting/__init__.py
@@ -1,7 +1,12 @@
-from .lad_regressor import (
+from .lad_regressors import (
     LADRegressor,
 )
 
+from .weighted_regressors import (
+    BaseWeightedRegressor,
+)
+
 __all__ = [
+    "BaseWeightedRegressor",
     "LADRegressor",
 ]
