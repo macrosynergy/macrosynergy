b721b09571cce7f255c816a44cab58cdbb6f7e05
Rushil Gholkar <67958144+rushilg99@users.noreply.github.com>
2024-10-02 15:27:34 +0100

    constructor for weighted L2 linear regressors

---
+++ macrosynergy/learning/forecasting/linear_model/ols_regressors/__init__.py
@@ -0,0 +1,9 @@
+from .weighted_ols_regressors import (
+    SignWeightedLinearRegression,
+    TimeWeightedLinearRegression,
+)
+
+__all__ = [
+    "SignWeightedLinearRegression",
+    "TimeWeightedLinearRegression",
+]
