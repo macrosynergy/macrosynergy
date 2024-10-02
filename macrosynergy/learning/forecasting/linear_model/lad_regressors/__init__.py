05da65d8ab077f5c3938f2992cccb17c37660865
Rushil Gholkar <67958144+rushilg99@users.noreply.github.com>
2024-10-02 15:09:46 +0100

    constructor for lad regressors

--- macrosynergy/learning/forecasting/linear_model/lad_regressors/__init__.py
+++ macrosynergy/learning/forecasting/linear_model/lad_regressors/__init__.py
@@ -2,6 +2,13 @@ from .lad_regressor import (
     LADRegressor,
 )
 
+from .weighted_lad_regressors import (
+    SignWeightedLADRegressor,
+    TimeWeightedLADRegressor,
+)
+
 __all__ = [
     "LADRegressor",
+    "SignWeightedLADRegressor",
+    "TimeWeightedLADRegressor",
 ]
