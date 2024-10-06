19ff981acc97eb1c72634cee20c378a71eabb406
rushilg99 <rushil1.gholkar@gmail.com>
2024-10-06 23:50:46 +0100

    start lad regressor tests

---
+++ tests/unit/learning/forecasting/test_lad_regressor.py
@@ -0,0 +1,48 @@
+import os
+import numpy as np
+import pandas as pd
+
+import unittest
+
+from macrosynergy.learning import (
+    LADRegressor,
+)
+
+from parameterized import parameterized
+
+class TestLADRegressor(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        # Generate data with true linear relationship
+        cids = ["AUD", "CAD", "GBP", "USD"]
+        xcats = ["XR", "CPI", "GROWTH", "RIR"]
+
+        df_cids = pd.DataFrame(index=cids, columns=["earliest", "latest"])
+        df_cids.loc["AUD"] = ["2019-01-01", "2020-12-31"]
+        df_cids.loc["CAD"] = ["2020-01-01", "2020-12-31"]
+        df_cids.loc["GBP"] = ["2020-01-01", "2020-12-31"]
+        df_cids.loc["USD"] = ["2019-06-01", "2020-12-31"]
+
+        tuples = []
+
+        for cid in cids:
+            # get list of all eligible dates
+            sdate = df_cids.loc[cid]["earliest"]
+            edate = df_cids.loc[cid]["latest"]
+            all_days = pd.date_range(sdate, edate)
+            work_days = all_days[all_days.weekday < 5]
+            for work_day in work_days:
+                tuples.append((cid, work_day))
+
+        n_samples = len(tuples)
+        ftrs = np.random.normal(loc=0, scale=1, size=(n_samples, 3))
+        labels = np.matmul(ftrs, [1, 2, -1]) + np.random.normal(0, 0.5, len(ftrs))
+        df = pd.DataFrame(
+            data=np.concatenate((np.reshape(labels, (-1, 1)), ftrs), axis=1),
+            index=pd.MultiIndex.from_tuples(tuples, names=["cid", "real_date"]),
+            columns=xcats,
+            dtype=np.float32,
+        )
+
+        self.X = df.drop(columns="XR")
+        self.y = df["XR"]
