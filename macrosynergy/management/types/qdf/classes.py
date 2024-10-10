9c6be56335e1efb527dd6916d9a197dc276dce30
Palash Tyagi <23239946+Magnus167@users.noreply.github.com>
2024-10-10 09:39:57 +0100

    Refactor QuantamentalDataFrame initialization to improve categorical handling

--- macrosynergy/management/types/qdf/classes.py
+++ macrosynergy/management/types/qdf/classes.py
@@ -47,18 +47,20 @@ class QuantamentalDataFrame(QuantamentalDataFrameBase):
         if type(df) is QuantamentalDataFrame:
             if _initialized_as_categorical is None:
                 _initialized_as_categorical = df.InitializedAsCategorical
-
-        df = df[get_col_sort_order(df)]
+        else:
+            df = df[get_col_sort_order(df)]
         super().__init__(df)
+        _check_cat = check_is_categorical(self)
         if _initialized_as_categorical is None:
-            self.InitializedAsCategorical = check_is_categorical(self)
+            self.InitializedAsCategorical = _check_cat
         else:
             if not isinstance(_initialized_as_categorical, bool):
                 raise TypeError("`_initialized_as_categorical` must be a boolean.")
             self.InitializedAsCategorical = _initialized_as_categorical
 
         if categorical:
-            self.to_categorical()
+            if not _check_cat:
+                self.to_categorical()
         else:
             self.to_string_type()
 
