ad98f2b814e8069892acfd7e578ecb29f658653c
Rushil Gholkar <67958144+rushilg99@users.noreply.github.com>
2024-10-16 12:35:08 +0100

    example scaler usage

--- macrosynergy/learning/preprocessing/scalers/scalers.py
+++ macrosynergy/learning/preprocessing/scalers/scalers.py
@@ -1,11 +1,6 @@
-import pandas as pd
-
 from macrosynergy.learning.preprocessing.scalers.base_panel_scaler import (
     BasePanelScaler,
 )
-
-from typing import Any, Union
-
 
 class PanelMinMaxScaler(BasePanelScaler):
     """
@@ -141,3 +136,62 @@ class PanelStandardScaler(BasePanelScaler):
                 return X[feature] / statistics[1]
             else:
                 return X[feature]
+
+if __name__ == "__main__":
+    import numpy as np
+    import pandas as pd
+
+    from macrosynergy.management import (
+        categories_df,
+        make_qdf,
+    )
+    cids = ["AUD", "CAD", "GBP", "USD"]
+    xcats = ["XR", "CRY", "GROWTH", "INFL"]
+    cols = ["earliest", "latest", "mean_add", "sd_mult", "ar_coef", "back_coef"]
+
+    """Example: Unbalanced panel """
+
+    df_cids = pd.DataFrame(
+        index=cids, columns=["earliest", "latest", "mean_add", "sd_mult"]
+    )
+    df_cids.loc["AUD"] = ["2002-01-01", "2020-12-31", 0, 1]
+    df_cids.loc["CAD"] = ["2003-01-01", "2020-12-31", 0, 1]
+    df_cids.loc["GBP"] = ["2000-01-01", "2020-12-31", 0, 1]
+    df_cids.loc["USD"] = ["2000-01-01", "2020-12-31", 0, 1]
+
+    df_xcats = pd.DataFrame(index=xcats, columns=cols)
+    df_xcats.loc["XR"] = ["2000-01-01", "2020-12-31", 0.1, 1, 0, 0.3]
+    df_xcats.loc["CRY"] = ["2000-01-01", "2020-12-31", 1, 2, 0.95, 1]
+    df_xcats.loc["GROWTH"] = ["2000-01-01", "2020-12-31", 1, 2, 0.9, 1]
+    df_xcats.loc["INFL"] = ["2000-01-01", "2020-12-31", -0.1, 2, 0.8, 0.3]
+
+    dfd = make_qdf(df_cids, df_xcats, back_ar=0.75)
+    dfd["grading"] = np.ones(dfd.shape[0])
+    black = {
+        "GBP": (
+            pd.Timestamp(year=2009, month=1, day=1),
+            pd.Timestamp(year=2012, month=6, day=30),
+        ),
+        "CAD": (
+            pd.Timestamp(year=2015, month=1, day=1),
+            pd.Timestamp(year=2100, month=1, day=1),
+        ),
+    }
+
+    train = categories_df(
+        df=dfd, xcats=xcats, cids=cids, val="value", blacklist=black, freq="M", lag=1
+    ).dropna()
+    train = train[train.index.get_level_values(1) >= pd.Timestamp(year=2005,month=8,day=1)]
+
+    X_train = train.drop(columns=["XR"])
+    y_train = train["XR"]
+
+    # Standard scaling over each cross-section
+    scaler = PanelStandardScaler(type="cross_section")
+    scaler.fit(X_train, y_train)
+    print(scaler.transform(X_train))
+
+    # MinMax scaling over the panel
+    scaler = PanelMinMaxScaler()
+    scaler.fit(X_train, y_train)
+    print(scaler.transform(X_train))
