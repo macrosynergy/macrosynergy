ce39eaba45f142b30c27eee3900196882023aecb
Samuel Andresen <sandresen@macrosynergy.com>
2024-10-22 10:46:05 +0100

    Change setup.py to not include timestamp

--- setup.py
+++ setup.py
@@ -81,10 +81,8 @@ def get_version_info():
         GIT_REVISION = "Unknown"
 
     if not ISRELEASED:
-        import time
 
-        time_stamp = time.strftime("%Y%m%d%H%M", time.localtime())
-        FULLVERSION += f"dev0+{time_stamp}_{GIT_REVISION[:7]}"
+        FULLVERSION += f"dev0+{GIT_REVISION[:7]}"
 
     return FULLVERSION, GIT_REVISION
 
