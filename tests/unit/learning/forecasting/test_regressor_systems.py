9549e2e46a848339a0e68f7893339386d3fb6860
rushilg99 <rushil1.gholkar@gmail.com>
2024-10-25 20:07:57 +0100

    linear regression system unit tests

--- tests/unit/learning/forecasting/test_regressor_systems.py
+++ tests/unit/learning/forecasting/test_regressor_systems.py
@@ -12,11 +12,16 @@ from macrosynergy.learning import (
     RidgeRegressionSystem,
     CorrelationVolatilitySystem,
 )
+from macrosynergy.learning.forecasting.model_systems.base_regression_system import (
+    BaseRegressionSystem,
+)
 
 from sklearn.linear_model import (
     LinearRegression,
     Ridge,
 )
+
+from sklearn.base import BaseEstimator, RegressorMixin
 
 from parameterized import parameterized
 
@@ -56,15 +61,217 @@ class TestLinearRegressionSystem(unittest.TestCase):
         )
 
         self.X = df.drop(columns="XR")
+        self.X_nan = self.X.copy()
+        self.X_nan["nan_col"] = np.nan
+
         self.y = df["XR"]
+        self.y_nan = self.y.copy()
+        self.y_nan.iloc[0] = np.nan
+
+    def test_types_fit(self):
+        """
+        Test validity of parameters entered into the fit method.
+        """
+        model = LinearRegressionSystem()
+        # X
+        self.assertRaises(TypeError, model.fit, X=1, y=self.y)
+        self.assertRaises(TypeError, model.fit, X="string", y=self.y)
+        self.assertRaises(ValueError, model.fit, X=self.X.reset_index(), y=self.y)
+        self.assertRaises(ValueError, model.fit, X=self.X_nan, y=self.y)
+        # y
+        self.assertRaises(TypeError, model.fit, X=self.X, y=1)
+        self.assertRaises(TypeError, model.fit, X=self.X, y="string")
+        self.assertRaises(ValueError, model.fit, X=self.X, y=self.y.reset_index())
+        self.assertRaises(ValueError, model.fit, X=self.X, y=self.y_nan)
+
+    @parameterized.expand([True])#, False])
+    def test_valid_fit(self, single_feature):
+        """ Check the model fits are as expected """
+        cross_sections = self.X.index.get_level_values(0).unique()
+        param_names = ["roll", "fit_intercept", "positive", "data_freq", "min_xs_samples"]
+        param_values = list(
+            itertools.product(
+                [None, 21, 21 * 6, 21 * 12], # roll
+                [True, False], # fit_intercept
+                [True, False], # positive
+                [None, "unadjusted"], # data_freq
+                [2, 21*15], # min_xs_samples
+            )
+        )
+        for params in param_values:
+            param_dict = {name: param for name, param in zip(param_names, params)}
+            system_model = LinearRegressionSystem().set_params(**param_dict)
+            if single_feature:
+                system_model.fit(pd.DataFrame(self.X.iloc[:,0]), self.y)
+            # else:
+            #     system_model.fit(pd.DataFrame(self.X), self.y)
+            system_coefs = system_model.coefs_
+            system_intercepts = system_model.intercepts_
+            for cid in cross_sections:
+                model = LinearRegression().set_params(**{key: value for key, value in param_dict.items() if key in ["fit_intercept", "positive"]})
+                unique_dates = sorted(self.X.xs(cid).index.unique())
+                if params[4] > len(unique_dates):
+                    continue
+                if params[0] is not None:
+                    roll_dates = unique_dates[-params[0]:]
+                    if len(roll_dates) >= len(unique_dates):
+                        continue
+                    if single_feature:
+                        X = self.X.iloc[:,0][self.X.index.get_level_values(1).isin(roll_dates)]
+                        y = self.y[self.y.index.get_level_values(1).isin(roll_dates)]
+                    # else:
+                    #     X = self.X[self.X.index.get_level_values(1).isin(roll_dates)] 
+                    #     y = self.y[self.y.index.get_level_values(1).isin(roll_dates)]
+                else:
+                    if single_feature:
+                        X =  self.X.iloc[:,0]
+                        y = self.y
+                    # else:
+                    #     X = self.X
+                    #     y = self.y
+
+                if single_feature:
+                    model.fit(pd.DataFrame(X).xs(cid, level=0), y.xs(cid, level=0))
+                # else:
+                #     model.fit(pd.DataFrame(X).xs(cid, level=0), y.xs(cid, level=0))
+                # Check that the model coefficients and intercepts are the same
+                np.testing.assert_almost_equal(model.intercept_, system_intercepts[cid])
+                np.testing.assert_almost_equal(model.coef_, system_coefs[cid])
+
+        # Now test that weekly frequency works as expected
+        param_values = list(
+            itertools.product(
+                [None, 5*3, 5*6, 5*12], # roll
+                [True, False], # fit_intercept
+                [True, False], # positive
+                ["W"], # data_freq
+                [2], # min_xs_samples
+            )
+        )
+
+        for params in param_values:
+            param_dict = {name: param for name, param in zip(param_names, params)}
+            system_model = LinearRegressionSystem().set_params(**param_dict)
+            if single_feature:
+                system_model.fit(pd.DataFrame(self.X.iloc[:,0]), self.y)
+            else:
+                system_model.fit(pd.DataFrame(self.X), self.y)
+            system_coefs = system_model.coefs_
+            system_intercepts = system_model.intercepts_
+            for cid in cross_sections:
+                # First downsample the data to weekly
+                X = self.X.groupby(
+                    [
+                        pd.Grouper(level="cid"),
+                        pd.Grouper(level="real_date", freq="W"),
+                    ]
+                ).sum()
+                y = self.y.groupby(
+                    [
+                        pd.Grouper(level="cid"),
+                        pd.Grouper(level="real_date", freq="W"),
+                    ]
+                ).sum()
+                model = LinearRegression().set_params(**{key: value for key, value in param_dict.items() if key in ["fit_intercept", "positive"]})
+                unique_dates = sorted(X.xs(cid).index.unique())
+                if params[4] > len(unique_dates):
+                    continue
+                if params[0] is not None:
+                    roll_dates = unique_dates[-params[0]:]
+                    if len(roll_dates) >= len(unique_dates):
+                        continue
+                    if single_feature:
+                        X = X.iloc[:,0][X.index.get_level_values(1).isin(roll_dates)]
+                        y = y[y.index.get_level_values(1).isin(roll_dates)]
+                    else:
+                        X = X[X.index.get_level_values(1).isin(roll_dates)] 
+                        y = y[y.index.get_level_values(1).isin(roll_dates)]
+                else:
+                    if single_feature:
+                        X =  X.iloc[:,0]
+
+                if single_feature:
+                    model.fit(pd.DataFrame(X).xs(cid, level=0), y.xs(cid, level=0))
+                else:
+                    model.fit(pd.DataFrame(X).xs(cid, level=0), y.xs(cid, level=0))
+                # Check that the model coefficients and intercepts are the same
+                np.testing.assert_almost_equal(model.intercept_, system_intercepts[cid], decimal=5)
+                np.testing.assert_almost_equal(model.coef_, system_coefs[cid],decimal=5)
+
+        # Now test that monthly frequency works as expected
+        param_values = list(
+            itertools.product(
+                [None, 6, 12], # roll
+                [True, False], # fit_intercept
+                [True, False], # positive
+                ["M"], # data_freq
+                [2], # min_xs_samples
+            )
+        )
+
+        for params in param_values:
+            param_dict = {name: param for name, param in zip(param_names, params)}
+            system_model = LinearRegressionSystem().set_params(**param_dict)
+            if single_feature:
+                system_model.fit(pd.DataFrame(self.X.iloc[:,0]), self.y)
+            else:
+                system_model.fit(pd.DataFrame(self.X), self.y)
+            system_coefs = system_model.coefs_
+            system_intercepts = system_model.intercepts_
+            for cid in cross_sections:
+                # First downsample the data to weekly
+                X = self.X.groupby(
+                    [
+                        pd.Grouper(level="cid"),
+                        pd.Grouper(level="real_date", freq="M"),
+                    ]
+                ).sum()
+                y = self.y.groupby(
+                    [
+                        pd.Grouper(level="cid"),
+                        pd.Grouper(level="real_date", freq="M"),
+                    ]
+                ).sum()
+                model = LinearRegression().set_params(**{key: value for key, value in param_dict.items() if key in ["fit_intercept", "positive"]})
+                unique_dates = sorted(X.xs(cid).index.unique())
+                if params[4] > len(unique_dates):
+                    continue
+                if params[0] is not None:
+                    unique_dates = sorted(y.xs(cid).index.unique())
+                    roll_dates = unique_dates[-params[0]:]
+                    if len(roll_dates) >= len(unique_dates):
+                        continue
+                    if single_feature:
+                        X = X.iloc[:,0][X.index.get_level_values(1).isin(roll_dates)]
+                        y = y[y.index.get_level_values(1).isin(roll_dates)]
+                    else:
+                        X = X[X.index.get_level_values(1).isin(roll_dates)] 
+                        y = y[y.index.get_level_values(1).isin(roll_dates)]
+                else:
+                    if single_feature:
+                        X =  X.iloc[:,0]
+
+                if single_feature:
+                    model.fit(pd.DataFrame(X).xs(cid, level=0), y.xs(cid, level=0))
+                else:
+                    model.fit(pd.DataFrame(X).xs(cid, level=0), y.xs(cid, level=0))
+                # Check that the model coefficients and intercepts are the same
+                np.testing.assert_almost_equal(model.intercept_, system_intercepts[cid], decimal=5)
+                np.testing.assert_almost_equal(model.coef_, system_coefs[cid],decimal=5)
+
+    def test_types_predict(self):
+        pass
+
+    def test_valid_predict(self):
+        pass
 
     def test_check_init_params(self):
         # Test default params
         model = LinearRegressionSystem()
-        self.assertEqual(model.roll, None)
+        self.assertEqual(model.roll, "full")
         self.assertEqual(model.fit_intercept, True)
         self.assertEqual(model.positive, False)
-        self.assertEqual(model.data_freq, "D")
+        self.assertEqual(model.data_freq, None)
         self.assertEqual(model.min_xs_samples, 2)
 
         # Test custom params
@@ -82,27 +289,35 @@ class TestLinearRegressionSystem(unittest.TestCase):
         self.assertEqual(model.min_xs_samples, 3)
 
     def test_invalid_params(self):
-
         with self.assertRaises(TypeError):
             LinearRegressionSystem(roll=5.5)
         with self.assertRaises(ValueError):
             LinearRegressionSystem(roll=-5)
+        with self.assertRaises(TypeError):
+            LinearRegressionSystem(roll=None)
         with self.assertRaises(TypeError):
             LinearRegressionSystem(fit_intercept="False")
         with self.assertRaises(TypeError):
             LinearRegressionSystem(positive="True")
         with self.assertRaises(TypeError):
             LinearRegressionSystem(data_freq=5)
+        with self.assertRaises(ValueError):
+            LinearRegressionSystem(data_freq="hello")
         with self.assertRaises(TypeError):
             LinearRegressionSystem(min_xs_samples="2")
         with self.assertRaises(ValueError):
-            LinearRegressionSystem(roll=-5)
+            LinearRegressionSystem(min_xs_samples=-5)
+        with self.assertRaises(ValueError):
+            LinearRegressionSystem(min_xs_samples=0)
+        with self.assertRaises(ValueError):
+            LinearRegressionSystem(min_xs_samples=1)
+        with self.assertRaises(TypeError):
+            LinearRegressionSystem(min_xs_samples=3.7)
 
     @parameterized.expand(
-        [(5, True, False, "D", 2), (5, False, True, "D", 2), (5, True, True, "M", 2)]
+        [(5, True, False, "unadjusted", 2), (5, False, True, "unadjusted", 2), (5, True, True, "M", 2)]
     )
     def test_valid_init(self, roll, fit_intercept, positive, data_freq, min_xs_samples):
-
         # Test default params
         try:
             model = LinearRegressionSystem()
@@ -110,6 +325,16 @@ class TestLinearRegressionSystem(unittest.TestCase):
             self.fail(
                 "LinearRegressionSystem constructor raised an exception: {}".format(e)
             )
+        self.assertEqual(model.roll, "full")
+        self.assertEqual(model.fit_intercept, True)
+        self.assertEqual(model.positive, False)
+        self.assertEqual(model.data_freq, None)
+        self.assertEqual(model.min_xs_samples, 2)
+        self.assertIsInstance(model, LinearRegressionSystem)
+        self.assertIsInstance(model, BaseRegressionSystem)
+        self.assertIsInstance(model, RegressorMixin)
+        self.assertIsInstance(model, BaseEstimator)
+
         try:
             model = LinearRegressionSystem(
                 roll=roll,
@@ -122,12 +347,39 @@ class TestLinearRegressionSystem(unittest.TestCase):
             self.fail(
                 "LinearRegressionSystem constructor raised an exception: {}".format(e)
             )
+        self.assertEqual(model.roll, roll)
+        self.assertEqual(model.fit_intercept, fit_intercept)
+        self.assertEqual(model.positive, positive)
+        self.assertEqual(model.data_freq, data_freq)
+        self.assertEqual(model.min_xs_samples, min_xs_samples)
+        self.assertIsInstance(model, LinearRegressionSystem)
+        self.assertIsInstance(model, BaseRegressionSystem)
+        self.assertIsInstance(model, RegressorMixin)
+        self.assertIsInstance(model, BaseEstimator)
 
-    def test_create_model(self):
-        model = LinearRegressionSystem()
+    @parameterized.expand(
+        [
+            ("full", True, False, "unadjusted", 2),
+            ("full", False, False, "unadjusted", 2),
+            ("full", False, True, "unadjusted", 2),
+            ("full", True, True, "unadjusted", 2),
+            (5, True, False, "unadjusted", 2),
+            (5, False, False, "unadjusted", 2),
+            (5, False, True, "unadjusted", 2),
+            (5, True, True, "unadjusted", 2),
+            (21, True, False, "unadjusted", 2),
+            (21, False, False, "unadjusted", 2),
+            (21, False, True, "unadjusted", 2),
+            (21, True, True, "unadjusted", 2),
+        ]
+    )
+    def test_create_model(self, roll, fit_intercept, positive, data_freq, min_xs_samples):
+        model = LinearRegressionSystem(roll=roll, fit_intercept=fit_intercept, positive=positive, data_freq=data_freq, min_xs_samples=min_xs_samples)
         self.assertIsInstance(model.create_model(), LinearRegression)
+        self.assertEqual(model.create_model().fit_intercept, fit_intercept)
+        self.assertEqual(model.create_model().positive, positive)
 
-    @parameterized.expand(itertools.product([None, 5], ["D", "W", "M", "Q"]))
+    @parameterized.expand(itertools.product(["full", 5], ["unadjusted", "W", "M", "Q"]))
     def test_fit(self, roll, data_freq):
         model = LinearRegressionSystem(roll=roll, data_freq=data_freq)
         model.fit(self.X, self.y)
