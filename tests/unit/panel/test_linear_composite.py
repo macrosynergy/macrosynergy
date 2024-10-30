6cba8169e4d3005ca6ac2bedbe11ffdc6489154c
Palash Tyagi <ptyagi@macrosynergy.com>
2024-10-30 16:55:03 +0000

    removed test_linear_composite_weights_and_signs

--- tests/unit/panel/test_linear_composite.py
+++ tests/unit/panel/test_linear_composite.py
@@ -677,56 +677,6 @@ class TestAll(unittest.TestCase):
             np.all(rdf[rdf["real_date"] != "2000-01-17"]["value"].values == 0)
         )
 
-    def test_linear_composite_weights_and_signs(self):
-        """
-        Meant to test the "cid_agg" mode of the linear_composite function.
-        (i.e. engaging `_linear_composite_cid_agg()` and `_check_weights_and_signs()`)
-        """
-        long_cids_list = ["AUD", "CAD", "GBP", "JPY", "USD", "EUR", "INR", "CNY"]
-        tickers = [f"{c}_TESTXCAT" for c in long_cids_list]
-        test_xcats = ["TESTXCAT", "ZEROXCAT"]
-
-        ticker_values = [(-n) ** 3 for n in range(len(tickers))]
-        expected_values = []
-        df_list = []
-        for cid, ticker, exval in zip(long_cids_list, tickers, ticker_values):
-            df = make_test_df(tickers=[ticker])
-            df["value"] = exval
-            df_list.append(df)
-            df = make_test_df(cids=[cid], xcats=["ZEROXCAT"])
-            df["value"] = 0
-            df_list.append(df)
-            expected_values += [exval / 2]
-
-        extra_cids = ["ECID1", "ECID2"]
-        extra_xcats = ["eXCAT1", "eXCAT2"]
-        df_list.append(make_test_df(cids=extra_cids, xcats=extra_xcats))
-
-        df = pd.concat(df_list)
-
-        # test that when passed for xcat agg mode, the results come back as it is
-        rdf = linear_composite(
-            df=df,
-            xcats=test_xcats,
-            cids=long_cids_list,
-        )
-
-        for cid, exval in zip(long_cids_list, expected_values):
-            vx = rdf[(rdf["cid"] == cid)]["value"].unique().tolist()
-            self.assertTrue(vx == [exval])
-
-        # test that wehn extra cids are passed, the results come back as it is
-        long_extra_cids_list = long_cids_list + extra_cids
-        rdf = linear_composite(
-            df=df,
-            xcats=test_xcats,
-            cids=long_extra_cids_list,
-        )
-
-        for cid, exval in zip(long_cids_list, expected_values):
-            vx = rdf[(rdf["cid"] == cid)]["value"].unique().tolist()
-            self.assertTrue(vx == [exval])
-
 
 if __name__ == "__main__":
     unittest.main()
