2f87a244277bf4649b0c1626a5148e060ce77eb8
Palash Tyagi <ptyagi@macrosynergy.com>
2024-11-04 20:56:00 +0000

    added docstrings

--- macrosynergy/management/types/qdf/classes.py
+++ macrosynergy/management/types/qdf/classes.py
@@ -2,7 +2,7 @@
 Module hosting custom types and meta-classes for use across the package.
 """
 
-from typing import Optional, Any, Mapping, Union, Callable, Sequence, List, Tuple
+from typing import Optional, Mapping, Union, Sequence, List, Tuple, Dict
 import pandas as pd
 from macrosynergy.compat import PD_2_0_OR_LATER
 from .methods import (
@@ -78,18 +78,33 @@ class QuantamentalDataFrame(QuantamentalDataFrameBase):
     def is_categorical(self) -> bool:
         """
         Returns True if the QuantamentalDataFrame is categorical.
+
+        Returns
+        -------
+        bool
+            True if the QuantamentalDataFrame is categorical
         """
         return check_is_categorical(self)
 
     def to_categorical(self) -> "QuantamentalDataFrame":
         """
         Converts the QuantamentalDataFrame to a categorical DataFrame.
+
+        Returns
+        -------
+        QuantamentalDataFrame
+            The QuantamentalDataFrame with categorical columns.
         """
         return change_column_format(self, cols=self._StrIndexCols, dtype="category")
 
     def to_string_type(self) -> "QuantamentalDataFrame":
         """
         Converts the QuantamentalDataFrame to a string DataFrame.
+
+        Returns
+        -------
+        QuantamentalDataFrame
+            The QuantamentalDataFrame with string columns.
 
         """
         return change_column_format(self, cols=self._StrIndexCols, dtype="object")
@@ -98,6 +113,13 @@ class QuantamentalDataFrame(QuantamentalDataFrameBase):
         """
         Converts the QuantamentalDataFrame to its original dtypes (using the
         `InitialisedAsCategorical` attribute).
+
+        Returns
+        -------
+        QuantamentalDataFrame
+            The QuantamentalDataFrame with its original dtypes. The dtype is determined
+            by the `InitialisedAsCategorical` attribute. The output dtype will be either
+            'category' or 'object'.
         """
 
         if self.InitializedAsCategorical:
@@ -105,12 +127,26 @@ class QuantamentalDataFrame(QuantamentalDataFrameBase):
         return self.to_string_type()
 
     def list_tickers(self) -> List[str]:
+        """
+        List all tickers in the QuantamentalDataFrame.
+
+        Returns
+        -------
+        List[str]
+            A list of all tickers in the QuantamentalDataFrame.
+        """
         ltickers: List[str] = sorted(_get_tickers_series(self).unique())
         return ltickers
 
     def add_ticker_column(self) -> "QuantamentalDataFrame":
         """
-        Add a ticker column to the QuantamentalDataFrame.
+        Add a ticker column to the QuantamentalDataFrame. `ticker` is a combination of
+        `cid` and `xcat` columns. i.e. `ticker = cid_xcat`.
+
+        Returns
+        -------
+        QuantamentalDataFrame
+            The QuantamentalDataFrame with a `ticker` column.
         """
         ticker_col = _get_tickers_series(self)
         self["ticker"] = ticker_col
@@ -119,6 +155,16 @@ class QuantamentalDataFrame(QuantamentalDataFrameBase):
     def drop_ticker_column(self) -> "QuantamentalDataFrame":
         """
         Drop the ticker column from the QuantamentalDataFrame.
+
+        Raises
+        ------
+        ValueError
+            If no `ticker` column is found in the DataFrame.
+
+        Returns
+        -------
+        QuantamentalDataFrame
+            The QuantamentalDataFrame without the `ticker` column.
         """
         if "ticker" not in self.columns:
             raise ValueError("No `ticker` column found in the DataFrame.")
@@ -130,15 +176,39 @@ class QuantamentalDataFrame(QuantamentalDataFrameBase):
         xcats: Optional[Sequence[str]] = None,
         start: Optional[str] = None,
         end: Optional[str] = None,
-        blacklist: Mapping[str, Sequence[Union[str, pd.Timestamp]]] = None,
+        blacklist: Dict[str, Sequence[Union[str, pd.Timestamp]]] = None,
         out_all: bool = False,
         intersect: bool = False,
-        inplace: bool = False,
     ) -> Union[
         "QuantamentalDataFrame", Tuple["QuantamentalDataFrame", List[str], List[str]]
     ]:
         """
         Filter DataFrame by `cids`, `xcats`, and `start` & `end` dates.
+
+        Parameters
+        ----------
+        cids : Optional[Sequence[str]], optional
+            List of CIDs to filter by, by default None
+        xcats : Optional[Sequence[str]], optional
+            List of XCATs to filter by, by default None
+        start : Optional[str], optional
+            Start date to filter by, by default None
+        end : Optional[str], optional
+            End date to filter by, by default None
+        blacklist : Dict[str, Sequence[Union[str, pd.Timestamp]]], optional
+            Blacklist to apply to the DataFrame, by default None
+        out_all : bool, optional
+            If True, return the filtered DataFrame, the filtered XCATs, and the filtered
+            CIDs, by default False
+
+        Returns
+        -------
+        QuantamentalDataFrame
+            The filtered QuantamentalDataFrame. (if out_all=False, default)
+
+        Tuple[QuantamentalDataFrame, List[str], List[str]]
+            The filtered QuantamentalDataFrame, the filtered XCATs, and the filtered CIDs.
+            (if out_all=True)
         """
         result = reduce_df(
             df=self,
@@ -171,10 +241,25 @@ class QuantamentalDataFrame(QuantamentalDataFrameBase):
         start: Optional[str] = None,
         end: Optional[str] = None,
         blacklist: Mapping[str, Sequence[Union[str, pd.Timestamp]]] = None,
-        inplace: bool = False,
     ) -> "QuantamentalDataFrame":
         """
         Filter DataFrame by `ticker`, `start` & `end` dates.
+
+        Parameters
+        ----------
+        tickers : Sequence[str]
+            List of tickers to filter by
+        start : Optional[str], optional
+            Start date to filter by, by default None
+        end : Optional[str], optional
+            End date to filter by, by default None
+        blacklist : Mapping[str, Sequence[Union[str, pd.Timestamp]]], optional
+            Blacklist to apply to the DataFrame, by default None
+
+        Returns
+        -------
+        QuantamentalDataFrame
+            The filtered QuantamentalDataFrame.
         """
         result = reduce_df_by_ticker(
             df=self,
@@ -192,7 +277,6 @@ class QuantamentalDataFrame(QuantamentalDataFrameBase):
     def apply_blacklist(
         self,
         blacklist: Mapping[str, Sequence[Union[str, pd.Timestamp]]],
-        inplace: bool = False,
     ):
         """
         Apply a blacklist to the QuantamentalDataFrame.
@@ -208,10 +292,22 @@ class QuantamentalDataFrame(QuantamentalDataFrameBase):
         self,
         df_add: pd.DataFrame,
         xcat_replace: bool = False,
-        inplace: bool = False,
     ) -> "QuantamentalDataFrame":
         """
         Update the QuantamentalDataFrame with a new DataFrame.
+
+        Parameters
+        ----------
+        df_add : pd.DataFrame
+            DataFrame to update the QuantamentalDataFrame with
+        xcat_replace : bool, optional
+            If True, replace the XCATs in the QuantamentalDataFrame with the XCATs in
+            `df_add`, by default False
+
+        Returns
+        -------
+        QuantamentalDataFrame
+            The updated QuantamentalDataFrame.
         """
         result = update_df(df=self, df_add=df_add, xcat_replace=xcat_replace)
         return QuantamentalDataFrame(
@@ -225,10 +321,23 @@ class QuantamentalDataFrame(QuantamentalDataFrameBase):
         ticker: str,
         start: Optional[str] = None,
         end: Optional[str] = None,
-        inplace: bool = False,
     ) -> "QuantamentalDataFrame":
         """
         Add a NaN series to the QuantamentalDataFrame.
+
+        Parameters
+        ----------
+        ticker : str
+            Ticker to add the NaN series to
+        start : Optional[str], optional
+            Start date of the NaN series, by default None
+        end : Optional[str], optional
+            End date of the NaN series, by default None
+
+        Returns
+        -------
+        QuantamentalDataFrame
+            The QuantamentalDataFrame with the NaN series added.
         """
         result = add_nan_series(df=self, ticker=ticker, start=start, end=end)
         return QuantamentalDataFrame(
@@ -241,10 +350,21 @@ class QuantamentalDataFrame(QuantamentalDataFrameBase):
         self,
         column: str = "value",
         raise_warning: bool = True,
-        inplace: bool = False,
     ) -> "QuantamentalDataFrame":
         """
         Drop NaN series from the QuantamentalDataFrame.
+
+        Parameters
+        ----------
+        column : str, optional
+            Column to check for NaN series, by default "value"
+        raise_warning : bool, optional
+            If True, raise a warning if NaN series are dropped, by default True
+
+        Returns
+        -------
+        QuantamentalDataFrame
+            The QuantamentalDataFrame with NaN series dropped
         """
         result = drop_nan_series(df=self, column=column, raise_warning=raise_warning)
         return QuantamentalDataFrame(
@@ -261,10 +381,29 @@ class QuantamentalDataFrame(QuantamentalDataFrameBase):
         prefix: Optional[str] = None,
         name_all: Optional[str] = None,
         fmt_string: Optional[str] = None,
-        inplace: bool = False,
     ) -> "QuantamentalDataFrame":
         """
         Rename xcats in the QuantamentalDataFrame.
+
+        Parameters
+        ----------
+        xcat_map : Optional[Mapping[str, str]], optional
+            Mapping of xcats to rename, by default None
+        select_xcats : Optional[List[str]], optional
+            List of xcats to rename, by default None
+        postfix : Optional[str], optional
+            Postfix to add to the xcats, by default None
+        prefix : Optional[str], optional
+            Prefix to add to the xcats, by default None
+        name_all : Optional[str], optional
+            Name to rename all xcats to, by default None
+        fmt_string : Optional[str], optional
+            Format string to rename xcats, by default None
+
+        Returns
+        -------
+        QuantamentalDataFrame
+            The QuantamentalDataFrame with the xcats renamed.
         """
         result = rename_xcats(
             df=self,
@@ -287,6 +426,17 @@ class QuantamentalDataFrame(QuantamentalDataFrameBase):
     ) -> "QuantamentalDataFrame":
         """
         Pivot the QuantamentalDataFrame.
+
+        Parameters
+        ----------
+        value_column : str, optional
+            Column to pivot, by default "value"
+
+        Returns
+        -------
+        QuantamentalDataFrame
+            The pivoted QuantamentalDataFrame, with each ticker as a column with the
+            values of the `value_column` and the index as the `real_date`.
         """
         result = qdf_to_wide_df(self, value_column=value_column)
         if self.InitializedAsCategorical:
@@ -304,6 +454,20 @@ class QuantamentalDataFrame(QuantamentalDataFrameBase):
     ) -> "QuantamentalDataFrame":
         """
         Convert a timeseries DataFrame to a QuantamentalDataFrame.
+
+        Parameters
+        ----------
+        timeseries : pd.Series
+            Timeseries to convert to a QuantamentalDataFrame
+        ticker : str
+            Ticker to assign to the timeseries
+        metric : str, optional
+            Metric to assign to the timeseries, by default "value"
+
+        Returns
+        -------
+        QuantamentalDataFrame
+            The QuantamentalDataFrame created from the timeseries.
         """
         return QuantamentalDataFrame(
             qdf_from_timeseries(
@@ -326,8 +490,42 @@ class QuantamentalDataFrame(QuantamentalDataFrameBase):
     ) -> "QuantamentalDataFrame":
         """
         Convert a long DataFrame to a QuantamentalDataFrame. This is useful when the
-        DataFrame may contain only a CID or XCAT column, or in cases where the CID and
-        XCAT columns are not named as such.
+        DataFrame may contain only a `cid` or `xcat` column, or in cases where the `cid`
+        and `xcat` columns are not named as such.
+
+        Parameters
+        ----------
+        df : pd.DataFrame
+            Long DataFrame to convert to a QuantamentalDataFrame
+        real_date_column : str, optional
+            Column name of the real date, by default "real_date"
+        value_column : str, optional
+            Column name of the value, by default "value"
+        cid : Optional[str], optional
+            `cid` to assign to the DataFrame, by default None. If not specified, the `cid`
+            column must be present in the DataFrame.
+        xcat : Optional[str], optional
+            `xcat` to assign to the DataFrame, by default None
+        ticker : Optional[str], optional
+            Ticker to assign to the DataFrame, by default None
+        categorical : bool, optional
+            If True, convert the DataFrame to categorical, by default True
+
+        Raises
+        ------
+        ValueError
+            If the `real_date_column` or `value_column` are not found in the DataFrame,
+            or if `ticker` is specified with `cid` or `xcat`.
+        ValueError
+            If the input DataFrame is empty.
+        ValueError
+            If the `cid` or `xcat` columns are not found in the DataFrame, and have not
+            been specified in the function call.
+
+        Returns
+        -------
+        QuantamentalDataFrame
+            The QuantamentalDataFrame created from the long DataFrame.
         """
 
         if real_date_column not in df.columns:
@@ -383,6 +581,25 @@ class QuantamentalDataFrame(QuantamentalDataFrameBase):
     ) -> "QuantamentalDataFrame":
         """
         Concatenate a list of QuantamentalDataFrames into a single QuantamentalDataFrame.
+
+        Parameters
+        ----------
+        qdf_list : List[QuantamentalDataFrame]
+            List of QuantamentalDataFrames to concatenate
+        categorical : bool, optional
+            If True, convert the DataFrame to categorical, by default True
+
+        Raises
+        ------
+        TypeError
+            If any element in the list is not a QuantamentalDataFrame.
+        ValueError
+            If the input list is empty.
+
+        Returns
+        -------
+        QuantamentalDataFrame
+            The concatenated QuantamentalDataFrame.
         """
         if not all(isinstance(qdf, QuantamentalDataFrame) for qdf in qdf_list):
             raise TypeError("All elements in the list must be QuantamentalDataFrames.")
@@ -401,6 +618,29 @@ class QuantamentalDataFrame(QuantamentalDataFrameBase):
     ) -> "QuantamentalDataFrame":
         """
         Convert a wide DataFrame to a QuantamentalDataFrame.
+
+        Parameters
+        ----------
+        df : pd.DataFrame
+            Wide DataFrame to convert to a QuantamentalDataFrame
+        value_column : str, optional
+            Column to pivot, by default "value"
+        categorical : bool, optional
+            If True, convert the DataFrame to categorical, by default True
+
+        Raises
+        ------
+        TypeError
+            If `df` is not a pandas DataFrame.
+        ValueError
+            If `df` does not have a datetime index.
+        ValueError
+            If all columns are not in the format 'cid_xcat'.
+
+        Returns
+        -------
+        QuantamentalDataFrame
+            The QuantamentalDataFrame created from the wide DataFrame.
         """
 
         if not isinstance(df, pd.DataFrame):
@@ -439,12 +679,38 @@ class QuantamentalDataFrame(QuantamentalDataFrameBase):
         ticker: Optional[str] = None,
         metrics: List[str] = ["value"],
         date_range: Optional[pd.DatetimeIndex] = None,
-        start_date: Optional[str] = None,
-        end_date: Optional[str] = None,
+        start: Optional[str] = None,
+        end: Optional[str] = None,
         categorical: bool = True,
     ) -> "QuantamentalDataFrame":
         """
         Create an empty QuantamentalDataFrame.
+
+        Parameters
+        ----------
+        cid : Optional[str], optional
+            `cid` to assign to the DataFrame, by default None
+        xcat : Optional[str], optional
+            `xcat` to assign to the DataFrame, by default None
+        ticker : Optional[str], optional
+            Ticker to assign to the DataFrame, by default None. If specified, `cid` and
+            `xcat` must not be specified.
+        metrics : List[str], optional
+            Metrics to assign to the DataFrame, by default ["value"]
+        date_range : Optional[pd.DatetimeIndex], optional
+            Date range to assign to the DataFrame, by default None. If not specified,
+            `start` and `end` must be specified.
+        start : Optional[str], optional
+            Start date to assign to the DataFrame, by default None
+        end : Optional[str], optional
+            End date to assign to the DataFrame, by default None
+        categorical : bool, optional
+            If True, convert the DataFrame to categorical, by default True
+
+        Returns
+        -------
+        QuantamentalDataFrame
+            The empty QuantamentalDataFrame.
         """
         qdf = create_empty_categorical_qdf(
             cid=cid,
@@ -452,7 +718,7 @@ class QuantamentalDataFrame(QuantamentalDataFrameBase):
             ticker=ticker,
             metrics=metrics,
             date_range=date_range,
-            start=start_date,
-            end=end_date,
+            start=start,
+            end=end,
         )
         return QuantamentalDataFrame(qdf, categorical=categorical)
