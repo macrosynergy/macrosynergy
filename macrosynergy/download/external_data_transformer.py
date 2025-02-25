ae865ec3b6b75a22564e9e2c7b41a6581b275965
Samuel Andresen <sandresen@macrosynergy.com>
2025-02-25 12:37:51 +0000

    Add JSON Transformer

--- macrosynergy/download/external_data_transformer.py
+++ macrosynergy/download/external_data_transformer.py
@@ -9,8 +9,20 @@ class BaseTransformer(ABC):
     def transform(self, data, **kwargs):
         pass
 
+    @staticmethod
+    def get_mapping_value(d, keys):
+        if not isinstance(keys, list):
+            return d[keys]
+
+        for key in keys:
+            d = d[key]
+        return d
+
 
 class DataFrameTransformer(BaseTransformer):
+    """
+    This transformer is used to transform a pandas DataFrame into a QuantamentalDataFrame.
+    """
     def transform(self, data, mapping):
         if data.empty:
             raise ValueError("DataFrame is empty.")
@@ -47,11 +59,35 @@ class DataFrameTransformer(BaseTransformer):
                 " of indexes/multiindexes."
             )
 
+class JSONTransformer(BaseTransformer):
+    """
+    This transformer is used to transform a list of JSON objects into a QuantamentalDataFrame.
 
+    Any JSON object is assumed to be a dictionary in python.
+    """
+    def transform(self, data, mapping):
+
+        rows = []
+
+        for json in data:
+            cid = BaseTransformer.get_mapping_value(json, mapping["cid"])
+            xcat = BaseTransformer.get_mapping_value(json, mapping["xcat"])
+            time_series = BaseTransformer.get_mapping_value(json, mapping["time-series"])
+            for date, value in time_series:
+                rows.append({"real_date": date, "cid": cid, "xcat": xcat, "value": value})
+
+        df = pd.DataFrame(rows)
+        df["real_date"] = pd.to_datetime(df["real_date"])
+
+        return QuantamentalDataFrame(df)
 
 def transform_to_qdf(data, **kwargs):
     if isinstance(data, pd.DataFrame):
         return DataFrameTransformer().transform(
+            data, mapping=kwargs.get("mapping", {})
+        )
+    elif isinstance(data, list) and all(isinstance(x, dict) for x in data):
+        return JSONTransformer().transform(
             data, mapping=kwargs.get("mapping", {})
         )
     else:
@@ -75,5 +111,34 @@ if __name__ == "__main__":
     new_df = transform_to_qdf(
         df, mapping={"a": "value", "b": "cid", "c": "xcat", "date": "real_date"}
     )
+
+    print(new_df)
+
+    import os
+    from macrosynergy.download import DataQueryInterface
+
+    client_id: str = os.getenv("DQ_CLIENT_ID")
+    client_secret: str = os.getenv("DQ_CLIENT_SECRET")
+
+    expressions = ["DB(CFX,GBP,)", "DB(CFX,USD,)"]
+
+    with DataQueryInterface(
+        client_id=client_id,
+        client_secret=client_secret,
+    ) as dq:
+        assert dq.check_connection(verbose=True)
+
+        data = dq.download_data(
+            expressions=expressions,
+            start_date="2024-01-25",
+            end_date="2024-02-05",
+            show_progress=True,
+        )
+
+    new_df = transform_to_qdf(data, mapping = {
+        "cid": ["attributes", 0, "expression"],
+        "xcat": ["attributes", 0, "expression"],
+        "time-series": ["attributes", 0, "time-series"]
+    })
 
     print(new_df)
