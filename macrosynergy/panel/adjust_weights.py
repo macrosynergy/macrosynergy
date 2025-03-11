1ac1996dcab57682d329bff34837543ecad5be7f
Palash Tyagi <ptyagi@macrosynergy.com>
2025-03-11 09:35:19 +0000

    add docstrings for adjust_weights backend functions

--- macrosynergy/panel/adjust_weights.py
+++ macrosynergy/panel/adjust_weights.py
@@ -26,6 +26,9 @@ PD_FUTURE_STACK = (
 
 
 def check_missing_cids_xcats(weights, adj_zns, cids, r_xcats, r_cids):
+    """
+    Checks if there are missing cids or xcats in the input DataFrame.
+    """
     missing_xcats = list(set([weights, adj_zns]) - set(r_xcats))
     if missing_xcats:
         raise ValueError(f"Missing xcats: {missing_xcats}")
@@ -38,6 +41,9 @@ def check_missing_cids_xcats(weights, adj_zns, cids, r_xcats, r_cids):
 def check_types(
     weights: str, adj_zns: str, method: Callable, param: Number, cids: List[str]
 ):
+    """
+    Type checking for the input variables of adjust_weights.
+    """
     for _var, _name, _type in [
         (weights, "weights", str),
         (adj_zns, "adj_zns", str),
@@ -57,7 +63,30 @@ def adjust_weights_backend(
     df_adj_zns_wide: pd.DataFrame,
     method: Callable,
     param: Number,
-) -> QuantamentalDataFrame:
+) -> pd.DataFrame:
+    """
+    Backend function for adjust_weights. Applies the `method` function to the weights and
+    multiplies the result by the adjustment factors, and by the parameter `param`.
+
+    Parameters
+    ----------
+    df_weights_wide : pd.DataFrame
+        DataFrame with weights in wide format.
+
+    df_adj_zns_wide : pd.DataFrame
+        DataFrame with adjustment factors in wide format.
+
+    method : Callable
+        Function that will be applied to the weights to adjust them. This function must
+
+    param : Number
+        Parameter that will be passed to the method function.
+
+    Returns
+    -------
+    pd.DataFrame
+        DataFrame with the adjusted weights.
+    """
 
     assert set(df_weights_wide.columns) == set(df_adj_zns_wide.columns)
     cids = sorted(df_weights_wide.columns)
@@ -77,6 +106,28 @@ def adjust_weights_backend(
 def split_weights_adj_zns(
     df: QuantamentalDataFrame, weights: str, adj_zns: str
 ) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """
+    Splits the input DataFrame into two DataFrames, one containing the weights and the
+    other containing the adjustment factors.
+
+    Parameters
+    ----------
+    df : QuantamentalDataFrame
+        DataFrame containing the weights and adjustment factors.
+
+    weights : str
+        Name of the xcat to be used as weights.
+
+    adj_zns : str
+        Name of the xcat to be used as adjustment factors.
+
+    Returns
+    -------
+    Tuple[pd.DataFrame, pd.DataFrame]
+        Tuple containing two wide DataFrames (one for weights and one for adjustment
+        factors), with one column per cid.
+    """
+
     df_weights = df[df["xcat"] == weights]
     df_adj_zns = df[df["xcat"] == adj_zns]
 
@@ -95,6 +146,20 @@ def split_weights_adj_zns(
 
 
 def normalize_weights(df_weights_wide: pd.DataFrame) -> pd.DataFrame:
+    """
+    Weights are normalized by dividing each row by the sum of the row. Function exists to
+    allow easy modification of normalization method.
+
+    Parameters
+    ----------
+    df_weights_wide : pd.DataFrame
+        DataFrame with weights in wide format. (one column per cid)
+
+    Returns
+    -------
+    pd.DataFrame
+        DataFrame with normalized weights (sum of each row is 1).
+    """
     df_weights_wide = df_weights_wide.div(df_weights_wide.sum(axis=1), axis=0)
     return df_weights_wide
 
