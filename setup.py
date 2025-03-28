7e0c503de9eb7d1c7f63489170e00c17148fbe1b
Palash Tyagi <ptyagi@macrosynergy.com>
2025-03-28 14:34:49 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if sys.version_info[:2] < (3, 7):
 MAJOR = 1
 MINOR = 1
 MICRO = 4
-ISRELEASED = False
+ISRELEASED = True
 VERSION = "%d.%d.%d" % (MAJOR, MINOR, MICRO)
 
 if sys.version_info >= (3, 13):
