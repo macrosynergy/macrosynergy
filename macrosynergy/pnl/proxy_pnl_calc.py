83c2aa3d23253bc039a46f09099681c4908575d6
Palash Tyagi <ptyagi@macrosynergy.com>
2025-04-03 15:23:56 +0100

    Enhance error message in _split_returns_positions_tickers function to include sorted missing tickers for better clarity

--- macrosynergy/pnl/proxy_pnl_calc.py
+++ macrosynergy/pnl/proxy_pnl_calc.py
@@ -39,10 +39,15 @@ def _split_returns_positions_tickers(
     returns_wo_positions = set_returns - set_positions
     if (len(positions_wo_returns) + len(returns_wo_positions)) > 0:
         err_msg = "The following tickers are missing in the dataframe: \n"
+        positions_wo_returns = sorted(positions_wo_returns)
+        positions_wo_returns = list(map(lambda x: x + f"_{spos}", positions_wo_returns))
+        returns_wo_positions = sorted(returns_wo_positions)
+        returns_wo_positions = list(map(lambda x: x + rstring, returns_wo_positions))
+
         if positions_wo_returns:
-            err_msg += f"Positions without returns: {sorted(positions_wo_returns)} \n"
+            err_msg += f"Positions without returns: {positions_wo_returns} \n"
         if returns_wo_positions:
-            err_msg += f"Returns without positions: {sorted(returns_wo_positions)} \n"
+            err_msg += f"Returns without positions: {returns_wo_positions} \n"
         err_msg += "Please check the tickers in the dataframe."
         raise ValueError(err_msg)
 
