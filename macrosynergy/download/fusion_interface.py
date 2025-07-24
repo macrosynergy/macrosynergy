7a7c847c77df851adfb5c9bf9169fd8cf8bb43ab
Palash Tyagi <ptyagi@macrosynergy.com>
2025-07-24 16:12:51 +0100

    updated example in script

--- macrosynergy/download/fusion_interface.py
+++ macrosynergy/download/fusion_interface.py
@@ -1929,29 +1929,34 @@ if __name__ == "__main__":
         "data/fusion_client_credentials.json"
     )
     jpmaqs_client = JPMaQSFusionClient(oauth_handler=oauth_handler)
-
     st = time.time()
 
     df = jpmaqs_client.download(
-        folder="./data",
-        tickers=["USD_EQXR_NSA", "GBP_EQXR_NSA"],
+        # folder="./data",
         cids=["USD", "GBP", "EUR", "JPY", "CHF", "AUD", "CAD"],
         xcats=["FXXR_NSA", "EQXR_NSA", "EQCRY_NSA"],
+        tickers=["USD_EQXR_NSA", "GBP_EQXR_NSA"],
         start_date="2025-07-17",
     )
+    print(df.head())
     print(f"Time taken for download: {time.time() - st:.2f} seconds")
     df = None
     ds = jpmaqs_client.list_datasets()
 
     st = time.time()
     jpmaqs_client.download_latest_full_snapshot(
-        folder="./data", qdf=True, keep_raw_data=False
+        folder="./data",
+        keep_raw_data=False,
+        qdf=True,
+        as_csv=True,
     )
     print(f"Time taken for full snapshots download: {time.time() - st:.2f} seconds")
 
     st = time.time()
 
     jpmaqs_client.download_latest_delta_distribution(
-        folder="./data", qdf=True, keep_raw_data=False
+        folder="./data",
+        qdf=True,
+        keep_raw_data=False,
     )
     print(f"Time taken for latest delta download: {time.time() - st:.2f} seconds")
