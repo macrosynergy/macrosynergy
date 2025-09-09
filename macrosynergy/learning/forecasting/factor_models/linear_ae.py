110264ee515dc6004718668e467abbc9205f5ec6
Rushil Gholkar <67958144+rushilg99@users.noreply.github.com>
2025-09-09 17:21:10 +0100

    example usage

--- macrosynergy/learning/forecasting/factor_models/linear_ae.py
+++ macrosynergy/learning/forecasting/factor_models/linear_ae.py
@@ -110,3 +110,62 @@ class LinearFactorLearning(TorchEmbedding):
         )
 
         return X_embeddings_pandas
+    
+if __name__ == "__main__":
+    import pandas as pd
+    import numpy as np
+
+    from macrosynergy.management.simulate import make_qdf
+
+    cids = ["AUD", "CAD", "GBP", "USD"]
+    xcats = ["XR", "CRY", "GROWTH", "INFL"]
+    cols = ["earliest", "latest", "mean_add", "sd_mult", "ar_coef", "back_coef"]
+
+    df_cids = pd.DataFrame(
+        index=cids, columns=["earliest", "latest", "mean_add", "sd_mult"]
+    )
+    df_cids.loc["AUD"] = ["2012-01-01", "2020-12-31", 0, 1]
+    df_cids.loc["CAD"] = ["2012-01-01", "2020-12-31", 0, 1]
+    df_cids.loc["GBP"] = ["2012-01-01", "2020-12-31", 0, 1]
+    df_cids.loc["USD"] = ["2012-01-01", "2020-12-31", 0, 1]
+
+    df_xcats = pd.DataFrame(index=xcats, columns=cols)
+    df_xcats.loc["XR"] = ["2012-01-01", "2020-12-31", 0.1, 1, 0, 0.3]
+    df_xcats.loc["CRY"] = ["2012-01-01", "2020-12-31", 1, 2, 0.95, 1]
+    df_xcats.loc["GROWTH"] = ["2012-01-01", "2020-12-31", 1, 2, 0.9, 1]
+    df_xcats.loc["INFL"] = ["2015-01-01", "2020-12-31", -0.1, 2, 0.8, 0.3]
+
+    dfd = make_qdf(df_cids, df_xcats, back_ar=0.75)
+    dfd["grading"] = np.ones(dfd.shape[0])
+    black = {
+        "GBP": (
+            pd.Timestamp(year=2009, month=1, day=1),
+            pd.Timestamp(year=2012, month=6, day=30),
+        ),
+        "CAD": (
+            pd.Timestamp(year=2015, month=1, day=1),
+            pd.Timestamp(year=2016, month=1, day=1),
+        ),
+    }
+
+    from macrosynergy.learning import SignalOptimizer
+    so = SignalOptimizer(
+        df=dfd,
+        xcats=["CRY", "GROWTH", "INFL", "XR"],
+        cids=cids,
+        blacklist=black,
+        drop_nas = True
+    )
+
+    X = so.X.copy()
+    y = so.y.copy()
+
+    model = LinearFactorLearning(
+        hidden_dim=2,
+        lr = 1e-3,
+        decoder_sparsity=10,
+        verbose = 1
+    ).fit(X, y)
+    # Print embeddings
+    embeddings = model.transform(X)
+    print(embeddings)
