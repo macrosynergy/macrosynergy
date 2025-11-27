a4571c408cc065d7f1453fca8fb5f17d8a438ec7
Rushil Gholkar <67958144+rushilg99@users.noreply.github.com>
2025-11-27 19:07:19 +0000

    SUR with checks and documentation

--- macrosynergy/learning/forecasting/linear_model/sur.py
+++ macrosynergy/learning/forecasting/linear_model/sur.py
@@ -3,17 +3,76 @@ import pandas as pd
 
 from sklearn.linear_model import LinearRegression
 from sklearn.base import BaseEstimator, RegressorMixin
+from sklearn.feature_selection import SelectorMixin
 
 from scipy.optimize import minimize
 
 class LinearMultiTargetRegression(BaseEstimator, RegressorMixin):
-    def __init__(self, feature_selection=None, fit_intercept=True, seemingly_unrelated=False):
-        self.feature_selection = feature_selection
+    """
+    Linear regression model with multiple targets, supporting seemingly unrelated
+    regression (SUR) via feasible generalized least squares (FGLS).
+
+    Parameters
+    ----------
+    fit_intercept : bool, default=True
+        Whether to include an intercept term in the regression.
+    seemingly_unrelated : bool, default=False
+        Whether to make the regression seemingly unrelated.
+    span : int, default=60
+        Span parameter for exponentially weighted covariance estimation of residuals.
+    feature_selection : object, default=None
+        A feature selection object inheriting from scikit-learn's `SelectorMixin` base
+        class in `sklearn.feature_selection`.
+        If provided, feature selection is applied per target before fitting. 
+    """
+    def __init__(
+        self,
+        fit_intercept=True,
+        seemingly_unrelated=False,
+        span=60,
+        feature_selection=None,
+    ):
+        # Checks
+        if not isinstance(fit_intercept, bool):
+            raise TypeError("The 'fit_intercept' parameter must be a boolean.")
+        if not isinstance(seemingly_unrelated, bool):
+            raise TypeError("The 'seemingly_unrelated' parameter must be a boolean.")
+        if not isinstance(span, int) or span <= 0:
+            raise ValueError("The 'span' parameter must be a positive integer.")
+        if feature_selection is not None and not isinstance(
+            feature_selection, SelectorMixin
+        ):
+            raise TypeError(
+                "The 'feature_selection' parameter must be a SelectorMixin instance."
+            )
+        
+        # Attributes
         self.fit_intercept = fit_intercept
         self.seemingly_unrelated = seemingly_unrelated
+        self.span = span
+        self.feature_selection = feature_selection
 
-    def fit(self, X, y):
+    def fit(self, X, y, sample_weight=None):
+        """
+        Fit the linear multi-target regression model.
+
+        Parameters
+        ----------
+        X : pd.DataFrame
+            Feature matrix of shape (n_samples, n_features). Should be multi-indexed by 
+            asset and real date.
+        y : pd.DataFrame
+            Target matrix of shape (n_samples, n_assets). Should be multi-indexed by 
+            asset and real date.
+        sample_weight : array-like of shape (n_samples,), default=None
+            Individual weights for each sample.
+        """
+        # Checks
+        self._check_fit_params(X, y, sample_weight)
+        
         # For now, only support pandas dataframes with no missing values
+        if isinstance(y, pd.Series):
+            y = y.to_frame()
         assert isinstance(y, pd.DataFrame)
         assert y.isna().sum().sum() == 0
 
@@ -26,27 +85,30 @@ class LinearMultiTargetRegression(BaseEstimator, RegressorMixin):
 
         self.assets = list(y.columns)
         self.n_assets = len(self.assets)
-        self.n_periods = y.shape[0] # TODO: Think about this more carefully for the instance where we operate on panel data
+        self.n_samples = y.shape[
+            0
+        ] 
         self.features_ = list(X.columns)
         self.n_features = X.shape[1]
 
         # Store "initial" coefficients for FGLS
         # When seemingly_unrelated = False, these are final OLS coefficients
-        # Separate logic when feature selection is applied 
+        # Separate logic when feature selection is applied
         self.X_features = {}
         self.initial_coefs = {}
 
         if self.feature_selection is not None:
+            col_index = {col: i for i, col in enumerate(X.columns)} # includes an intercept if fit_intercept=True
             for asset in self.assets:
                 # Store selected features indices per asset
                 if self.fit_intercept:
-                    selector = self.feature_selection.fit(X.iloc[:,1:], y[asset])
+                    selector = self.feature_selection.fit(X.iloc[:, 1:], y[asset])
                     feats = selector.get_feature_names_out()
-                    cols = [0] + [X.columns.get_loc(f) for f in feats]
+                    cols = [0] + [col_index[f] for f in feats]
                 else:
                     selector = self.feature_selection.fit(X, y[asset])
                     feats = selector.get_feature_names_out()
-                    cols = [X.columns.get_loc(f) for f in feats]
+                    cols = [col_index[f] for f in feats]
                 self.X_features[asset] = cols
 
                 # Fit OLS on selected features and store them
@@ -60,11 +122,13 @@ class LinearMultiTargetRegression(BaseEstimator, RegressorMixin):
 
             # Fit OLS jointly and store coefficients
             lr = LinearRegression(fit_intercept=False).fit(X, y)
-            W = lr.coef_.T     # shape (n_features × n_assets)
-            self.initial_coefs = {asset: W[:, idx] for idx, asset in enumerate(self.assets)}
+            W = lr.coef_.T  # shape (n_features × n_assets)
+            self.initial_coefs = {
+                asset: W[:, idx] for idx, asset in enumerate(self.assets)
+            }
 
-        # If not SUR, job is done here
-        # Just store OLS coefficients 
+        # If not SUR_SELECT, job is done here
+        # Just store OLS coefficients
         if not self.seemingly_unrelated:
             self.coefs_ = {}
             self.intercepts_ = {}
@@ -80,126 +144,244 @@ class LinearMultiTargetRegression(BaseEstimator, RegressorMixin):
 
             return self
 
-        # If SUR, first calculate covariance of residuals
-        resids = np.column_stack([
-            y[asset].to_numpy() - X.iloc[:, self.X_features[asset]].to_numpy() @ self.initial_coefs[asset]
-            for asset in self.assets
-        ])
-        # TODO: replace with graphical lasso 
-        cov = np.cov(resids, rowvar=False)
+        # If SUR_SELECT, first calculate covariance of residuals
+        resids = pd.DataFrame(
+            data=np.column_stack(
+                [
+                    y[asset].to_numpy()
+                    - X.iloc[:, self.X_features[asset]].to_numpy()
+                    @ self.initial_coefs[asset]
+                    for asset in self.assets
+                ]
+            ),
+            index=y.index,
+            columns=y.columns,
+        )
+
+        # Calculate time decay weights for EWM covariance
+        weights = np.array(
+            [(1 - 2 / (self.span + 1)) ** i for i in range(len(y))][::-1]
+        )
+        cov = np.cov(resids.values.T, aweights=weights)
+        
+        # Invert matrix 
+        # TODO: apply graphical lasso to cov when # of assets gets big
         invcov = np.linalg.inv(cov)
 
-        """
-        Set up FGLS optimization. 
-        This is tricky with feature selection, as each asset has different features. 
-        My logic is to create a full coefficient matrix W of shape (n_features × n_assets),
-        but pack and unpack the matrix only for the selected coefficients.
-        We don't want the optimizer to update unselected coefficients, so I create a mask
-        to indicate which coefficients are real parameters.
-        """
-        # Initial full W matrix
+        # FGLS optimization
+        # Due to feature selection, create a full matrix but pack/unpack the matrix 
+        # only to update trainable coefficients. 
         W_full = np.zeros((self.n_features, self.n_assets))
         for idx, asset in enumerate(self.assets):
-            W_full[self.X_features[asset], idx] = self.initial_coefs[asset]
+            W_full[self.X_features[asset], idx] = self.initial_coefs[asset] # Initialize with OLS coefficients
 
         # Mask
-        mask = np.zeros_like(W_full, dtype=bool)
+        mask = np.zeros_like(W_full, dtype=bool) # matrix of Falses
         for idx, asset in enumerate(self.assets):
             mask[self.X_features[asset], idx] = True
 
         # Flatten W for scipy optimize
         x0 = W_full[mask]
 
-        # Minimise SUR loss over the panel
+        # Minimise SUR_SELECT loss over the panel
+        if sample_weight is not None:
+            sample_weight = np.asarray(sample_weight).reshape(-1)
+
         self.coefs_, self.intercepts_ = self.__minimise_sur_loss(
-            X = X.to_numpy(),
-            y = y.to_numpy(),
-            invcov = invcov,
-            x0 = x0,
-            mask = mask
+            X=X.to_numpy(),
+            y=y.to_numpy(),
+            invcov=invcov,
+            x0=x0,
+            mask=mask,
+            sample_weight=sample_weight,
         )
 
         return self
 
-    def __minimise_sur_loss(self, X, y, invcov, x0, mask):
-        # Run LBFGS 
+    def __minimise_sur_loss(self, X, y, invcov, x0, mask, sample_weight):
+        """
+        Fit the SUR model via minimization of the SUR loss function. Coefficients
+        and intercepts are extracted after optimization.
+
+        Parameters
+        ----------
+        X : np.ndarray
+            Feature matrix of shape (n_samples, n_features).
+        y : np.ndarray
+            Target matrix of shape (n_samples, n_assets).
+        invcov : np.ndarray
+            Inverse of the covariance matrix of residuals.
+        x0 : np.ndarray
+            Initial coefficients for optimization.
+        mask : np.ndarray
+            Boolean mask of shape (n_features, n_assets) indicating which coefficients
+            are trainable.
+        sample_weight : np.ndarray or None
+            Optional sample weights for the loss function.
+        """
+        # Run LBFGS
         res = minimize(
-            fun = lambda params: self.__sur_loss_and_grad(params, X, y, invcov, mask, return_grad = False),
-            x0 = x0,
-            jac = lambda params: self.__sur_loss_and_grad(params, X, y, invcov, mask, return_grad = True),
+            fun=lambda params: self.__sur_loss_and_grad(
+                params, X, y, invcov, mask, sample_weight, return_grad=False
+            ),
+            x0=x0,
+            jac=lambda params: self.__sur_loss_and_grad(
+                params, X, y, invcov, mask, sample_weight, return_grad=True
+            ),
             method="L-BFGS-B",
         )
 
-        # Reshape into full matrix 
+        # Reshape into full matrix
         W = np.zeros((self.n_features, self.n_assets))
         W[mask] = res.x
 
         # Extract intercepts and coefs
+        # Convert to asset specific intercepts and coefficients
         if self.fit_intercept:
-            # Convert to asset specific intercepts and coefficients
             coefs_ = {
-                asset : W[self.X_features[asset][1:], idx]
+                asset: W[self.X_features[asset][1:], idx]
                 for idx, asset in enumerate(self.assets)
             }
-            intercepts_ = {
-                asset : W[0, idx]
-                for idx, asset in enumerate(self.assets)
-            }
+            intercepts_ = {asset: W[0, idx] for idx, asset in enumerate(self.assets)}
         else:
-            # Convert to asset specific intercepts and coefficients
             coefs_ = {
-                asset : W[self.X_features[asset], idx]
+                asset: W[self.X_features[asset], idx]
                 for idx, asset in enumerate(self.assets)
             }
-            intercepts_ = {
-                asset : 0
-                for asset in self.assets
-            }
+            intercepts_ = {asset: 0 for asset in self.assets}
 
         return coefs_, intercepts_
-    
-    def __sur_loss_and_grad(self, params, X, y, invcov, mask, return_grad):
+
+    def __sur_loss_and_grad(
+        self, params, X, y, invcov, mask, sample_weight, return_grad
+    ):
+        """
+        SUR loss and derivative evaluation.
+
+        Parameters
+        ----------
+        params : np.ndarray
+            Flattened array of trainable coefficients.
+        X : np.ndarray
+            Feature matrix of shape (n_samples, n_features).
+        y : np.ndarray
+            Target matrix of shape (n_samples, n_assets).
+        invcov : np.ndarray
+            Inverse of the covariance matrix of residuals.
+        mask : np.ndarray
+            Boolean mask of shape (n_features, n_assets) indicating which coefficients
+            are trainable.
+        sample_weight : np.ndarray or None
+            Optional sample weights for the loss function.
+        return_grad : bool
+            Whether to return the gradient instead of the loss.
+        """
         # First unpack coefficients
         W = np.zeros((self.n_features, self.n_assets))
         W[mask] = params
-        
+
         # Residuals
-        resids = y - X @ W 
+        resids = y - X @ W
 
         if not return_grad:
-            loss = np.einsum("ti,ij,tj->", resids, invcov, resids) / self.n_periods
+            if sample_weight is None:
+                loss = np.einsum("ti,ij,tj->", resids, invcov, resids) / self.n_samples
+            else:
+                loss = (
+                    np.einsum("ti,ij,tj,t->", resids, invcov, resids, sample_weight)
+                    / self.n_samples
+                )
+
             return loss
         else:
-            grad_full = -2 * X.T @ (resids @ invcov) / self.n_periods
+            wresids = resids @ invcov
+            if sample_weight is not None:
+                wresids = wresids * sample_weight[:, None]
+
+            grad_full = -2 * (X.T @ wresids) / self.n_samples
             grad = grad_full[mask]
+
             return grad
 
     def predict(self, X):
+        """
+        Predict method to return predictions for each asset.
+
+        Parameters
+        ----------
+        X : pd.DataFrame
+            Feature matrix of shape (n_samples, n_features). Should be multi-indexed by 
+            asset and real date.
+        """
+        # Checks
+        self._check_predict_params(X)
+
+        # Add intercept and set up predictions dataframe
         X = X.copy()
         if self.fit_intercept:
             X.insert(0, "intercept", 1)
 
         preds = pd.DataFrame(index=X.index, columns=self.assets)
 
+        # Evaluate for each asset with asset specific coefficients.
         for asset in self.assets:
             coefs = self.coefs_[asset]
             if self.fit_intercept:
                 intercept = self.intercepts_[asset]
-                preds[asset] = X.iloc[:, self.X_features[asset][1:]].to_numpy() @ coefs + intercept
+                preds[asset] = (
+                    X.iloc[:, self.X_features[asset][1:]].to_numpy() @ coefs + intercept
+                )
             else:
                 preds[asset] = X.iloc[:, self.X_features[asset]].to_numpy() @ coefs
 
         return preds.values
     
+    def _check_fit_params(self, X, y, sample_weight):
+        # Type checks for X and y
+        if not isinstance(X, pd.DataFrame):
+            raise TypeError("X must be a pandas DataFrame.")
+        if not isinstance(y, (pd.DataFrame, pd.Series)):
+            raise TypeError("y must be a pandas DataFrame or Series.")
+        # This model can't handle NAs.
+        if X.isna().sum().sum() > 0:
+            raise ValueError("X must not contain missing values.")
+        if y.isna().sum().sum() > 0:
+            raise ValueError("y must not contain missing values.")
+        # Ensure shapes align
+        if X.shape[0] != y.shape[0]:
+            raise ValueError("X and y must have the same number of samples.")
+        # If sample weight is set, make sure its shape and type are correct
+        if sample_weight is not None:
+            if not isinstance(sample_weight, (np.ndarray, list)):
+                raise TypeError("sample_weight must be a numpy array or list.")
+            if len(sample_weight) != X.shape[0]:
+                raise ValueError("sample_weight must have the same number of samples as X and y.")
+
+    def _check_predict_params(self, X):
+        # Type checks for X
+        if not isinstance(X, pd.DataFrame):
+            raise TypeError("X must be a pandas DataFrame.")
+        # This model can't handle NAs.
+        if X.isna().sum().sum() > 0:
+            raise ValueError("X must not contain missing values.")
+        # Ensure the features align with those seen in training
+        if self.fit_intercept:
+            if X.shape[1] != self.n_features - 1:
+                raise ValueError("X must have the same number of features as during training.")
+            if list(X.columns) != list(self.features_[1:]):
+                raise ValueError("X must have the same feature columns as during training.")
+        else:
+            if X.shape[1] != self.n_features:
+                raise ValueError("X must have the same number of features as during training.")
+            if list(X.columns) != list(self.features_):
+                raise ValueError("X must have the same feature columns as during training.")
+
 if __name__ == "__main__":
-    from sklearn.linear_model import LinearRegression
-    from sklearn.ensemble import RandomForestRegressor
-    from sklearn.metrics import make_scorer, r2_score, mean_absolute_error
     from macrosynergy.learning import (
-        ExpandingKFoldPanelSplit, SignalOptimizer, LarsSelector
+        SignalOptimizer,
+        LarsSelector,
     )
     from macrosynergy.management.simulate import make_qdf
-    from macrosynergy.management.types import QuantamentalDataFrame
 
     cids = ["AUD", "CAD", "GBP", "USD"]
     xcats = ["XR1", "CRY", "GROWTH", "RATES", "XR2"]
@@ -238,12 +420,17 @@ if __name__ == "__main__":
         xcats=["CRY", "GROWTH", "RATES", "XR1", "XR2"],
         cids=cids,
         blacklist=black,
-        drop_nas = True,
+        drop_nas=True,
         n_targets=2,
     )
     X = so.X.copy(deep=True)
     y = so.y.copy(deep=True)
 
-    model = LinearMultiTargetRegression(seemingly_unrelated=True, fit_intercept = True, feature_selection=LarsSelector(n_factors = 2))
+    model = LinearMultiTargetRegression(
+        seemingly_unrelated=True,
+        fit_intercept=False,
+        feature_selection=LarsSelector(n_factors=2),
+    )
     model.fit(X, y)
+
     print(model.predict(X))
