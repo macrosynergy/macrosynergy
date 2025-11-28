0217abb2a304ab15807eac4919af413e0e848a7b
Rushil Gholkar <67958144+rushilg99@users.noreply.github.com>
2025-11-28 12:39:51 +0000

    sharpe ratio

--- macrosynergy/learning/model_evaluation/scorers/__init__.py
+++ macrosynergy/learning/model_evaluation/scorers/__init__.py
@@ -1,9 +1,11 @@
 from .scorers import (
+    multi_output_sharpe,
     multi_output_sortino,
     neg_mean_abs_corr,
 )
 
 __all__ = [
+    "multi_output_sharpe",
     "multi_output_sortino",
     "neg_mean_abs_corr",
 ]
