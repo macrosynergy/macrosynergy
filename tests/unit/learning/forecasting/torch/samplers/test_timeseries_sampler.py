7a6d2d0aab43ee5a7428e1d46612074ae00f9960
rushilg99 <rushil1.gholkar@gmail.com>
2026-01-15 15:31:29 +0000

    fixed test for correct sampler length

--- tests/unit/learning/forecasting/torch/samplers/test_timeseries_sampler.py
+++ tests/unit/learning/forecasting/torch/samplers/test_timeseries_sampler.py
@@ -100,6 +100,7 @@ class TestTimeSeriesSampler(unittest.TestCase):
     @parameterized.expand(itertools.product([True, False], [True, False], [True, False]))
     def test_valid_init(self, shuffle, aggregate_last, drop_last):
         """ Test that valid initialization works """
+        # Try with a batch size that divides dataset size evenly
         if not (aggregate_last and drop_last):
             # Check instantiation works
             try:
@@ -129,7 +130,40 @@ class TestTimeSeriesSampler(unittest.TestCase):
                 expected_num_batches = len(self.valid_dataset) // 4
             else:
                 expected_num_batches = len(self.valid_dataset) // 4 # 64 / 4 is a whole number
-                
+
+            self.assertEqual(num_batches, expected_num_batches)
+
+        # Try with a batch size that doesn't divides the dataset size evenly
+        if not (aggregate_last and drop_last):
+            # Check instantiation works
+            try:
+                sampler = TimeSeriesSampler(
+                    dataset = self.valid_dataset,
+                    batch_size = 3,
+                    shuffle = shuffle,
+                    aggregate_last = aggregate_last,
+                    drop_last = drop_last,
+                )
+            except Exception as e:
+                self.fail(f"TimeSeriesSampler with parameters shuffle = {shuffle}, aggregate_last = {aggregate_last}, drop_last = {drop_last} raised an exception unexpectedly: {e}")
+            
+            # Check attributes set correctly
+            self.assertIsInstance(sampler, TimeSeriesSampler)
+            self.assertIsInstance(sampler, torch.utils.data.Sampler)
+            self.assertEqual(sampler.dataset, self.valid_dataset)
+            self.assertEqual(sampler.batch_size, 3)
+            self.assertEqual(sampler.shuffle, shuffle)
+            self.assertEqual(sampler.aggregate_last, aggregate_last)
+            self.assertEqual(sampler.drop_last, drop_last)
+            self.assertEqual(sampler.dataset_size, len(self.valid_dataset))
+
+            # Check length of the sampler is correct
+            num_batches = len(sampler.batches)
+            if drop_last or aggregate_last:
+                expected_num_batches = len(self.valid_dataset) // 3
+            else:
+                expected_num_batches = len(self.valid_dataset) // 3 + 1 # 64 / 4 is a whole number
+
             self.assertEqual(num_batches, expected_num_batches)
 
     
