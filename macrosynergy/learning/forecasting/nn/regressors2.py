ca8555254a36d804bf8739eb555906f183f29d9b
emhbrine <ebrine@macrosynergy.com>
2026-01-15 15:53:00 +0000

    MLPTrainer initial

---
+++ macrosynergy/learning/forecasting/nn/regressors2.py
@@ -0,0 +1,89 @@
+from sklearn.preprocessing import StandardScaler
+
+
+from sklearn.preprocessing import StandardScaler
+from copy import deepcopy
+
+
+class MultiHeadReturnRegressor(BaseEstimator, RegressorMixin):
+    def __init__(
+        self,
+        # Neural network structure hyperparameters
+        n_latent,
+        # Neural network training hyperparameters
+        weight_decay=1e-4,
+        reg_turnover=0,
+        batch_size=16,
+        learning_rate=3e-4,
+        use_ts_sampler=True,
+        fit_intercept=True,
+        activation="tanh",
+        epochs=10000,
+        patience=1000,
+        train_pct=0.7,
+        # Other stuff
+        verbose=False,
+        random_state=42,
+    ):
+        # assert economic_objective in ["sharpe", "sortino", "meanvariance", "meanvarianceskew", "crra"]
+
+        self.n_latent = n_latent
+        self.weight_decay = weight_decay
+        self.reg_turnover = reg_turnover
+        self.batch_size = batch_size
+        self.learning_rate = learning_rate
+        self.use_ts_sampler = use_ts_sampler
+        self.fit_intercept = fit_intercept
+        self.activation = activation
+        self.epochs = epochs
+        self.patience = patience
+        self.train_pct = train_pct
+        # self.long_only = long_only
+        self.verbose = verbose
+        self.random_state = random_state
+
+        self.model = None
+
+    def fit(self, X, y):
+        torch.manual_seed(self.random_state)
+
+        self.model = MultiHeadRetNet(
+            n_inputs=X.shape[1],
+            n_latent=self.n_latent,
+            n_outputs=y.shape[1],
+            fit_intercept=self.fit_intercept,
+            activation=self.activation,
+        )
+
+        cfg = NNTrainConfig(
+            weight_decay=self.weight_decay,
+            reg_turnover=self.reg_turnover,
+            batch_size=self.batch_size,
+            learning_rate=self.learning_rate,
+            use_ts_sampler=self.use_ts_sampler,
+            epochs=self.epochs,
+            patience=self.patience,
+            train_pct=self.train_pct,
+            verbose=self.verbose,
+            random_state=self.random_state,
+        )
+        trainer = TorchTrainer(cfg)
+
+        self.model, self.X_scaler, self.y_scaler = trainer.fit(self.model, X, y)
+        return self
+
+    def predict(self, X):
+        # Scale data
+        X_scaled = self.X_scaler.transform(X)
+
+        # Switch to evaluation mode
+        self.model.eval()
+        with torch.no_grad():
+            # Convert to torch and pass through network
+            X_scaled_torch = torch.Tensor(X_scaled)
+            preds = self.model(X_scaled_torch).numpy()
+
+            # Convert back to original
+            preds = self.y_scaler.inverse_transform(preds)
+
+        return preds
---
+++ macrosynergy/learning/forecasting/torch/trainer.py
@@ -0,0 +1,170 @@
+from dataclasses import dataclass
+from copy import deepcopy
+import numpy as np
+import torch
+import torch.nn as nn
+from sklearn.preprocessing import StandardScaler
+
+
+class MLPTrainer:
+    def __init__(
+        self,
+        train_pct: float = 0.8,
+        batch_size: int = 256,
+        use_ts_sampler: bool = False,
+        learning_rate: float = 1e-3,
+        weight_decay: float = 0.0,
+        epochs: int = 50,
+        loss_fn: nn.Module = nn.MSELoss(),
+        x_scaler=StandardScaler(with_mean=False),
+        y_scaler=StandardScaler(with_mean=False),
+        patience: int = 5,
+        reg_turnover: float = 0.0,
+        random_state: int = 0,
+        verbose: bool = False,
+    ):
+        self.train_pct = train_pct
+        self.batch_size = batch_size
+        self.use_ts_sampler = use_ts_sampler
+        self.learning_rate = learning_rate
+        self.weight_decay = weight_decay
+        self.epochs = epochs
+        self.loss_fn = loss_fn
+        self.patience = patience
+        self.reg_turnover = reg_turnover
+        self.random_state = random_state
+        self.verbose = verbose
+
+        self.x_scaler = None
+        self.y_scaler = None
+
+    def fit(self, model, X, y):
+        torch.manual_seed(self.random_state)
+
+        X_train, y_train, X_val, y_val = self._time_split(X, y)
+
+        X_train_s, X_val_s = self._fit_transform_X(X_train, X_val)
+        y_train_s, y_val_s = self._fit_transform_y(y_train, y_val)
+
+        train_loader, train_loader_eval, valid_loader = self._make_loaders(
+            X_train_s, y_train_s, X_val_s, y_val_s
+        )
+
+        optimizer = torch.optim.AdamW(
+            params=model.parameters(),
+            lr=self.learning_rate,
+            weight_decay=self.weight_decay,
+        )
+
+        best_score = np.inf
+        best_state = None
+        counter = 0
+
+        for epoch in range(self.epochs):
+            model.train()
+            for X_i, y_i in train_loader:
+                optimizer.zero_grad()
+                preds = model(X_i)
+                loss = self.loss_fn(y_i, preds)
+
+                if self.reg_turnover > 0:
+                    pweight_levels = preds[1:] - preds[:-1]
+                    pweight_l1 = torch.mean(torch.abs(pweight_levels))
+                    loss = loss + self.reg_turnover * pweight_l1
+
+                loss.backward()
+                optimizer.step()
+
+            train_loss = self._eval_loss(model, train_loader_eval, self.loss_fn)
+            valid_loss = self._eval_loss(model, valid_loader, self.loss_fn)
+
+            if valid_loss < best_score:
+                best_score = valid_loss
+                best_state = deepcopy(model.state_dict())
+                counter = 0
+            else:
+                counter += 1
+                if counter >= self.patience:
+                    break
+
+            if self.verbose and (epoch % 5 == 0):
+                print(
+                    f"Epoch {epoch+1}: train_loss={train_loss:.6g}, valid_loss={valid_loss:.6g}"
+                )
+
+        if best_state is not None:
+            model.load_state_dict(best_state)
+
+        return model, self.x_scaler, self.y_scaler
+
+    def _time_split(self, X, y):
+        dates = sorted(X.index.get_level_values(1).unique())
+        cut = int(self.train_pct * len(dates))
+        train_dates, val_dates = dates[:cut], dates[cut:]
+
+        X_train = X[X.index.get_level_values(1).isin(train_dates)]
+        y_train = y[y.index.get_level_values(1).isin(train_dates)]
+        X_val = X[X.index.get_level_values(1).isin(val_dates)]
+        y_val = y[y.index.get_level_values(1).isin(val_dates)]
+        return X_train, y_train, X_val, y_val
+
+    def _make_loaders(self, X_train_s, y_train_s, X_val_s, y_val_s):
+        train_dataset = torch.utils.data.TensorDataset(
+            torch.Tensor(X_train_s), torch.Tensor(y_train_s)
+        )
+        valid_dataset = torch.utils.data.TensorDataset(
+            torch.Tensor(X_val_s), torch.Tensor(y_val_s)
+        )
+
+        if not self.use_ts_sampler:
+            train_loader = torch.utils.data.DataLoader(
+                dataset=train_dataset, batch_size=self.batch_size, shuffle=True
+            )
+            train_loader_eval = torch.utils.data.DataLoader(
+                dataset=train_dataset, batch_size=32, shuffle=False
+            )
+        else:
+            train_loader = torch.utils.data.DataLoader(
+                dataset=train_dataset,
+                batch_sampler=TimeSeriesSampler(
+                    dataset=train_dataset, batch_size=self.batch_size, shuffle=True
+                ),
+            )
+            train_loader_eval = torch.utils.data.DataLoader(
+                dataset=train_dataset,
+                batch_sampler=TimeSeriesSampler(
+                    dataset=train_dataset, batch_size=self.batch_size, shuffle=False
+                ),
+            )
+
+        valid_loader = torch.utils.data.DataLoader(
+            dataset=valid_dataset, batch_size=32, shuffle=False
+        )
+
+        return train_loader, train_loader_eval, valid_loader
+
+    def _fit_transform_X(self, X_train, X_val):
+        if self.x_scaler is None:
+            return X_train, X_val
+        else:
+            X_train_s = self.x_scaler.transform(X_train)
+            X_val_s = self.x_scaler.transform(X_val)
+        return X_train_s, X_val_s
+
+    def _fit_transform_y(self, y_train, y_val):
+        if self.y_scaler is None:
+            return y_train, y_val
+        else:
+            y_train_s = self.y_scaler.transform(y_train)
+            y_val_s = self.y_scaler.transform(y_val)
+        return y_train_s, y_val_s
+
+    @staticmethod
+    def _eval_loss(model, loader, loss_fn):
+        model.eval()
+        total = 0.0
+        with torch.no_grad():
+            for X_i, y_i in loader:
+                preds = model(X_i)
+                total += loss_fn(y_i, preds).item()
+        return total / len(loader)
