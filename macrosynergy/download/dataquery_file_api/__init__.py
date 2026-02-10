2e2e2134a27a7e28f841988bcd1e6494f4b63e37
Palash Tyagi <ptyagi@macrosynergy.com>
2026-02-10 09:47:11 +0000

    moved dataquery_file_api to a submodule

---
+++ macrosynergy/download/dataquery_file_api/__init__.py
@@ -0,0 +1,15 @@
+from dataquery_file_api import (
+    DataQueryFileAPIClient,
+    DataQueryFileAPIOauth,
+    JPMAQS_EARLIEST_FILE_DATE,
+    JPMAQS_DATASET_THEME_MAPPING,
+)
+
+
+__all__ = [
+    "DataQueryFileAPIClient",
+    "DataQueryFileAPIOauth",
+    # Constants
+    "JPMAQS_EARLIEST_FILE_DATE",
+    "JPMAQS_DATASET_THEME_MAPPING",
+]
