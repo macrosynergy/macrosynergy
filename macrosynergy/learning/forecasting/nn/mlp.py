812f63f9c600cf6165bf78de6d6a4c8daad404b0
Rushil Gholkar <67958144+rushilg99@users.noreply.github.com>
2026-04-24 23:09:31 +0100

    checks, optimizer options, optional scaling, drop_last and aggregate_last

--- macrosynergy/learning/forecasting/nn/mlp.py
+++ macrosynergy/learning/forecasting/nn/mlp.py
@@ -13,6 +13,8 @@ from macrosynergy.learning.forecasting.torch.models.mlps import MultiLayerPercep
 
 from copy import deepcopy
 
+import numbers
+
 class MLPRegressor(BaseEstimator, RegressorMixin):
     def __init__(
         self,
@@ -26,25 +28,53 @@ class MLPRegressor(BaseEstimator, RegressorMixin):
         head_activation = "identity",
         # Neural network training dynamics
         loss_func = torch.nn.MSELoss(),
-        optimizer: str = "AdamW", # TODO: SGD, LARS, option for custom optimizer object
-        scheduler: Optional[str] = None, # TODO: consider default of one cycle LR, options for other schedulers, option for custom scheduler object
+        optimizer: str = "AdamW", # TODO: option for multiple optimizers as well as lars and ability to pass in a custom optimizer. 
+        scheduler: Optional[str] = None, # TODO: options for other schedulers, option for custom scheduler object
         batch_size = 32,
         learning_rate = 3e-4,
         weight_decay = 1e-4,
         reg_turnover = 0,
-        use_ts_sampler = True,
-        aggregate_last = True, # TODO: aggregate last and drop_last can't both be set to True
+        use_ts_sampler = True, # TODO: turn this into an optional sampler object
+        aggregate_last = True,
         drop_last = False,
         epochs = 10000, # NOTE: when a scheduler is used, the epochs default is way too high unless the patience is high
         patience = 1000,
         train_pct = 0.7,
-        x_scaler = StandardScaler(with_mean=False), # TODO: Can be made optional due to panel z scoring
-        y_scaler = StandardScaler(with_mean=False), # TODO: Can be made optional due to vol targeted returns and sharpe loss
+        x_scaler = StandardScaler(with_mean=False),
+        y_scaler = StandardScaler(with_mean=False), 
         # Other stuff 
         verbose = False,
         random_state = 42,
         inverse_transform_preds = False
     ):
+        # Checks 
+        self._check_init_params(
+            n_latent,
+            fit_encoder_intercept,
+            fit_head_intercept,
+            encoder_activation,
+            head_activation,
+            loss_func,
+            optimizer,
+            scheduler,
+            batch_size,
+            learning_rate,
+            weight_decay,
+            reg_turnover,
+            use_ts_sampler,
+            aggregate_last,
+            drop_last,
+            epochs,
+            patience,
+            train_pct,
+            x_scaler,
+            y_scaler,
+            verbose,
+            random_state,
+            inverse_transform_preds
+        )
+
+        # Attributes
         self.n_latent = n_latent
         self.fit_encoder_intercept = fit_encoder_intercept
         self.fit_head_intercept = fit_head_intercept
@@ -186,19 +216,25 @@ class MLPRegressor(BaseEstimator, RegressorMixin):
         x_scaler,
         y_scaler,
     ):
-        """
-        TODO: handle case for no scaling 
-        """
         # Scale independent variables
-        x_scaler.fit(X_train)
-        X_train_s = x_scaler.transform(X_train)
-        X_valid_s = x_scaler.transform(X_valid)
+        if x_scaler:
+            x_scaler.fit(X_train)
+            X_train_s = x_scaler.transform(X_train)
+            X_valid_s = x_scaler.transform(X_valid)
+        else:
+            X_train_s = X_train.values
+            X_valid_s = X_valid.values
+
 
         # Scale dependent variables
         # TODO: ensure ys are 2d for this to work
-        y_scaler.fit(y_train)
-        y_train_s = y_scaler.transform(y_train) 
-        y_valid_s = y_scaler.transform(y_valid)
+        if y_scaler:
+            y_scaler.fit(y_train)
+            y_train_s = y_scaler.transform(y_train) 
+            y_valid_s = y_scaler.transform(y_valid)
+        else:
+            y_train_s = y_train.values
+            y_valid_s = y_valid.values
 
         return X_train_s, X_valid_s, y_train_s, y_valid_s
     
@@ -231,11 +267,13 @@ class MLPRegressor(BaseEstimator, RegressorMixin):
                 dataset = train_dataset,
                 batch_size = self.batch_size,
                 shuffle = True,
+                drop_last = drop_last
             )
             train_loader_eval = torch.utils.data.DataLoader(
                 dataset = train_dataset,
                 batch_size = self.batch_size,
                 shuffle = False,
+                drop_last = False,
             )
         else:
             train_loader = torch.utils.data.DataLoader(
@@ -244,6 +282,8 @@ class MLPRegressor(BaseEstimator, RegressorMixin):
                     dataset=train_dataset,
                     batch_size=batch_size,
                     shuffle = True,
+                    aggregate_last = aggregate_last,
+                    drop_last = drop_last
                 )
             )
             train_loader_eval = torch.utils.data.DataLoader(
@@ -252,13 +292,19 @@ class MLPRegressor(BaseEstimator, RegressorMixin):
                     dataset=train_dataset,
                     batch_size=batch_size,
                     shuffle = False,
+                    aggregate_last = aggregate_last,
+                    drop_last = False
                 )
             )
 
         valid_loader = torch.utils.data.DataLoader(
             dataset = valid_dataset,
             batch_sampler=TimeSeriesSampler(
-                dataset=valid_dataset, batch_size=self.batch_size, shuffle=False
+                dataset=valid_dataset,
+                batch_size=self.batch_size,
+                shuffle=False,
+                aggregate_last = aggregate_last,
+                drop_last = False
             ),
         )
 
@@ -267,8 +313,13 @@ class MLPRegressor(BaseEstimator, RegressorMixin):
     def make_optimizer(self, model, optimizer_name, learning_rate, weight_decay):
         if optimizer_name == "AdamW":
             optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+        elif optimizer_name == "SGD":
+            optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+        elif optimizer_name == "SGD+mom":
+            optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, weight_decay=weight_decay, momentum = 0.9)
         else:
-            # TODO: add more optimizers later
+            # TODO: add LARS for large batch SGD training
+            # TODO: add ability to pass in an optimizer class inheriting from torch.optim.Optimizer
             raise ValueError(f"Unsupported optimizer: {optimizer_name}")
         
         return optimizer
@@ -387,6 +438,165 @@ class MLPRegressor(BaseEstimator, RegressorMixin):
             counter += 1
             
         return best_score, best_state, counter
+    
+    def _check_init_params(
+        self,
+        n_latent,
+        fit_encoder_intercept,
+        fit_head_intercept,
+        encoder_activation,
+        head_activation,
+        loss_func,
+        optimizer,
+        scheduler,
+        batch_size,
+        learning_rate,
+        weight_decay,
+        reg_turnover,
+        use_ts_sampler,
+        aggregate_last,
+        drop_last,
+        epochs,
+        patience,
+        train_pct,
+        x_scaler,
+        y_scaler,
+        verbose,
+        random_state,
+        inverse_transform_preds
+    ):
+        # n_latent
+        if not isinstance(n_latent, numbers.Integral):
+            if not isinstance(n_latent, list):
+                raise TypeError("n_latent must be either an integer or a list of integers.")
+            if not all(isinstance(x, numbers.Integral) for x in n_latent):
+                raise TypeError("When n_latent is a list, all elements must be integers.")
+            if len(n_latent) <= 1:
+                raise ValueError("When n_latent is a list, it must contain more than one element.")
+            if not all(x >= 1 for x in n_latent):
+                raise ValueError("When n_latent is a list, all elements must be at least 1.")
+        else:
+            if n_latent < 1:
+                raise ValueError("When n_latent is an integer, it must be at least 1.")
+            
+        # fit_encoder_intercept
+        if not isinstance(fit_encoder_intercept, bool):
+            raise TypeError("fit_encoder_intercept must be a boolean.")
+        
+        # fit_head_intercept
+        if not isinstance(fit_head_intercept, bool):
+            raise TypeError("fit_head_intercept must be a boolean.")
+        
+        # encoder_activation
+        if not isinstance(encoder_activation, str):
+            raise TypeError("encoder_activation must be a string.")
+        if encoder_activation not in {"tanh", "relu", "sigmoid"}:
+            raise ValueError(
+                "encoder_activation must be one of 'tanh', 'relu', or 'sigmoid'."
+            )
+        
+        # head_activation
+        if not isinstance(head_activation, str):
+            raise TypeError("head_activation must be a string.")
+        if head_activation not in {"tanh", "relu", "sigmoid", "identity"}:
+            raise ValueError(
+                "head_activation must be one of 'tanh', 'relu', 'sigmoid', or 'identity'."
+            )
+        
+        # loss_func
+        if not isinstance(loss_func, nn.Module):
+            raise TypeError("loss_func must inherit from nn.Module.")
+        
+        # optimizer
+        if not isinstance(optimizer, str):
+            raise TypeError("optimizer must be a string.")
+        if optimizer not in {"AdamW", "SGD", "SGD+mom"}:
+            raise ValueError("optimizer must be one of 'AdamW', 'SGD', or 'SGD+mom'.")
+        
+        # scheduler
+        if scheduler is not None:
+            if not isinstance(scheduler, str):
+                raise TypeError("scheduler must be a string or None.")
+            if scheduler not in {"OneCycleLR"}:
+                raise ValueError("scheduler must be one of 'OneCycleLR' or None.")
+            
+        # batch_size
+        if not isinstance(batch_size, numbers.Integral):
+            raise TypeError("batch_size must be an integer.")
+        if batch_size < 1:
+            raise ValueError("batch_size must be at least 1.")
+        
+        # learning_rate
+        if not isinstance(learning_rate, numbers.Real):
+            raise TypeError("learning_rate must be a real number.")
+        if learning_rate <= 0:
+            raise ValueError("learning_rate must be positive.")
+
+        # weight_decay
+        if not isinstance(weight_decay, numbers.Real):
+            raise TypeError("weight_decay must be a real number.")
+        if weight_decay < 0:
+            raise ValueError("weight_decay must be non-negative.")
+        
+        # reg_turnover
+        if not isinstance(reg_turnover, numbers.Real):
+            raise TypeError("reg_turnover must be a real number.")
+        if reg_turnover < 0:
+            raise ValueError("reg_turnover must be non-negative.")
+        
+        # use_ts_sampler
+        if not isinstance(use_ts_sampler, bool):
+            raise TypeError("use_ts_sampler must be a boolean.")
+        
+        # aggregate_last
+        if not isinstance(aggregate_last, bool):
+            raise TypeError("aggregate_last must be a boolean.")
+        
+        # drop_last
+        if not isinstance(drop_last, bool):
+            raise TypeError("drop_last must be a boolean.")
+        if aggregate_last and drop_last:
+            raise ValueError("aggregate_last and drop_last cannot both be True.")
+        
+        # epochs
+        if not isinstance(epochs, numbers.Integral):
+            raise TypeError("epochs must be an integer.")
+        if epochs < 1:
+            raise ValueError("epochs must be at least 1.")
+        
+        # patience
+        if not isinstance(patience, numbers.Integral):
+            raise TypeError("patience must be an integer.")
+        if patience < 1:
+            raise ValueError("patience must be at least 1.")
+        
+        # train_pct
+        if not isinstance(train_pct, numbers.Real):
+            raise TypeError("train_pct must be a real number.")
+        if not (0 < train_pct < 1):
+            raise ValueError("train_pct must be between 0 and 1.")
+        
+        # x_scaler
+        if x_scaler is not None:
+            if not isinstance(x_scaler, StandardScaler):
+                raise TypeError("x_scaler must be an instance of StandardScaler or None.")
+        
+        # y_scaler
+        if y_scaler is not None:
+            if not isinstance(y_scaler, StandardScaler):
+                raise TypeError("y_scaler must be an instance of StandardScaler or None.")
+
+        # verbose
+        if not isinstance(verbose, bool):
+            raise TypeError("verbose must be a boolean.")
+        
+        # random_state
+        if not isinstance(random_state, numbers.Integral):
+            raise TypeError("random_state must be an integer.")
+        
+        # inverse_transform_preds
+        if not isinstance(inverse_transform_preds, bool):
+            raise TypeError("inverse_transform_preds must be a boolean.")
 
 if __name__ == "__main__":
     from macrosynergy.learning import (
